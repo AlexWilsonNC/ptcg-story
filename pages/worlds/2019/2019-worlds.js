70f0db5442510cb4d4f03b93aaf156324ca77439
AlexWilsonNC <alexwilson0017@yahoo.com>
2022-03-16 17:53:20 -0400

    results

--- pages/worlds/2019/2019-worlds.js
+++ pages/worlds/2019/2019-worlds.js
@@ -6,14 +6,14 @@ let masters = [
     flag: '../../../assets/flags/australia.png',
     sprite1: '../../../assets/sprites/mewtwo.png',
     sprite2: '../../../assets/sprites/mew.png',
-    list: '../../../assets/list-icon.jpg'
+    list: '../../../assets/list-icon.png'
   },
   {
     firstName: 'Shintaro Ito',
     flag: '../../../assets/flags/japan.png',
-    sprite1: '../assets/sprites/blacephalon.png',
-    sprite2: '../assets/sprites/naganadel.png',
-    list: '../../../assets/list-icon.jpg'
+    sprite1: '../../../assets/sprites/blacephalon.png',
+    sprite2: '../../../assets/sprites/naganadel.png',
+    list: '../../../assets/list-icon.png'
   },
 ]
 
@@ -25,20 +25,39 @@ function displayList(array = []) {
 
     let item_element = document.createElement('li');
     item_element.classList.add('player-list-hover');
-
     let title = document.createElement('li');
     title.classList.add('results-list-item');
+
     let playerName = document.createElement('div');
     playerName.classList.add('name-n-flag');
 
     let flagImg = document.createElement('img');
     flagImg.classList.add('flag-size');
-    document.querySelectorAll('flag-size').src = item.flag;
+    flagImg.setAttribute('src', item.flag);
     
-    playerName.innerHTML = item.flag + " " + item.firstName;
+    playerName.innerText = item.firstName;
 
+    let deck = document.createElement('div');
+    deck.classList.add('player-deck-icons');
+
+    let firstSprite = document.createElement('img');
+    firstSprite.classList.add('sprite');
+    firstSprite.setAttribute('src', item.sprite1);
+    let secondSprite = document.createElement('img');
+    secondSprite.classList.add('sprite');
+    secondSprite.classList.add('second-sprite');
+    secondSprite.setAttribute('src', item.sprite2);
+    let listIcon = document.createElement('img');
+    listIcon.classList.add('list-icon');
+    listIcon.setAttribute('src', item.list);
+    
     item_element.appendChild(title);
     title.appendChild(playerName);
+    playerName.appendChild(flagImg);
+    title.appendChild(deck);
+    playerName.appendChild(firstSprite);
+    playerName.appendChild(secondSprite);
+    playerName.appendChild(listIcon);
 
     if (item.list === '../../../assets/list-icon.jpg') {
       item_element.addEventListener('click', function () {
@@ -47,12 +66,6 @@ function displayList(array = []) {
         document.querySelector('.playerName').innerHTML = item.firstName;
       })
     }
-
-    // let cs = document.createElement('div');
-    // cs.classList.add('item-point');
-    // cs.innerHTML = item.csPoints;
-
-    // item_element.appendChild(cs);
 
     list.appendChild(item_element);
   }
