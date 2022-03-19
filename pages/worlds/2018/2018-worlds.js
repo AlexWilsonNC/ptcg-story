6709b66521cee6b57b7f897c1cb6f15c69fc880b
AlexWilsonNC <alexwilson0017@yahoo.com>
2022-03-18 22:03:33 -0400

    18

---
+++ pages/worlds/2018/2018-worlds.js
@@ -0,0 +1,530 @@
+const pokemonCard = fetch('https://api.pokemontcg.io/v2/cards/swsh1-1')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+const masterList = document.querySelector('.masters-ol');
+const seniorList = document.querySelector('.seniors-ol');
+const juniorList = document.querySelector('.juniors-ol');
+
+let masters = [
+  {
+    firstName: 'Robin Schulz',
+    flag: '../../../assets/flags/germany.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Jeff Kolenc',
+    flag: '../../../assets/flags/canada.png',
+    sprite1: '../../../assets/sprites/malamar.png',
+    sprite2: '../../../assets/sprites/necrozma.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Klive Aw Junjie',
+    flag: '../../../assets/flags/singapore.png',
+    sprite1: '../../../assets/sprites/zygarde-complete.png',
+    sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Pedro Torres',
+    flag: '../../../assets/flags/spain.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/rayquaza.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Eric Smith',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/rayquaza.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Nicolas Galaz',
+    flag: '../../../assets/flags/canada.png',
+    sprite1: '../../../assets/sprites/buzzwole.png',
+    sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Shuto Itagaki',
+    flag: '../../../assets/flags/japan.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/rayquaza.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Brian Miller',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/golisopod.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Martin Janous',
+    flag: '../../../assets/flags/czech-republic.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/greninja.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Michael Bergerac',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/rayquaza.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Masataka Hirano',
+    flag: '../../../assets/flags/japan.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/gallade.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Fabien Pujol',
+    flag: '../../../assets/flags/france.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/rayquaza.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Rahul Reddy',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/banette.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Jimmy Pendarvis',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Christopher Schemanske',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/buzzwole.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Hunter Butler',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/buzzwole.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Luca Clavadetscher',
+    flag: '../../../assets/flags/switzerland.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/greninja.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Marc Lutz',
+    flag: '../../../assets/flags/germany.png',
+    sprite1: '../../../assets/sprites/buzzwole.png',
+    sprite2: '../../../assets/sprites/magcargo.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Joey Ruettiger',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Azul Griego',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Alessandro Cremascoli',
+    flag: '../../../assets/flags/italy.png',
+    sprite1: '../../../assets/sprites/buzzwole.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Takuya Yoneda',
+    flag: '../../../assets/flags/japan.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/gallade.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Seitaro Shibuya',
+    flag: '../../../assets/flags/japan.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Natalie Millar',
+    flag: '../../../assets/flags/australia.png',
+    sprite1: '../../../assets/sprites/buzzwole.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Ryan Moorhouse',
+    flag: '../../../assets/flags/uk.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Alex Schemanske',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/buzzwole.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Kristian Hodas',
+    flag: '../../../assets/flags/slovakia.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/greninja.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Gustavo Wada',
+    flag: '../../../assets/flags/brazil.png',
+    sprite1: '../../../assets/sprites/malamar.png',
+    sprite2: '../../../assets/sprites/hoopa-unbound.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Angel Torres',
+    flag: '../../../assets/flags/spain.png',
+    sprite1: '../../../assets/sprites/blank.png',
+    sprite2: '../../../assets/sprites/rayquaza.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Treynor Wolfe',
+    flag: '../../../assets/flags/usa.png',
+    sprite1: '../../../assets/sprites/buzzwole.png',
+    sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Clifton Goh',
+    flag: '../../../assets/flags/singapore.png',
+    sprite1: '../../../assets/sprites/yveltal.png',
+    sprite2: '../../../assets/sprites/hoopa-unbound.png',
+    list: '../../../assets/list-icon.png'
+  },
+  {
+    firstName: 'Philip Schulz',
+    flag: '../../../assets/flags/germany.png',
+    sprite1: '../../../assets/sprites/zoroark.png',
+    sprite2: '../../../assets/sprites/garbodor.png',
+    list: '../../../assets/list-icon.png'
+  }
+]
+
+let seniors = [
+    {
+        firstName: 'Magnus Pederson',
+        flag: '../../../assets/flags/denmark.png',
+        sprite1: '../../../assets/sprites/banette.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Conner Pederson',
+        flag: '../../../assets/flags/usa.png',
+        sprite1: '../../../assets/sprites/buzzwole.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    },{
+        firstName: 'Takumi Kaji',
+        flag: '../../../assets/flags/japan.png',
+        sprite1: '../../../assets/sprites/buzzwole.png',
+        sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Matthew Campbell',
+        flag: '../../../assets/flags/usa.png',
+        sprite1: '../../../assets/sprites/blank.png',
+        sprite2: '../../../assets/sprites/greninja.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Benjamin Branch-Trevathan',
+        flag: '../../../assets/flags/usa.png',
+        sprite1: '../../../assets/sprites/buzzwole.png',
+        sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Caleb Banwarie',
+        flag: '../../../assets/flags/canada.png',
+        sprite1: '../../../assets/sprites/zoroark.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Jihun Choi',
+        flag: '../../../assets/flags/korea.png',
+        sprite1: '../../../assets/sprites/golisopod.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Tanner Hurley',
+        flag: '../../../assets/flags/usa.png',
+        sprite1: '../../../assets/sprites/zoroark.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    }
+]
+
+let juniors = [
+    {
+        firstName: 'Naohito Inoue',
+        flag: '../../../assets/flags/japan.png',
+        sprite1: '../../../assets/sprites/buzzwole.png',
+        sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Sebastian Enriquez',
+        flag: '../../../assets/flags/usa.png',
+        sprite1: '../../../assets/sprites/buzzwole.png',
+        sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Rune Heiremans',
+        flag: '../../../assets/flags/belgium.png',
+        sprite1: '../../../assets/sprites/buzzwole.png',
+        sprite2: '../../../assets/sprites/lycanroc-midnight.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Walker Halliburton',
+        flag: '../../../assets/flags/usa.png',
+        sprite1: '../../../assets/sprites/zoroark.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Kota Onohara',
+        flag: '../../../assets/flags/japan.png',
+        sprite1: '../../../assets/sprites/naganadel.png',
+        sprite2: '../../../assets/sprites/stakataka.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Benjamin Bussert',
+        flag: '../../../assets/flags/usa.png',
+        sprite1: '../../../assets/sprites/buzzwole.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Joao Penteado',
+        flag: '../../../assets/flags/brazil.png',
+        sprite1: '../../../assets/sprites/buzzwole.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    },
+    {
+        firstName: 'Sechan Oh',
+        flag: '../../../assets/flags/korea.png',
+        sprite1: '../../../assets/sprites/zoroark.png',
+        sprite2: '../../../assets/sprites/garbodor.png',
+        list: '../../../assets/list-icon.png'
+    }
+]
+
+//masters
+function displayList(array = []) {
+    masterList.innerHTML = '';
+
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+
+    let item_element = document.createElement('li');
+    item_element.classList.add('player-list-hover');
+    let title = document.createElement('li');
+    title.classList.add('results-list-item');
+
+    let playerName = document.createElement('div');
+    playerName.classList.add('name-n-flag');
+
+    let flagImg = document.createElement('img');
+    flagImg.classList.add('flag-size');
+    flagImg.setAttribute('src', item.flag);
+    
+    let deck = document.createElement('div');
+    deck.classList.add('player-deck-icons');
+
+    let firstSprite = document.createElement('img');
+    firstSprite.classList.add('sprite');
+    firstSprite.setAttribute('src', item.sprite1);
+    let secondSprite = document.createElement('img');
+    secondSprite.classList.add('sprite');
+    secondSprite.classList.add('second-sprite');
+    secondSprite.setAttribute('src', item.sprite2);
+    let listIcon = document.createElement('img');
+    listIcon.classList.add('list-icon');
+    listIcon.setAttribute('src', item.list);
+    
+    item_element.appendChild(title);
+    title.appendChild(playerName);
+    playerName.appendChild(flagImg);
+    playerName.append(item.firstName);
+    title.appendChild(deck);
+    deck.appendChild(firstSprite);
+    deck.appendChild(secondSprite);
+    deck.appendChild(listIcon);
+
+    if (item.list === '../../../assets/list-icon.png') {
+      item_element.addEventListener('click', function () {
+        document.querySelector('.modal').style.display = 'block';
+        document.querySelector('.behind-modal').style.display = 'block';
+        document.querySelector('.playerName').innerHTML = item.firstName;
+      })
+    }
+
+    masterList.appendChild(item_element);
+  }
+}
+
+// seniors
+function displayList2(array = []) {
+    seniorList.innerHTML = '';
+
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+
+    let item_element = document.createElement('li');
+    item_element.classList.add('player-list-hover');
+    let title = document.createElement('li');
+    title.classList.add('results-list-item');
+
+    let playerName = document.createElement('div');
+    playerName.classList.add('name-n-flag');
+
+    let flagImg = document.createElement('img');
+    flagImg.classList.add('flag-size');
+    flagImg.setAttribute('src', item.flag);
+    
+    let deck = document.createElement('div');
+    deck.classList.add('player-deck-icons');
+
+    let firstSprite = document.createElement('img');
+    firstSprite.classList.add('sprite');
+    firstSprite.setAttribute('src', item.sprite1);
+    let secondSprite = document.createElement('img');
+    secondSprite.classList.add('sprite');
+    secondSprite.classList.add('second-sprite');
+    secondSprite.setAttribute('src', item.sprite2);
+    let listIcon = document.createElement('img');
+    listIcon.classList.add('list-icon');
+    listIcon.setAttribute('src', item.list);
+    
+    item_element.appendChild(title);
+    title.appendChild(playerName);
+    playerName.appendChild(flagImg);
+    playerName.append(item.firstName);
+    title.appendChild(deck);
+    deck.appendChild(firstSprite);
+    deck.appendChild(secondSprite);
+    deck.appendChild(listIcon);
+
+    if (item.list === '../../../assets/list-icon.png') {
+      item_element.addEventListener('click', function () {
+        document.querySelector('.modal').style.display = 'block';
+        document.querySelector('.behind-modal').style.display = 'block';
+        document.querySelector('.playerName').innerHTML = item.firstName;
+      })
+    }
+
+    seniorList.appendChild(item_element);
+  }
+}
+
+// juniors
+function displayList3(array = []) {
+    juniorList.innerHTML = '';
+
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+
+    let item_element = document.createElement('li');
+    item_element.classList.add('player-list-hover');
+    let title = document.createElement('li');
+    title.classList.add('results-list-item');
+
+    let playerName = document.createElement('div');
+    playerName.classList.add('name-n-flag');
+
+    let flagImg = document.createElement('img');
+    flagImg.classList.add('flag-size');
+    flagImg.setAttribute('src', item.flag);
+    
+    let deck = document.createElement('div');
+    deck.classList.add('player-deck-icons');
+
+    let firstSprite = document.createElement('img');
+    firstSprite.classList.add('sprite');
+    firstSprite.setAttribute('src', item.sprite1);
+    let secondSprite = document.createElement('img');
+    secondSprite.classList.add('sprite');
+    secondSprite.classList.add('second-sprite');
+    secondSprite.setAttribute('src', item.sprite2);
+    let listIcon = document.createElement('img');
+    listIcon.classList.add('list-icon');
+    listIcon.setAttribute('src', item.list);
+    
+    item_element.appendChild(title);
+    title.appendChild(playerName);
+    playerName.appendChild(flagImg);
+    playerName.append(item.firstName);
+    title.appendChild(deck);
+    deck.appendChild(firstSprite);
+    deck.appendChild(secondSprite);
+    deck.appendChild(listIcon);
+
+    if (item.list === '../../../assets/list-icon.png') {
+      item_element.addEventListener('click', function () {
+        document.querySelector('.modal').style.display = 'block';
+        document.querySelector('.behind-modal').style.display = 'block';
+        document.querySelector('.playerName').innerHTML = item.firstName;
+      })
+    }
+
+    juniorList.appendChild(item_element);
+  }
+}
+
+const modal = document.querySelector('.modal');
+const modalX = document.querySelector('.modal-x');
+const behind = document.querySelector('.behind-modal');
+
+modalX.addEventListener('click', () => {
+  modal.style.display = 'none';
+  behind.style.display = 'none';
+})
+behind.addEventListener('click', () => {
+  modal.style.display = 'none';
+  behind.style.display = 'none';
+})
+
+displayList(masters);
+displayList2(seniors);
+displayList3(juniors);
