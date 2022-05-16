efaa2998ff1b8d11771d74f700e32acbb6e938f3
AlexWilsonNC <alexwilson0017@yahoo.com>
2022-05-16 18:43:11 -0400

    2016 extras

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const cardTouch = document.querySelector('.pokemon-card');
-const cardImage = document.querySelector('.pok-card-small');
+const cardTouch = document.querySelectorAll('.pokemon-card');
+const cardImage = document.querySelectorAll('.pok-card-small');
 const darkBackground = document.querySelector('.behind-large-card');
 
-cardTouch.addEventListener('click', () => {
-    cardImage.classList.add('large-card');
-    darkBackground.style.display = 'block';
-    if (cardImage.classList.contains('large-card')) {
-        darkBackground.addEventListener('click', () => {
-            cardImage.classList.remove('large-card');
-            darkBackground.style.display = 'none';
-        })
-    }
+cardTouch.forEach(card => {
+    card.addEventListener('click', () => {
+        card.querySelector('.pok-card-small').classList.add('large-card');
+        darkBackground.style.display = 'block';
+        if (card.querySelector('.pok-card-small').classList.contains('large-card')) {
+            darkBackground.addEventListener('click', () => {
+                card.querySelector('.pok-card-small').classList.remove('large-card');
+                darkBackground.style.display = 'none';
+            })
+        }
+    })
 });
 
 
