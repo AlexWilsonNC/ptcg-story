d1db6d0e07d1a85faab29943961d49eca41e1036
AlexWilsonNC <alexwilson0017@yahoo.com>
2022-07-27 12:14:42 -0400

    removing formats

--- index.js
+++ index.js
@@ -8,18 +8,24 @@ const deckbuild3 = document.querySelector('.deckbuild3');
 const deckbuild4 = document.querySelector('.deckbuild4');
 const deckbuild5 = document.querySelector('.deckbuild5');
 const deckbuild6 = document.querySelector('.deckbuild6');
+const deckbuild7 = document.querySelector('.deckbuild7');
+const deckbuild8 = document.querySelector('.deckbuild8');
 const buildBox1 = document.querySelector('.build-box1');
 const buildBox2 = document.querySelector('.build-box2');
 const buildBox3 = document.querySelector('.build-box3');
 const buildBox4 = document.querySelector('.build-box4');
 const buildBox5 = document.querySelector('.build-box5');
 const buildBox6 = document.querySelector('.build-box6');
+const buildBox7 = document.querySelector('.build-box7');
+const buildBox8 = document.querySelector('.build-box8');
 const descBox1 = document.querySelector('.descrip-box1');
 const descBox2 = document.querySelector('.descrip-box2');
 const descBox3 = document.querySelector('.descrip-box3');
 const descBox4 = document.querySelector('.descrip-box4');
 const descBox5 = document.querySelector('.descrip-box5');
 const descBox6 = document.querySelector('.descrip-box6');
+const descBox7 = document.querySelector('.descrip-box7');
+const descBox8 = document.querySelector('.descrip-box8');
 
 cardTouch.forEach(card => {
     card.addEventListener('click', () => {
@@ -115,6 +121,26 @@ function changeToBuild6() {
     descBox5.style.display = 'none';
     descBox6.style.display = 'flex';
 }
+function changeToBuild7() {
+    deckbuild7.classList.add('dark-bg');
+    deckbuild8.classList.remove('dark-bg');
+    deckbuild8.classList.add('light-bg');
+
+    buildBox7.style.display = 'flex';
+    buildBox8.style.display = 'none';
+    descBox7.style.display = 'flex';
+    descBox8.style.display = 'none';
+}
+function changeToBuild8() {
+    deckbuild8.classList.add('dark-bg');
+    deckbuild7.classList.remove('dark-bg');
+    deckbuild7.classList.add('light-bg');
+
+    buildBox8.style.display = 'flex';
+    buildBox7.style.display = 'none';
+    descBox7.style.display = 'none';
+    descBox8.style.display = 'flex';
+}
 
 deckbuild1.addEventListener('click', changeToBuild1)
 deckbuild2.addEventListener('click', changeToBuild2)
@@ -122,3 +148,5 @@ deckbuild3.addEventListener('click', changeToBuild3)
 deckbuild4.addEventListener('click', changeToBuild4)
 deckbuild5.addEventListener('click', changeToBuild5)
 deckbuild6.addEventListener('click', changeToBuild6)
+deckbuild7.addEventListener('click', changeToBuild7)
+deckbuild8.addEventListener('click', changeToBuild8)
