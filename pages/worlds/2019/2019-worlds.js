210a7bfd4fd1911f41de90c7d5b5186c6f5b305b
AlexWilsonNC <alexwilson0017@yahoo.com>
2022-08-21 18:38:14 -0400

    white border

--- pages/worlds/2019/2019-worlds.js
+++ pages/worlds/2019/2019-worlds.js
@@ -7721,6 +7721,10 @@ function displayList(array = []) {
           document.querySelector('.modal').style.backgroundImage = "url(../../../assets/dark-bg.png)";
         }
 
+        if (item.type == 'normal') {
+          document.querySelector('.playerName').style.color = "black";
+        }
+
         for (let i = 0; i < item.deck.length; i++) {
           let deck = item.deck[i];
 
