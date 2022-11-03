a2c6006e6413da343e191185cc17d01b5b1a7527
AlexWilsonNC <alexwilson0017@yahoo.com>
2022-11-03 15:06:40 -0400

    message

--- zoom.js
+++ zoom.js
@@ -9,6 +9,17 @@ window.onload = () => {
         zoombox.innerHTML = "";
         zoombox.appendChild(clone);
         zoombox.className = "show";
+
+        // document.body.style.position = 'fixed';
+        // document.body.style.top = `-${window.scrollY}px`;
+
+        // document.body.style.position = '';
+        // document.body.style.top = '';
+
+        // const scrollY = document.body.style.top;
+        // document.body.style.position = '';
+        // document.body.style.top = '';
+        // window.scrollTo(0, parseInt(scrollY || '0') * -1);
       };
     }}
    
