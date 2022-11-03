658592cd692e1567fe2ef8fce81d9ac8b10a19c0
AlexWilsonNC <alexwilson0017@yahoo.com>
2022-11-03 15:06:50 -0400

    message

--- zoom-worlds.js
+++ zoom-worlds.js
@@ -10,16 +10,7 @@ window.onload = () => {
         zoombox.appendChild(clone);
         zoombox.className = "show";
 
-        // document.body.style.position = 'fixed';
-        // document.body.style.top = `-${window.scrollY}px`;
 
-        // document.body.style.position = '';
-        // document.body.style.top = '';
-
-        // const scrollY = document.body.style.top;
-        // document.body.style.position = '';
-        // document.body.style.top = '';
-        // window.scrollTo(0, parseInt(scrollY || '0') * -1);
       };
     }}
    
