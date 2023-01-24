1d346d398f87c93d7dc4b1466187d9eac42e1cf9
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-01-24 12:32:44 -0500

    17 naic

--- pages/nats/2017/naic/2017-naic.js
+++ pages/nats/2017/naic/2017-naic.js
@@ -4315,8 +4315,12 @@ let masters = [
         numImg: '../../../../assets/02.png'
       },
       {
+        cardImg: '../../../../assets/cards/2018-17/oricorio.png',
+        numImg: '../../../../assets/01.png'
+      },
+      {
         cardImg: '../../../../assets/cards/2018-17/oricorio-energy.png',
-        numImg: '../../../../assets/02.png'
+        numImg: '../../../../assets/01.png'
       },
       {
         cardImg: '../../../../assets/cards/2018-17/professor-sycamore.png',
