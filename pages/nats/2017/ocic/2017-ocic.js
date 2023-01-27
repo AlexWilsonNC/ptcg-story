46bf466e30009144b970c4c4990dffe633425695
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-01-27 13:37:42 -0500

    17 ocic

--- pages/nats/2017/ocic/2017-ocic.js
+++ pages/nats/2017/ocic/2017-ocic.js
@@ -3267,7 +3267,7 @@ function displayList(array = []) {
         document.querySelector('#modal-section').style.display = "flex";
         document.querySelector('.modal').style.display = 'block';
         document.querySelector('.behind-modal').style.display = 'block';
-                    document.querySelector('.playerName').innerHTML = item.firstName + " - 2020 LAIC";
+                    document.querySelector('.playerName').innerHTML = item.firstName + " - 2017 OCIC";
 
         if (item.type == 'psychic') {
           document.querySelector('.modal').style.backgroundImage = "url(../../../../assets/psychic-bg.png)";
@@ -3391,7 +3391,7 @@ function displayList2(array = []) {
         document.querySelector('#modal-section').style.display = "flex";
         document.querySelector('.modal').style.display = 'block';
         document.querySelector('.behind-modal').style.display = 'block';
-                    document.querySelector('.playerName').innerHTML = item.firstName + " - 2020 LAIC";
+                    document.querySelector('.playerName').innerHTML = item.firstName + " - 2017 OCIC";
 
         if (item.type == 'psychic') {
           document.querySelector('.modal').style.backgroundImage = "url(../../../../assets/psychic-bg.png)";
@@ -3514,7 +3514,7 @@ function displayList3(array = []) {
         document.querySelector('#modal-section').style.display = "flex";
         document.querySelector('.modal').style.display = 'block';
         document.querySelector('.behind-modal').style.display = 'block';
-                    document.querySelector('.playerName').innerHTML = item.firstName + " - 2020 LAIC";
+                    document.querySelector('.playerName').innerHTML = item.firstName + " - 2017 OCIC";
 
         if (item.type == 'psychic') {
           document.querySelector('.modal').style.backgroundImage = "url(../../../../assets/psychic-bg.png)";
