c421648971f12dbe2359884859c8457736bcbf2c
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-02-03 10:47:38 -0500

    nats

--- pages/nats/country.js
+++ pages/nats/country.js
@@ -1,4 +1,5 @@
 const natSelector = document.querySelector('.open-country-nats');
+const natSelector2010 = document.querySelector('.open-country-nats2010');
 
 function showOtherNats() {
 
@@ -16,6 +17,23 @@ function showOtherNats() {
 
 }
 
+function showOtherNats2010() {
+
+    document.querySelector('.hidden-nat20101').style.display = 'block';
+    document.querySelector('.hidden-nat20102').style.display = 'block';
+
+
+    document.querySelector('.behind-modal3').style.display = 'block';
+
+    behind.addEventListener('click', () => {
+        behind.style.display = 'none';
+        document.querySelector('.hidden-nat20101').style.display = 'none';
+        document.querySelector('.hidden-nat20102').style.display = 'none';
+    })
+
+}
+
 const behind = document.querySelector('.behind-modal3');
 
 natSelector.addEventListener('click', showOtherNats)
+natSelector2010.addEventListener('click', showOtherNats2010)
