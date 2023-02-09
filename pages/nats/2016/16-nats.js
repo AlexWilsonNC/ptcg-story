bc28370c1efeb01a65c1e84259d5f8f1757b7789
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-02-08 22:58:39 -0500

    2016 decks

--- pages/nats/2016/16-nats.js
+++ pages/nats/2016/16-nats.js
@@ -3,6 +3,7 @@ const seniorList = document.querySelector('.uk-seniors-ol');
 const juniorList = document.querySelector('.uk-juniors-ol');
 const fourList = document.querySelector('.four-ol');
 const fiveList = document.querySelector('.five-ol');
+const sixList = document.querySelector('.six-ol');
 
 const modalBottom = document.querySelector('.modal-bottom');
 
@@ -3737,6 +3738,200 @@ let fiveEvent = [
     },
 ]
 
+let sixEvent = [
+    {
+        firstName: 'Rose Gregson',
+        sprite1: '../../../assets/sprites/bronzong.png',
+        sprite2: '../../../assets/sprites/aegislash.png',
+        // list: '../../../assets/list-icon.png',
+        list: '../../../assets/sprites/blank.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../../assets/cards/2014-2016',
+                numImg: '../../../assets/04.png',
+            },
+        ]
+    },
+    {
+        firstName: 'Joshua Richards',
+        sprite1: '../../../assets/sprites/joltik.png',
+        sprite2: '../../../assets/sprites/pumpkaboo.png',
+        list: '../../../assets/list-icon.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../../assets/cards/2014-2016/pumpkaboo.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/joltik.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/lampent.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/shaymin-ex.png',
+                numImg: '../../../assets/03.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/gallade.png',
+                numImg: '../../../assets/01.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/professor-sycamore.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/judge.png',
+                numImg: '../../../assets/02.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/teammates.png',
+                numImg: '../../../assets/02.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/lysandre.png',
+                numImg: '../../../assets/01.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/hex-maniac.png',
+                numImg: '../../../assets/01.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/maxie-hidden-ball-trick.png',
+                numImg: '../../../assets/01.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/vs-seeker.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/puzzle-of-time.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/ultra-ball.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/battle-compressor.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/trainers-mail.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/town-map.png',
+                numImg: '../../../assets/01.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/ffb.png',
+                numImg: '../../../assets/03.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/float-stone.png',
+                numImg: '../../../assets/01.png',
+            },
+            {
+                cardImg: '../../../assets/cards/2014-2016/deminsion-valley.png',
+                numImg: '../../../assets/04.png',
+            },
+            {
+                cardImg: '../../../assets/cards/energy/xy-dce.png',
+                numImg: '../../../assets/04.png',
+            },
+        ]
+    },
+    {
+        firstName: 'David Mathias',
+        sprite1: '../../../assets/sprites/blank.png',
+        sprite2: '../../../assets/hyphen.png',
+        // list: '../../../assets/list-icon.png',
+        list: '../../../assets/sprites/blank.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../../assets/cards/2014-2016',
+                numImg: '../../../assets/04.png',
+            },
+        ]
+    },
+    {
+        firstName: 'Josh Toulmin',
+        sprite1: '../../../assets/sprites/blank.png',
+        sprite2: '../../../assets/sprites/mewtwo-mega-y.png',
+        // list: '../../../assets/list-icon.png',
+        list: '../../../assets/sprites/blank.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../../assets/cards/2014-2016',
+                numImg: '../../../assets/04.png',
+            },
+        ]
+    },
+    {
+        firstName: 'Yasin Balela',
+        sprite1: '../../../assets/sprites/blank.png',
+        sprite2: '../../../assets/hyphen.png',
+        // list: '../../../assets/list-icon.png',
+        list: '../../../assets/sprites/blank.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../../assets/cards/2014-2016',
+                numImg: '../../../assets/04.png',
+            },
+        ]
+    },
+    {
+        firstName: 'Kyle Guest',
+        sprite1: '../../../assets/sprites/blank.png',
+        sprite2: '../../../assets/sprites/rayquaza-mega.png',
+        // list: '../../../assets/list-icon.png',
+        list: '../../../assets/sprites/blank.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../../assets/cards/2014-2016',
+                numImg: '../../../assets/04.png',
+            },
+        ]
+    },
+    {
+        firstName: 'Jack Culkin',
+        sprite1: '../../../assets/sprites/joltik.png',
+        sprite2: '../../../assets/sprites/pumpkaboo.png',
+        // list: '../../../assets/list-icon.png',
+        list: '../../../assets/sprites/blank.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../../assets/cards/2014-2016',
+                numImg: '../../../assets/04.png',
+            },
+        ]
+    },
+    {
+        firstName: 'Lucinda Barton',
+        sprite1: '../../../assets/sprites/blank.png',
+        sprite2: '../../../assets/hyphen.png',
+        // list: '../../../assets/list-icon.png',
+        list: '../../../assets/sprites/blank.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../../assets/cards/2014-2016',
+                numImg: '../../../assets/04.png',
+            },
+        ]
+    },
+]
+
 //masters
 function displayList(array = []) {
     masterList.innerHTML = '';
@@ -4345,6 +4540,128 @@ function displayList5(array = []) {
     }
 }
 
+// six
+function displayList6(array = []) {
+    sixList.innerHTML = '';
+
+    for (let i = 0; i < array.length; i++) {
+        let item = array[i];
+
+        let item_element = document.createElement('li');
+        item_element.classList.add('player-list-hover');
+        let title = document.createElement('li');
+        title.classList.add('results-list-item');
+
+        let playerName = document.createElement('div');
+        playerName.classList.add('name-n-flag');
+
+        let deck = document.createElement('div');
+        deck.classList.add('player-deck-icons');
+
+        let firstSprite = document.createElement('img');
+        firstSprite.classList.add('sprite');
+        firstSprite.setAttribute('src', item.sprite1);
+        let secondSprite = document.createElement('img');
+        secondSprite.classList.add('sprite');
+        secondSprite.classList.add('second-sprite');
+        secondSprite.setAttribute('src', item.sprite2);
+        let listIcon = document.createElement('img');
+        listIcon.classList.add('list-icon');
+        listIcon.setAttribute('src', item.list);
+
+        item_element.appendChild(title);
+        title.appendChild(playerName);
+        playerName.append(item.firstName);
+        title.appendChild(deck);
+        deck.appendChild(firstSprite);
+        deck.appendChild(secondSprite);
+        deck.appendChild(listIcon);
+
+        if (item.list === '../../../assets/list-icon.png') {
+
+            item_element.addEventListener('click', function () {
+
+                document.querySelector('#modal-section').style.display = "flex";
+                document.querySelector('.modal').style.display = 'block';
+                document.querySelector('.behind-modal').style.display = 'block';
+                document.querySelector('.playerName').innerHTML = item.firstName + " - 2016 UK Nats";
+
+                if (item.type == 'psychic') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/psychic-bg.png)";
+                } else if (item.type == 'fire') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/fire-bg.png)";
+                } else if (item.type == 'fairy') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/fairy-bg.png)";
+                } else if (item.type == 'normal') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/normal-bg.png)";
+                } else if (item.type == 'lightning') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/lightning-bg.png)";
+                } else if (item.type == 'dark') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/dark-bg.png)";
+                } else if (item.type == 'dragon') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/dragon-bg.png)";
+                } else if (item.type == 'fighting') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/fighting-bg.png)";
+                } else if (item.type == 'grass') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/grass-bg.png)";
+                } else if (item.type == 'metal') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/metal-bg.png)";
+                } else if (item.type == 'water') {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/water-bg.png)";
+                } else {
+                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/dark-bg.png)";
+                }
+
+
+                for (let i = 0; i < item.deck.length; i++) {
+                    let deck = item.deck[i];
+
+                    let cardSpace = document.createElement('div');
+                    cardSpace.classList.add('pokemon-card');
+
+                    let pokeCard = document.createElement('img');
+                    pokeCard.classList.add('pok-card-small')
+                    pokeCard.setAttribute('src', deck.cardImg);
+
+                    if (deck.oldCard === true) {
+                        pokeCard.classList.add('old-card')
+                    }
+                    if (deck.oldCard === true) {
+                        pokeCard.classList.add('radius-card')
+                    }
+                    if (deck.exCard === true) {
+                        pokeCard.classList.add('ex-era-card')
+                    }
+                    if (deck.radiusCard === true) {
+                        pokeCard.classList.add('radius-card')
+                    }
+
+                    let numberCounter = document.createElement('img');
+                    numberCounter.classList.add('num-1')
+                    numberCounter.setAttribute('src', deck.numImg);
+
+                    cardSpace.appendChild(pokeCard);
+                    cardSpace.appendChild(numberCounter);
+                    modalBottom.appendChild(cardSpace);
+
+                    modalX.addEventListener('click', () => {
+                        modal.style.display = 'none';
+                        behind.style.display = 'none';
+                        cardSpace.remove();
+                    })
+                    behind.addEventListener('click', () => {
+                        modal.style.display = 'none';
+                        behind.style.display = 'none';
+                        cardSpace.remove();
+                    })
+                }
+            })
+        }
+
+        sixList.appendChild(item_element);
+    }
+}
+
 const modal = document.querySelector('.modal');
 const modalX = document.querySelector('.modal-x');
 const behind = document.querySelector('.behind-modal');
@@ -4354,3 +4671,4 @@ displayList2(seniors);
 displayList3(juniors);
 displayList4(fourEvent);
 displayList5(fiveEvent);
+displayList6(sixEvent);
