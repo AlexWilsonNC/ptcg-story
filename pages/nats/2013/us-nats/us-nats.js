809e517a30be78484078f5c7f6bcd6df894b14c8
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-02-15 11:36:35 -0500

    2013 NATS

--- pages/nats/2013/us-nats/us-nats.js
+++ pages/nats/2013/us-nats/us-nats.js
@@ -2179,8 +2179,8 @@ let seniors = [
   {
     firstName: 'David Tomhave',
     flag: '../../../../assets/flags/usa.png',
-    sprite1: '../../../../assets/sprites/blank.png',
-    sprite2: '../../../../assets/hyphen.png',
+    sprite1: '../../../../assets/sprites/kyurem.png',
+    sprite2: '../../../../assets/sprites/deoxys.png',
     // list: '../../../../assets/list-icon.png',
     list: '../../../../assets/sprites/blank.png',
     type: 'dark',
