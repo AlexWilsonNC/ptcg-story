4fa3aed18218244101720fa22906fb87145a5b47
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-03-15 19:23:48 -0400

    sc

--- major-events/2004/east-coast-stadium-challenge/us-nats.js
+++ major-events/2004/east-coast-stadium-challenge/us-nats.js
@@ -268,10 +268,6 @@ function displayList(array = []) {
     let playerName = document.createElement('div');
     playerName.classList.add('name-n-flag');
 
-    let flagImg = document.createElement('img');
-    flagImg.classList.add('flag-size');
-    flagImg.setAttribute('src', item.flag);
-
     let deck = document.createElement('div');
     deck.classList.add('player-deck-icons');
 
@@ -288,7 +284,6 @@ function displayList(array = []) {
 
     item_element.appendChild(title);
     title.appendChild(playerName);
-    playerName.appendChild(flagImg);
     playerName.append(item.firstName);
     title.appendChild(deck);
     deck.appendChild(firstSprite);
@@ -410,10 +405,6 @@ function displayList2(array = []) {
     let playerName = document.createElement('div');
     playerName.classList.add('name-n-flag');
 
-    let flagImg = document.createElement('img');
-    flagImg.classList.add('flag-size');
-    flagImg.setAttribute('src', item.flag);
-
     let deck = document.createElement('div');
     deck.classList.add('player-deck-icons');
 
@@ -430,7 +421,6 @@ function displayList2(array = []) {
 
     item_element.appendChild(title);
     title.appendChild(playerName);
-    playerName.appendChild(flagImg);
     playerName.append(item.firstName);
     title.appendChild(deck);
     deck.appendChild(firstSprite);
@@ -551,10 +541,6 @@ function displayList3(array = []) {
     let playerName = document.createElement('div');
     playerName.classList.add('name-n-flag');
 
-    let flagImg = document.createElement('img');
-    flagImg.classList.add('flag-size');
-    flagImg.setAttribute('src', item.flag);
-
     let deck = document.createElement('div');
     deck.classList.add('player-deck-icons');
 
@@ -571,7 +557,6 @@ function displayList3(array = []) {
 
     item_element.appendChild(title);
     title.appendChild(playerName);
-    playerName.appendChild(flagImg);
     playerName.append(item.firstName);
     title.appendChild(deck);
     deck.appendChild(firstSprite);
--- major-events/2004/west-coast-stadium-challenge/us-nats.js
+++ major-events/2004/west-coast-stadium-challenge/us-nats.js
@@ -328,9 +328,7 @@ function displayList(array = []) {
     let playerName = document.createElement('div');
     playerName.classList.add('name-n-flag');
 
-    let flagImg = document.createElement('img');
-    flagImg.classList.add('flag-size');
-    flagImg.setAttribute('src', item.flag);
+
 
     let deck = document.createElement('div');
     deck.classList.add('player-deck-icons');
@@ -348,7 +346,6 @@ function displayList(array = []) {
 
     item_element.appendChild(title);
     title.appendChild(playerName);
-    playerName.appendChild(flagImg);
     playerName.append(item.firstName);
     title.appendChild(deck);
     deck.appendChild(firstSprite);
@@ -470,9 +467,7 @@ function displayList2(array = []) {
     let playerName = document.createElement('div');
     playerName.classList.add('name-n-flag');
 
-    let flagImg = document.createElement('img');
-    flagImg.classList.add('flag-size');
-    flagImg.setAttribute('src', item.flag);
+
 
     let deck = document.createElement('div');
     deck.classList.add('player-deck-icons');
@@ -490,7 +485,6 @@ function displayList2(array = []) {
 
     item_element.appendChild(title);
     title.appendChild(playerName);
-    playerName.appendChild(flagImg);
     playerName.append(item.firstName);
     title.appendChild(deck);
     deck.appendChild(firstSprite);
@@ -611,9 +605,7 @@ function displayList3(array = []) {
     let playerName = document.createElement('div');
     playerName.classList.add('name-n-flag');
 
-    let flagImg = document.createElement('img');
-    flagImg.classList.add('flag-size');
-    flagImg.setAttribute('src', item.flag);
+
 
     let deck = document.createElement('div');
     deck.classList.add('player-deck-icons');
@@ -631,7 +623,6 @@ function displayList3(array = []) {
 
     item_element.appendChild(title);
     title.appendChild(playerName);
-    playerName.appendChild(flagImg);
     playerName.append(item.firstName);
     title.appendChild(deck);
     deck.appendChild(firstSprite);
