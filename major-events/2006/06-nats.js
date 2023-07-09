51da33f4dc9aff2349846c223182ee70d6958914
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-07-09 01:53:08 -0400

    anna

--- major-events/2006/06-nats.js
+++ major-events/2006/06-nats.js
@@ -1,7 +1,6 @@
 const masterList = document.querySelector('.uk-masters-ol');
 const seniorList = document.querySelector('.uk-seniors-ol');
 const juniorList = document.querySelector('.uk-juniors-ol');
-const fourList = document.querySelector('.four-ol');
 const fiveList = document.querySelector('.five-ol');
 const sixList = document.querySelector('.six-ol');
 const sevenList = document.querySelector('.seven-ol');
@@ -436,28 +435,156 @@ let juniors = [
     },
 ]
 
-let fourEvent = [
+let fiveEvent = [
     {
-        firstName: 'eeee',
-        sprite1: '../../assets/sprites',
-        sprite2: '../../assets/sprites',
-        // list: '../../assets/list-icon.png',
+        firstName: 'Fares Sekkoum',
+        sprite1: '../../assets/sprites/blank.png',
+        sprite2: '../../assets/sprites/politoed.png',
+        // // list../../../assets/list-icon.png',
         list: '../../assets/sprites/blank.png',
         type: 'dark',
         deck: [
             {
-                cardImg: '../../assets/cards/2004-2006',
+                cardImg: '../../assets/cards/2008-2010',
                 numImg: '../../assets/04.png'
             },
         ]
     },
-]
-
-let fiveEvent = [
     {
-        firstName: 'eeee',
-        sprite1: '../../assets/sprites',
-        sprite2: '../../assets/sprites',
+        firstName: 'Adam Hawkins',
+        sprite1: '../../assets/sprites/blank.png',
+        sprite2: '../../assets/sprites/flygon.png',
+        list: '../../assets/list-icon.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../assets/cards/2007-2008/flygon-ex.png',
+                numImg: '../../assets/03.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/vibrava-grass.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/vibrava-psychic.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/trapinch-grass.png',
+                numImg: '../../assets/04.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/nidoqueen-metal.png',
+                numImg: '../../assets/02.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/nidoran-metal.png',
+                numImg: '../../assets/02.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/magcargo.png',
+                numImg: '../../assets/02.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/slugma.png',
+                numImg: '../../assets/02.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/castform.png',
+                numImg: '../../assets/04.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/holons-magnemite.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/tv-reporter.png',
+                numImg: '../../assets/03.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/holon-mentor.png',
+                numImg: '../../assets/02.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/holon-scientist.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/holon-adventurer.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/holons-farmer.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/holon-researcher.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/holon-transceiver.png',
+                numImg: '../../assets/04.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/rare-candy-2006.png',
+                numImg: '../../assets/04.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/windstorm.png',
+                numImg: '../../assets/03.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/pluspower.png',
+                numImg: '../../assets/02.png'
+            },
+            {
+                cardImg: '../../assets/cards/2004-2006/warp-point.png',
+                numImg: '../../assets/02.png'
+            },
+            {
+                cardImg: '../../assets/cards/2004-2006/switch.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/2007-2008/giant-stump.png',
+                numImg: '../../assets/02.png'
+            },
+            {
+                cardImg: '../../assets/cards/old-energy/lightning-energy-holon-phantoms.png',
+                numImg: '../../assets/01.png'
+            },
+            {
+                cardImg: '../../assets/cards/old-energy/boost-energy.png',
+                numImg: '../../assets/04.png'
+            },
+            {
+                cardImg: '../../assets/cards/old-energy/rainbow-energy-2006.png',
+                numImg: '../../assets/03.png'
+            },
+            {
+                cardImg: '../../assets/cards/old-energy/react-energy.png',
+                numImg: '../../assets/03.png'
+            },
+        ]
+    },
+    {
+        firstName: 'Christopher Fotheringham',
+        sprite1: '../../assets/sprites/blank.png',
+        sprite2: '../../assets/hyphen.png',
+        // // list../../../assets/list-icon.png',
+        list: '../../assets/sprites/blank.png',
+        type: 'dark',
+        deck: [
+            {
+                cardImg: '../../assets/cards/2008-2010',
+                numImg: '../../assets/04.png'
+            },
+        ]
+    },
+    {
+        firstName: 'David Wyatt',
+        sprite1: '../../assets/sprites/blank.png',
+        sprite2: '../../assets/hyphen.png',
         // // list../../../assets/list-icon.png',
         list: '../../assets/sprites/blank.png',
         type: 'dark',
@@ -960,146 +1087,6 @@ function displayList3(array = []) {
     }
 }
 
-// four
-function displayList4(array = []) {
-    fourList.innerHTML = '';
-
-    for (let i = 0; i < array.length; i++) {
-        let item = array[i];
-
-        let item_element = document.createElement('li');
-        item_element.classList.add('player-list-hover');
-        let title = document.createElement('li');
-        title.classList.add('results-list-item');
-
-        let playerName = document.createElement('div');
-        playerName.classList.add('name-n-flag');
-
-        let deck = document.createElement('div');
-        deck.classList.add('player-deck-icons');
-
-        let firstSprite = document.createElement('img');
-        firstSprite.classList.add('sprite');
-        firstSprite.setAttribute('src', item.sprite1);
-        let secondSprite = document.createElement('img');
-        secondSprite.classList.add('sprite');
-        secondSprite.classList.add('second-sprite');
-        secondSprite.setAttribute('src', item.sprite2);
-        let listIcon = document.createElement('img');
-        listIcon.classList.add('list-icon');
-        listIcon.setAttribute('src', item.list);
-
-        item_element.appendChild(title);
-        title.appendChild(playerName);
-        playerName.append(item.firstName);
-        title.appendChild(deck);
-        deck.appendChild(firstSprite);
-        deck.appendChild(secondSprite);
-        deck.appendChild(listIcon);
-
-        if (item.list === '../../assets/list-icon.png') {
-
-            item_element.addEventListener('click', function () {
-
-                document.querySelector('#modal-section').style.display = "flex";
-                document.querySelector('.modal').style.display = 'block';
-                document.querySelector('.behind-modal').style.display = 'block';
-                document.querySelector('.playerName').innerHTML = item.firstName + " - 2007 Germany Nats";
-
-                if (item.type == 'psychic') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/psychic-bg.png)";
-                } else if (item.type == 'fire') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/fire-bg.png)";
-                } else if (item.type == 'fairy') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/fairy-bg.png)";
-                } else if (item.type == 'normal') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/normal-bg.png)";
-                } else if (item.type == 'lightning') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/lightning-bg.png)";
-                } else if (item.type == 'dark') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/dark-bg.png)";
-                } else if (item.type == 'dragon') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/dragon-bg.png)";
-                } else if (item.type == 'fighting') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/fighting-bg.png)";
-                } else if (item.type == 'grass') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/grass-bg.png)";
-                } else if (item.type == 'metal') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/metal-bg.png)";
-                } else if (item.type == 'water') {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/water-bg.png)";
-                } else {
-                    document.querySelector('.modal').style.backgroundImage = "url(../../../assets/dark-bg.png)";
-                }
-
-
-                for (let i = 0; i < item.deck.length; i++) {
-                    let deck = item.deck[i];
-
-                    let cardSpace = document.createElement('div');
-                    cardSpace.classList.add('pokemon-card');
-
-                    let pokeCard = document.createElement('img');
-                    pokeCard.classList.add('pok-card-small')
-                    pokeCard.setAttribute('src', deck.cardImg);
-
-                    if (deck.oldCard === true) {
-                        pokeCard.classList.add('old-card')
-                    }
-                    if (deck.oldCard === true) {
-                        pokeCard.classList.add('radius-card')
-                    }
-                    if (deck.exCard === true) {
-                        pokeCard.classList.add('ex-era-card')
-                    }
-                    if (deck.radiusCard === true) {
-                        pokeCard.classList.add('radius-card')
-                    }
-
-                    let numberCounter = document.createElement('img');
-                    numberCounter.classList.add('num-1')
-                    numberCounter.setAttribute('src', deck.numImg);
-
-                    cardSpace.appendChild(pokeCard);
-                    cardSpace.appendChild(numberCounter);
-                    modalBottom.appendChild(cardSpace);
-
-                    modalX.addEventListener('click', () => {
-                        modal.style.display = 'none';
-                        behind.style.display = 'none';
-                        cardSpace.remove();
-                    })
-                    behind.addEventListener('click', () => {
-                        modal.style.display = 'none';
-                        behind.style.display = 'none';
-                        cardSpace.remove();
-                    })
-                }
-                document.querySelectorAll('.pokemon-card').forEach(card => {
-                    let meep = document.getElementsByClassName("pok-card-small"),
-                    zoombox = document.getElementById("zoom-card-box");
-                   
-                    if (meep.length > 0) { for (let i of meep) {
-                      i.onclick = () => {
-                        let clone = i.cloneNode();
-                        clone.className = "";
-                        zoombox.innerHTML = "";
-                        zoombox.appendChild(clone);
-                        zoombox.className = "show";
-                      };
-                    }}
-                   
-                    zoombox.onclick = () => {
-                      zoombox.className = "";
-                    };
-                  });
-            })
-        }
-
-        fourList.appendChild(item_element);
-    }
-}
-
 // five
 function displayList5(array = []) {
     fiveList.innerHTML = '';
@@ -1144,7 +1131,7 @@ function displayList5(array = []) {
                 document.querySelector('#modal-section').style.display = "flex";
                 document.querySelector('.modal').style.display = 'block';
                 document.querySelector('.behind-modal').style.display = 'block';
-                document.querySelector('.playerName').innerHTML = item.firstName + " - 2011 France Nats";
+                document.querySelector('.playerName').innerHTML = item.firstName + " - 2006 UK Nats";
 
                 if (item.type == 'psychic') {
                     document.querySelector('.modal').style.backgroundImage = "url(../../../assets/psychic-bg.png)";
@@ -1807,7 +1794,6 @@ const behind = document.querySelector('.behind-modal');
 displayList(masters);
 displayList2(seniors);
 displayList3(juniors);
-displayList4(fourEvent);
 displayList5(fiveEvent);
 displayList6(sixEvent);
 displayList7(sevenEvent);
