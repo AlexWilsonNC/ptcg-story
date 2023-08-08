e5fe1034a7163064a26a569421ee7b754ccf631b
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-08-08 17:19:00 -0400

    archive

---
+++ darkmode/darkmode-old-home.js
@@ -0,0 +1,64 @@
+let darkMode = localStorage.getItem('darkMode');
+
+const toggle = document.getElementById('toggle');
+const topnavr = document.getElementById('top-navr');
+const menu = document.getElementById('menu');
+const menuToggle = document.getElementById('menuToggle');
+const navleftr = document.getElementById('nav-leftr');
+const navrightr = document.getElementById('nav-rightr');
+const dropitdown1 = document.querySelector('.drop-it-down1');
+const dropitdown2 = document.querySelector('.drop-it-down2');
+const creator = document.querySelector('.image-text-creator');
+
+// const worldsbox = document.querySelectorAll(".worlds-box");
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkon');
+    toggle.classList.add('darkon');
+    topnavr.classList.add('darkon');
+    menu.classList.add('darkon');
+    menuToggle.classList.add('darkon');
+    navleftr.classList.add('darkon');
+    navrightr.classList.add('darkon');
+    dropitdown1.classList.add('darkon');
+    dropitdown2.classList.add('darkon');
+    creator.classList.add('darkon');
+
+    // worldsbox.forEach((worldsbox) => {
+    //   worldsbox.classList.add('darkon');
+    // })
+
+    localStorage.setItem('darkMode', 'enabled');
+  };
+  
+  const disableDarkMode = () => {
+    document.body.classList.remove('darkon');
+    toggle.classList.remove('darkon');
+    topnavr.classList.remove('darkon');
+    menu.classList.remove('darkon');
+    menuToggle.classList.remove('darkon');
+    navleftr.classList.remove('darkon');
+    navrightr.classList.remove('darkon');
+    dropitdown1.classList.remove('darkon');
+    dropitdown2.classList.remove('darkon');
+    creator.classList.remove('darkon');
+
+    // worldsbox.forEach((worldsbox) => {
+    //   worldsbox.classList.remove('darkon');
+    // })
+
+    localStorage.setItem('darkMode', null);
+  };
+  
+  if(darkMode === 'enabled') {
+    enableDarkMode();
+  }
+  
+  toggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'enabled') {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
+    }
+  });
