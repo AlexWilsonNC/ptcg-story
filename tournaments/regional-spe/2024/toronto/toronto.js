fdaca02ae16ed56cbba8c73731f37159f305e41c
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-10-29 15:55:13 -0400

    toronto top 16

--- tournaments/regional-spe/2024/toronto/toronto.js
+++ tournaments/regional-spe/2024/toronto/toronto.js
@@ -319,35 +319,43 @@ let masters = [
         flag: '../../../../assets/flags/usa.png',
         sprite1: '../../../../assets/sprites/lugia.png',
         sprite2: '../../../../assets/sprites/archeops.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
+        list: '../../../../assets/list-icon.png',
         "event": "2024 Toronto Regionals",
-        // "decklist": {
-        //     "pokemon": [
-        //         {
-        //             "count": 0,
-        //             "name": "eee",
-        //             "number": "eee",
-        //             "set": "eee"
-        //         },
-        //     ],
-        //     "trainer": [
-        //         {
-        //             "count": 0,
-        //             "name": "eee",
-        //             "number": "eee",
-        //             "set": "eee"
-        //         },
-        //     ],
-        //     "energy": [
-        //         {
-        //             "count": 0,
-        //             "name": "eee",
-        //             "number": "eee",
-        //             "set": "eee"
-        //         },
-        //     ]
-        // }
+        "decklist":
+        {
+            "pokemon": [
+                { "count": 3, "name": "Lugia VSTAR", "number": "139", "set": "SIT" },
+                { "count": 4, "name": "Lugia V", "number": "138", "set": "SIT" },
+                { "count": 4, "name": "Archeops", "number": "147", "set": "SIT" },
+                { "count": 2, "name": "Tyranitar V", "number": "109", "set": "PR-SW" },
+                { "count": 2, "name": "Lumineon V", "number": "250", "set": "PR-SW" },
+                { "count": 1, "name": "Yveltal", "number": "175", "set": "FST" },
+                { "count": 1, "name": "Stonjourner", "number": "84", "set": "BST" },
+                { "count": 1, "name": "Radiant Tsareena", "number": "16", "set": "SIT" },
+                { "count": 1, "name": "Pumpkaboo", "number": "76", "set": "EVS" },
+            ],
+            "trainer":
+                [
+                    { "count": 4, "name": "Professor's Research", "number": "189", "set": "SVI" },
+                    { "count": 3, "name": "Iono", "number": "185", "set": "PAL" },
+                    { "count": 3, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                    { "count": 1, "name": "Professor Burnet", "number": "167", "set": "PR-SW" },
+                    { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+                    { "count": 4, "name": "Capturing Aroma", "number": "153", "set": "SIT" },
+                    { "count": 3, "name": "Urn of Vitality", "number": "139", "set": "BST" },
+                    { "count": 2, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                    { "count": 2, "name": "Mesagoza", "number": "178", "set": "SVI" },
+                    { "count": 2, "name": "Collapsed Stadium", "number": "137", "set": "BRS" },
+                ],
+            "energy":
+                [
+                    { "count": 4, "name": "Single Strike Energy - Special", "number": "141", "set": "BST" },
+                    { "count": 3, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" },
+                    { "count": 3, "name": "Gift Energy - Special", "number": "171", "set": "LOR" },
+                    { "count": 2, "name": "V Guard Energy - Special", "number": "169", "set": "SIT" },
+                    { "count": 1, "name": "Jet Energy - Special", "number": "190", "set": "PAL" },
+                ]
+        },
     },
     {
         firstName: "Joshua Frink",
