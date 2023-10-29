8fb92ab8fb83970f73488a48860f3dff373dd548
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-10-29 15:30:52 -0400

    upcoming formats

--- tournaments/regional-spe/2024/toronto/toronto.js
+++ tournaments/regional-spe/2024/toronto/toronto.js
@@ -280,10 +280,10 @@ let masters = [
         // }
     },
     {
-        firstName: "--",
-        flag: '../../../../assets/flags/unknown.png',
+        firstName: "Christian LaBella",
+        flag: '../../../../assets/flags/canada.png',
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/arceus.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "event": "2024 Toronto Regionals",
@@ -315,10 +315,10 @@ let masters = [
         // }
     },
     {
-        firstName: "--",
-        flag: '../../../../assets/flags/unknown.png',
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        firstName: "Gabriel Smart",
+        flag: '../../../../assets/flags/usa.png',
+        sprite1: '../../../../assets/sprites/lugia.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "event": "2024 Toronto Regionals",
@@ -350,10 +350,10 @@ let masters = [
         // }
     },
     {
-        firstName: "--",
-        flag: '../../../../assets/flags/unknown.png',
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        firstName: "Joshua Frink",
+        flag: '../../../../assets/flags/usa.png',
+        sprite1: '../../../../assets/sprites/inteleon-vmax.png',
+        sprite2: '../../../../assets/sprites/urshifu-rapid-strike-gmax.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "event": "2024 Toronto Regionals",
@@ -385,10 +385,10 @@ let masters = [
         // }
     },
     {
-        firstName: "--",
-        flag: '../../../../assets/flags/unknown.png',
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        firstName: "Andrew Hedrick",
+        flag: '../../../../assets/flags/usa.png',
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/sableye.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "event": "2024 Toronto Regionals",
@@ -420,10 +420,10 @@ let masters = [
         // }
     },
     {
-        firstName: "--",
-        flag: '../../../../assets/flags/unknown.png',
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        firstName: "Lucas Xing",
+        flag: '../../../../assets/flags/canada.png',
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "event": "2024 Toronto Regionals",
@@ -455,10 +455,10 @@ let masters = [
         // }
     },
     {
-        firstName: "--",
-        flag: '../../../../assets/flags/unknown.png',
+        firstName: "Brett Gorneyus",
+        flag: '../../../../assets/flags/usa.png',
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/charizard.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "event": "2024 Toronto Regionals",
@@ -490,10 +490,10 @@ let masters = [
         // }
     },
     {
-        firstName: "--",
-        flag: '../../../../assets/flags/unknown.png',
+        firstName: "Iann Robb",
+        flag: '../../../../assets/flags/usa.png',
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "event": "2024 Toronto Regionals",
@@ -525,10 +525,10 @@ let masters = [
         // }
     },
     {
-        firstName: "--",
-        flag: '../../../../assets/flags/unknown.png',
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        firstName: "Calvin Connor",
+        flag: '../../../../assets/flags/usa.png',
+        sprite1: '../../../../assets/sprites/lugia.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "event": "2024 Toronto Regionals",
