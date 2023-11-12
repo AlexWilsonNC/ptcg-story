79c042c932606702ffc2cc14bbcce015c8ded720
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-11-12 11:37:52 -0500

    thailand

---
+++ tournaments/regional-spe/2024/thailand-regional-league1/thai.js
@@ -0,0 +1,275 @@
+let masters = [
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/inteleon-vmax.png",
+      sprite2: "../../../../assets/sprites/urshifu-rapid-strike-gmax.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/giratina-origin.png",
+      sprite2: "../../../../assets/sprites/comfey.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/mew-vmax.png",
+      sprite2: "../../../../assets/sprites/genesect.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/sableye.png",
+      sprite2: "../../../../assets/sprites/comfey.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/giratina-origin.png",
+      sprite2: "../../../../assets/sprites/comfey.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/gholdengo.png",
+     // list: '../../../../assets/list-icon.png',
+     list: '../../../../assets/sprites/blank.png',
+     "decklist": { 
+     "pokemon": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ], "trainer": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ], "energy": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/charizard.png",
+      sprite2: "../../../../assets/sprites/pidgeot.png",
+     // list: '../../../../assets/list-icon.png',
+     list: '../../../../assets/sprites/blank.png',
+     "decklist": { 
+     "pokemon": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ], "trainer": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ], "energy": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/lugia.png",
+      sprite2: "../../../../assets/sprites/archeops.png",
+     // list: '../../../../assets/list-icon.png',
+     list: '../../../../assets/sprites/blank.png',
+     "decklist": { 
+     "pokemon": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ], "trainer": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ], "energy": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/blank.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/blank.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/blank.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/blank.png",
+     // list: '../../../../assets/list-icon.png',
+     list: '../../../../assets/sprites/blank.png',
+     "decklist": { 
+     "pokemon": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ], "trainer": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ], "energy": [
+       { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+     ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/blank.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/blank.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/blank.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+    {
+      "firstName": "--",
+      flag: '../../../../assets/sprites/blank.png',
+      "event": "2024 Thailand Regional League #1",
+      sprite1: "../../../../assets/sprites/blank.png",
+      sprite2: "../../../../assets/sprites/blank.png",
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": { 
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]}
+    },
+  ]
+  
