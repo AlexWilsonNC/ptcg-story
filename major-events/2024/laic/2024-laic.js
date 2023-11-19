f7dfbefbbf5c81808b01d261a0bf1025b8f5c6b6
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-11-19 12:18:05 -0500

    24 laic

--- major-events/2024/laic/2024-laic.js
+++ major-events/2024/laic/2024-laic.js
@@ -1,9 +1,9 @@
 let masters = [
     {
-        firstName: '--',
-        flag: '../../../assets/flags/unknown.png',
-        sprite1: '../../../assets/sprites/blank.png',
-        sprite2: '../../../assets/sprites/blank.png',
+        firstName: 'Juho Kallama',
+        flag: '../../../assets/flags/finland.png',
+        sprite1: '../../../assets/sprites/miraidon.png',
+        sprite2: '../../../assets/sprites/flaaffy.png',
         // list: '../../../assets/list-icon.png',
         list: '../../../assets/sprites/blank.png',
         "event": "2024 Latin America Internationals",
@@ -41,21 +41,40 @@ let masters = [
         },
     },
     {
-        firstName: '--',
-        flag: '../../../assets/flags/unknown.png',
-        sprite1: '../../../assets/sprites/blank.png',
-        sprite2: '../../../assets/sprites/blank.png',
-        // list: '../../../assets/list-icon.png',
-        list: '../../../assets/sprites/blank.png',
+        firstName: 'Noah Sawyer',
+        flag: '../../../assets/flags/usa.png',
+        sprite1: '../../../assets/sprites/entei.png',
+        sprite2: '../../../assets/sprites/iron-valiant.png',
+        list: '../../../assets/list-icon.png',
         "event": "2024 Latin America Internationals",
         "decklist":
         {
             "pokemon": [
-                { "count": 4, "name": "eee", "number": "eee", "set": "eee" },
+                { "count": 4, "name": "Entei V", "number": "22", "set": "BRS" },
+                { "count": 4, "name": "Iron Valiant ex", "number": "89", "set": "PAR" },
+                { "count": 1, "name": "Medicham V", "number": "83", "set": "EVS" },
+                { "count": 1, "name": "Radiant Charizard", "number": "20", "set": "CRZ" },
+                { "count": 1, "name": "Squawkabilly ex", "number": "169", "set": "PAL" }
             ], "trainer": [
-                { "count": 4, "name": "eee", "number": "eee", "set": "eee" },
+                { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+                { "count": 3, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+                { "count": 2, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Switch Cart", "number": "154", "set": "ASR" },
+                { "count": 4, "name": "Escape Rope", "number": "125", "set": "BST" },
+                { "count": 4, "name": "Switch", "number": "194", "set": "SVI" },
+                { "count": 3, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 2, "name": "Future Booster Energy Capsule", "number": "164", "set": "PAR" },
+                { "count": 1, "name": "Earthen Vessel", "number": "163", "set": "PAR" },
+                { "count": 1, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 2, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 1, "name": "Bravery Charm", "number": "173", "set": "PAL" },
+                { "count": 1, "name": "Technical Machine: Devolution", "number": "177", "set": "PAR" },
+                { "count": 4, "name": "Magma Basin", "number": "144", "set": "BRS" },
             ], "energy": [
-                { "count": 4, "name": "eee", "number": "eee", "set": "eee" },
+                { "count": 5, "name": "Fire Energy - Basic", "number": "232", "set": "CRE" },
+                { "count": 2, "name": "Jet Energy - Special", "number": "190", "set": "PAL" },
+                { "count": 2, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" }
             ]
         },
     },
