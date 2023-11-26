d3120d1e27d761adb0d9e5affed51d1ebfda996e
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-11-26 11:02:33 -0500

    brisbane

--- tournaments/regional-spe/2024/gdansk/gdan.js
+++ tournaments/regional-spe/2024/gdansk/gdan.js
@@ -72,11 +72,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/sprites/blank.png',
+        "firstName": "Tord Reklev",
+        flag: '../../../../assets/flags/norway.png',
         "event": "2024 Gdańsk Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/roaring-moon.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -90,11 +90,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/sprites/blank.png',
+        "firstName": "Thomas Elliot Jones",
+        flag: '../../../../assets/flags/uk.png',
         "event": "2024 Gdańsk Regionals",
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/charizard.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -108,11 +108,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/sprites/blank.png',
+        "firstName": "Jamie Hindley",
+        flag: '../../../../assets/flags/uk.png',
         "event": "2024 Gdańsk Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        sprite1: '../../../../assets/sprites/mew-vmax.png',
+        sprite2: '../../../../assets/sprites/genesect.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -126,11 +126,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/sprites/blank.png',
+        "firstName": "Ewan West",
+        flag: '../../../../assets/flags/uk.png',
         "event": "2024 Gdańsk Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        sprite1: '../../../../assets/sprites/mew-vmax.png',
+        sprite2: '../../../../assets/sprites/genesect.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -185,15 +185,38 @@ let masters = [
         "event": "2024 Gdańsk Regionals",
         sprite1: '../../../../assets/sprites/inteleon-vmax.png',
         sprite2: '../../../../assets/sprites/urshifu-rapid-strike-gmax.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
+        list: '../../../../assets/list-icon.png',
         "decklist": {
             "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+                { "count": 3, "name": "Inteleon V", "number": "78", "set": "FST" },
+                { "count": 2, "name": "Rapid Strike Urshifu VMAX", "number": "88", "set": "BST" },
+                { "count": 2, "name": "Rapid Strike Urshifu V", "number": "87", "set": "BST" },
+                { "count": 2, "name": "Octillery", "number": "37", "set": "BST" },
+                { "count": 2, "name": "Remoraid", "number": "36", "set": "BST" },
+                { "count": 1, "name": "Medicham V", "number": "83", "set": "EVS" },
+                { "count": 1, "name": "Radiant Alakazam", "number": "59", "set": "SIT" },
+                { "count": 1, "name": "Squawkabilly ex", "number": "169", "set": "PAL" },
+                { "count": 1, "name": "Spiritomb", "number": "89", "set": "PAL" },
             ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 3, "name": "Professor's Research", "number": "190", "set": "SVI" },
+                { "count": 2, "name": "Klara", "number": "145", "set": "CRE" },
+                { "count": 2, "name": "Melony", "number": "146", "set": "CRE" },
+                { "count": 2, "name": "Korrina's Focus", "number": "128", "set": "BST" },
+                { "count": 2, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 1, "name": "Arven", "number": "186", "set": "OBF" },
+                { "count": 1, "name": "Professor Turo's Scenario", "number": "171", "set": "PAR" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+                { "count": 2, "name": "Energy Retrieval", "number": "171", "set": "SVI" },
+                { "count": 2, "name": "Earthen Vessel", "number": "163", "set": "PAR" },
+                { "count": 1, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 2, "name": "Technical Machine: Devolution", "number": "177", "set": "PAR" },
+                { "count": 2, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 3, "name": "Tower of Waters", "number": "138", "set": "BST" },
             ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 5, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+                { "count": 4, "name": "Rapid Strike Energy - Special", "number": "140", "set": "BST" },
             ]
         }
     },
