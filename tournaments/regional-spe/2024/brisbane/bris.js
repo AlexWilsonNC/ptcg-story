a75a63e958b913c64aa6069e3d35445055bfaba1
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-11-26 00:07:48 -0500

    brisbane

---
+++ tournaments/regional-spe/2024/brisbane/bris.js
@@ -0,0 +1,326 @@
+let masters = [
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "James Faulkner",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Kaiwen Cabbabe",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/entei.png',
+        sprite2: '../../../../assets/sprites/iron-valiant.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "Entei V", "number": "22", "set": "BRS" },
+                { "count": 4, "name": "Iron Valiant ex", "number": "89", "set": "PAR" },
+                { "count": 1, "name": "Medicham V", "number": "83", "set": "EVS" },
+                { "count": 1, "name": "Radiant Charizard", "number": "20", "set": "CRZ" },
+                { "count": 1, "name": "Squawkabilly ex", "number": "169", "set": "PAL" }
+            ], "trainer": [
+                { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+                { "count": 2, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+                { "count": 2, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 2, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Switch Cart", "number": "154", "set": "ASR" },
+                { "count": 4, "name": "Escape Rope", "number": "125", "set": "BST" },
+                { "count": 4, "name": "Switch", "number": "194", "set": "SVI" },
+                { "count": 3, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 2, "name": "Earthen Vessel", "number": "163", "set": "PAR" },
+                { "count": 1, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 2, "name": "Future Booster Energy Capsule", "number": "164", "set": "PAR" },
+                { "count": 2, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 1, "name": "Technical Machine: Devolution", "number": "177", "set": "PAR" },
+                { "count": 4, "name": "Magma Basin", "number": "144", "set": "BRS" },
+            ], "energy": [
+                { "count": 5, "name": "Fire Energy - Basic", "number": "232", "set": "CRE" },
+                { "count": 3, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" }
+            ]
+        }
+    },
+    {
+        "firstName": "Jiawel Sun",
+        flag: '../../../../assets/flags/new-zealand.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Yu Xiang",
+        flag: '../../../../assets/flags/new-zealand.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Joel Suryadi",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Callum Walker",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Tim Franklin",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/entei.png',
+        sprite2: '../../../../assets/sprites/iron-valiant.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "Entei V", "number": "22", "set": "BRS" },
+                { "count": 4, "name": "Iron Valiant ex", "number": "89", "set": "PAR" },
+                { "count": 1, "name": "Medicham V", "number": "83", "set": "EVS" },
+                { "count": 1, "name": "Radiant Charizard", "number": "20", "set": "CRZ" },
+                { "count": 1, "name": "Squawkabilly ex", "number": "169", "set": "PAL" }
+            ], "trainer": [
+                { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+                { "count": 2, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+                { "count": 2, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 2, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Switch Cart", "number": "154", "set": "ASR" },
+                { "count": 4, "name": "Escape Rope", "number": "125", "set": "BST" },
+                { "count": 4, "name": "Switch", "number": "194", "set": "SVI" },
+                { "count": 3, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 2, "name": "Earthen Vessel", "number": "163", "set": "PAR" },
+                { "count": 1, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 2, "name": "Future Booster Energy Capsule", "number": "164", "set": "PAR" },
+                { "count": 2, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 1, "name": "Technical Machine: Devolution", "number": "177", "set": "PAR" },
+                { "count": 4, "name": "Magma Basin", "number": "144", "set": "BRS" },
+            ], "energy": [
+                { "count": 5, "name": "Fire Energy - Basic", "number": "232", "set": "CRE" },
+                { "count": 3, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" }
+            ]
+        }
+    },
+    {
+        "firstName": "David Frans Daritan",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
