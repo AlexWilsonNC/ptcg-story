edea45728707fe7b0e1a9ee36dfb04cedcdc49aa
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-11-26 01:21:48 -0500

    brisbane

--- tournaments/regional-spe/2024/brisbane/bris.js
+++ tournaments/regional-spe/2024/brisbane/bris.js
@@ -72,11 +72,101 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/sprites/blank.png',
+        "firstName": "Calvin Connor",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Brisbane Regionals",
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 2, "name": "Gardevoir ex", "number": "86", "set": "SVI" },
+                { "count": 2, "name": "Gardevoir", "number": "61", "set": "CRE" },
+                { "count": 3, "name": "Kirlia", "number": "68", "set": "SIT" },
+                { "count": 1, "name": "Kirlia", "number": "60", "set": "CRE" },
+                { "count": 3, "name": "Ralts", "number": "60", "set": "ASR" },
+                { "count": 1, "name": "Ralts", "number": "67", "set": "SIT" },
+                { "count": 1, "name": "Scream Tail", "number": "86", "set": "PAR" },
+                { "count": 1, "name": "Zacian V", "number": "16", "set": "CEL" },
+                { "count": 1, "name": "Cresselia", "number": "74", "set": "LOR" },
+                { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" },
+                { "count": 1, "name": "Manaphy", "number": "41", "set": "BRS" },
+                { "count": 1, "name": "Jirachi", "number": "126", "set": "PAR" },
+                { "count": 1, "name": "Lumineon V", "number": "250", "set": "PR-SW" },
+            ], "trainer": [
+                { "count": 3, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 2, "name": "Avery", "number": "130", "set": "CRE" },
+                { "count": 1, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 1, "name": "Professor's Research", "number": "189", "set": "SVI" },
+                { "count": 1, "name": "Professor Turo's Scenario", "number": "171", "set": "PAR" },
+                { "count": 4, "name": "Level Ball", "number": "129", "set": "BST" },
+                { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+                { "count": 2, "name": "Fog Crystal", "number": "140", "set": "CRE" },
+                { "count": 2, "name": "Rare Candy", "number": "191", "set": "SVI" },
+                { "count": 2, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+                { "count": 2, "name": "Super Rod", "number": "188", "set": "PAL" },
+                { "count": 1, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 1, "name": "Pal Pad", "number": "182", "set": "SVI" },
+                { "count": 2, "name": "Artazon", "number": "171", "set": "PAL" },
+                { "count": 1, "name": "Collapsed Stadium", "number": "137", "set": "BRS" },
+            ], "energy": [
+                { "count": 10, "name": "Psychic Energy - Basic", "number": "232", "set": "CRE" },
+                { "count": 2, "name": "Reversal Energy - Special", "number": "192", "set": "PAL" },
+            ]
+        }
+    },
+    {
+        "firstName": "Samantha Turner",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/snorlax.png',
+        sprite2: '../../../../assets/sprites/mimikyu.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "Snorlax", "number": "55", "set": "PGO" },
+                { "count": 2, "name": "Mimikyu", "number": "97", "set": "PAL" },
+                { "count": 1, "name": "Rotom V", "number": "58", "set": "LOR" },
+                { "count": 1, "name": "Pidgeot V", "number": "137", "set": "LOR" },
+                { "count": 1, "name": "Crabominable V", "number": "76", "set": "FST" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "Arven", "number": "186", "set": "OBF" },
+                { "count": 3, "name": "Miss Fortune Sisters", "number": "164", "set": "LOR" },
+                { "count": 3, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 3, "name": "Penny", "number": "183", "set": "SVI" },
+                { "count": 2, "name": "Erika's Invitation", "number": "160", "set": "MEW" },
+                { "count": 1, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 1, "name": "Avery", "number": "130", "set": "CRE" },
+                { "count": 1, "name": "Giacomo", "number": "182", "set": "PAL" },
+                { "count": 1, "name": "Cyllene", "number": "138", "set": "ASR" },
+                { "count": 1, "name": "Peonia", "number": "149", "set": "CRE" },
+                { "count": 1, "name": "Sidney", "number": "241", "set": "FST" },
+                { "count": 1, "name": "Team Yell's Cheer", "number": "149", "set": "BRS" },
+                { "count": 4, "name": "Pokegear 3.0", "number": "186", "set": "SVI" },
+                { "count": 4, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 4, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+                { "count": 2, "name": "Pal Pad", "number": "172", "set": "SSH" },
+                { "count": 1, "name": "Switch Cart", "number": "154", "set": "ASR" },
+                { "count": 1, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 1, "name": "Echoing Horn", "number": "136", "set": "CRE" },
+                { "count": 1, "name": "Hisuian Heavy Ball", "number": "146", "set": "ASR" },
+                { "count": 1, "name": "Super Rod", "number": "188", "set": "PAL" },
+                { "count": 3, "name": "Bravery Charm", "number": "173", "set": "PAL" },
+                { "count": 2, "name": "Luxurious Cape", "number": "166", "set": "PAR" },
+                { "count": 1, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 3, "name": "Temple of Sinnoh", "number": "155", "set": "ASR" },
+            ], "energy": [
+                { "count": 1, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+            ]
+        }
+    },
+    {
+        "firstName": "James Williams",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2024 Brisbane Regionals",
+        sprite1: '../../../../assets/sprites/snorlax.png',
+        sprite2: '../../../../assets/sprites/mimikyu.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -90,56 +180,46 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/sprites/blank.png',
+        "firstName": "Gabriel Smart",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Brisbane Regionals",
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        list: '../../../../assets/list-icon.png',
         "decklist": {
             "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 2, "name": "Gardevoir ex", "number": "86", "set": "SVI" },
+                { "count": 2, "name": "Gardevoir", "number": "61", "set": "CRE" },
+                { "count": 3, "name": "Kirlia", "number": "68", "set": "SIT" },
+                { "count": 1, "name": "Kirlia", "number": "60", "set": "CRE" },
+                { "count": 3, "name": "Ralts", "number": "60", "set": "ASR" },
+                { "count": 1, "name": "Ralts", "number": "67", "set": "SIT" },
+                { "count": 1, "name": "Scream Tail", "number": "86", "set": "PAR" },
+                { "count": 1, "name": "Zacian V", "number": "16", "set": "CEL" },
+                { "count": 1, "name": "Cresselia", "number": "74", "set": "LOR" },
+                { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" },
+                { "count": 1, "name": "Manaphy", "number": "41", "set": "BRS" },
+                { "count": 1, "name": "Jirachi", "number": "126", "set": "PAR" },
+                { "count": 1, "name": "Lumineon V", "number": "250", "set": "PR-SW" },
             ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 3, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 2, "name": "Avery", "number": "130", "set": "CRE" },
+                { "count": 1, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 1, "name": "Professor's Research", "number": "189", "set": "SVI" },
+                { "count": 1, "name": "Professor Turo's Scenario", "number": "171", "set": "PAR" },
+                { "count": 4, "name": "Level Ball", "number": "129", "set": "BST" },
+                { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+                { "count": 2, "name": "Fog Crystal", "number": "140", "set": "CRE" },
+                { "count": 2, "name": "Rare Candy", "number": "191", "set": "SVI" },
+                { "count": 2, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+                { "count": 2, "name": "Super Rod", "number": "188", "set": "PAL" },
+                { "count": 1, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 1, "name": "Pal Pad", "number": "182", "set": "SVI" },
+                { "count": 2, "name": "Artazon", "number": "171", "set": "PAL" },
+                { "count": 1, "name": "Collapsed Stadium", "number": "137", "set": "BRS" },
             ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ]
-        }
-    },
-    {
-        "firstName": "--",
-        flag: '../../../../assets/sprites/blank.png',
-        "event": "2024 Brisbane Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
-        "decklist": {
-            "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ]
-        }
-    },
-    {
-        "firstName": "--",
-        flag: '../../../../assets/sprites/blank.png',
-        "event": "2024 Brisbane Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/blank.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
-        "decklist": {
-            "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 10, "name": "Psychic Energy - Basic", "number": "232", "set": "CRE" },
+                { "count": 2, "name": "Reversal Energy - Special", "number": "192", "set": "PAL" },
             ]
         }
     },
