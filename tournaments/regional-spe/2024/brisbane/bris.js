a6e145d646a49d6a32d4c9a0332b4b002c659bf6
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-11-27 09:55:26 -0500

    2010

--- tournaments/regional-spe/2024/brisbane/bris.js
+++ tournaments/regional-spe/2024/brisbane/bris.js
@@ -110,15 +110,41 @@ let masters = [
         "event": "2024 Brisbane Regionals",
         sprite1: '../../../../assets/sprites/mew-vmax.png',
         sprite2: '../../../../assets/sprites/genesect.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
+        list: '../../../../assets/list-icon.png',
         "decklist": {
             "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 3, "name": "Mew VMAX", "number": "114", "set": "FST" },
+                { "count": 4, "name": "Mew V", "number": "113", "set": "FST" },
+                { "count": 4, "name": "Genesect V", "number": "185", "set": "FST" },
+                { "count": 1, "name": "Meloetta", "number": "124", "set": "FST" },
+                { "count": 1, "name": "Eiscue", "number": "44", "set": "BRS" },
+            ],
+            "trainer": [
+                { "count": 2, "name": "Elesa's Sparkle", "number": "233", "set": "FST" },
+                { "count": 2, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 1, "name": "Judge", "number": "176", "set": "SVI" },
+                { "count": 1, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 1, "name": "Professor Turo's Scenario", "number": "171", "set": "PAR" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+                { "count": 4, "name": "Cram-o-matic", "number": "229", "set": "FST" },
+                { "count": 4, "name": "Power Tablet", "number": "236", "set": "FST" },
+                { "count": 3, "name": "Grabber", "number": "162", "set": "MEW" },
+                { "count": 2, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 1, "name": "Escape Rope", "number": "125", "set": "BST" },
+                { "count": 1, "name": "Switch Cart", "number": "154", "set": "ASR" },
+                { "count": 1, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 1, "name": "Pal Pad", "number": "182", "set": "SVI" },
+                { "count": 3, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 1, "name": "Box of Disaster", "number": "154", "set": "LOR" },
+                { "count": 1, "name": "Choice Belt", "number": "176", "set": "PAL" },
+                { "count": 1, "name": "Path to the Peak", "number": "148", "set": "CRE" },
+                { "count": 1, "name": "Town Store", "number": "196", "set": "OBF" },
+                { "count": 1, "name": "Lost City", "number": "161", "set": "LOR" },
+            ],
+            "energy": [
+                { "count": 4, "name": "Fusion Strike Energy", "number": "244", "set": "FST" },
+                { "count": 3, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" }
             ]
         }
     },
