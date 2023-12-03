e52e626e77e8aea8e4056dc83567416793952805
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-12-03 13:28:09 -0500

    day2

---
+++ tournaments/regional-spe/2024/malaysia-regional-league2/malay.js
@@ -0,0 +1,103 @@
+let masters = [
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Malaysia Regional League #2",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "Giratina VSTAR", "number": "131", "set": "LOR" },
+                { "count": 3, "name": "Giratina V", "number": "130", "set": "LOR" },
+                { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+                { "count": 1, "name": "Roaring Moon ex", "number": "124", "set": "PAR" },
+                { "count": 1, "name": "Cramorant", "number": "50", "set": "LOR" },
+                { "count": 1, "name": "Sableye", "number": "70", "set": "LOR" },
+                { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" },
+                { "count": 1, "name": "Manaphy", "number": "41", "set": "BRS" },
+            ], "trainer": [
+                { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+                { "count": 2, "name": "Boss's Orders", "number": "132", "set": "BRS" },
+                { "count": 1, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 1, "name": "Roxanne", "number": "150", "set": "ASR" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Mirage Gate", "number": "163", "set": "LOR" },
+                { "count": 3, "name": "Switch Cart", "number": "154", "set": "ASR" },
+                { "count": 3, "name": "Escape Rope", "number": "125", "set": "BST" },
+                { "count": 3, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 2, "name": "Super Rod", "number": "188", "set": "PAL" },
+                { "count": 1, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+                { "count": 3, "name": "Path to the Peak", "number": "148", "set": "CRE" },
+            ], "energy": [
+                { "count": 4, "name": "Psychic Energy - Basic", "number": "232", "set": "CRE" },
+                { "count": 3, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+                { "count": 3, "name": "Darkness Energy - Basic", "number": "152", "set": "CRZ" },
+                { "count": 4, "name": "Jet Energy - Special", "number": "190", "set": "PAL" },
+            ]
+        },
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Malaysia Regional League #2",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Malaysia Regional League #2",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/sprites/blank.png',
+        "event": "2024 Malaysia Regional League #2",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/blank.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
