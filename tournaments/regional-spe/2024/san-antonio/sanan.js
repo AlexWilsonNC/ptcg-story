ac690a2a86f5b5bb8ff917d44506c3420c529e75
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-12-18 10:52:58 -0500

    dort

--- tournaments/regional-spe/2024/san-antonio/sanan.js
+++ tournaments/regional-spe/2024/san-antonio/sanan.js
@@ -49,16 +49,38 @@ let masters = [
         "event": "2024 San Antonio Regionals",
         sprite1: '../../../../assets/sprites/giratina-origin.png',
         sprite2: '../../../../assets/sprites/comfey.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
+        list: '../../../../assets/list-icon.png',
         "decklist": {
             "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ]
+                { "count": 3, "name": "Giratina VSTAR", "number": "131", "set": "LOR" },
+                { "count": 3, "name": "Giratina V", "number": "130", "set": "LOR" }, 
+                { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+                { "count": 2, "name": "Sableye", "number": "70", "set": "LOR" },
+                { "count": 1, "name": "Cramorant", "number": "50", "set": "LOR" },
+                { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" },
+              ],
+                "trainer": [
+                 { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+                 { "count": 2, "name": "Roxanne", "number": "150", "set": "ASR" },
+                  { "count": 1, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                  { "count": 1, "name": "Iono", "number": "185", "set": "PAL" },
+                  { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                  { "count": 4, "name": "Mirage Gate", "number": "163", "set": "LOR" },
+                  { "count": 3, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                  { "count": 2, "name": "Super Rod", "number": "188", "set": "PAL" },
+                  { "count": 2, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+                  { "count": 2, "name": "Switch Cart", "number": "154", "set": "ASR" },
+                  { "count": 2, "name": "Escape Rope", "number": "125", "set": "BST" },
+                  { "count": 1, "name": "Switch", "number": "194", "set": "SVI" },
+                  { "count": 1, "name": "Pokegear 3.0", "number": "186", "set": "SVI" },
+                  { "count": 4, "name": "Path to the Peak", "number": "148", "set": "CRE" },
+                ], 
+                  "energy": [
+                   { "count": 4, "name": "Psychic Energy - Basic", "number": "232", "set": "CRE" },
+                    { "count": 3, "name": "Grass Energy - Basic", "number": "152", "set": "CRZ" },
+                   { "count": 2, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+                    { "count": 4, "name": "Jet Energy - Special", "number": "190", "set": "PAL" },
+                    ]
         }
     },
     {
@@ -365,15 +387,41 @@ let masters = [
         "event": "2024 San Antonio Regionals",
         sprite1: '../../../../assets/sprites/mew-vmax.png',
         sprite2: '../../../../assets/sprites/genesect.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
+        list: '../../../../assets/list-icon.png',
         "decklist": {
             "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 3, "name": "Mew VMAX", "number": "114", "set": "FST" },
+                { "count": 4, "name": "Mew V", "number": "60", "set": "CRZ" },
+                { "count": 4, "name": "Genesect V", "number": "185", "set": "FST" },
+                { "count": 1, "name": "Meloetta", "number": "124", "set": "FST" },
+                { "count": 1, "name": "Deoxys", "number": "120", "set": "FST" },
+                { "count": 1, "name": "Eiscue", "number": "44", "set": "BRS" },
+            ],
+            "trainer": [
+                { "count": 2, "name": "Elesa's Sparkle", "number": "233", "set": "FST" },
+                { "count": 2, "name": "Judge", "number": "176", "set": "SVI" },
+                { "count": 2, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 1, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+                { "count": 4, "name": "Cram-o-matic", "number": "229", "set": "FST" },
+                { "count": 4, "name": "Power Tablet", "number": "236", "set": "FST" },
+                { "count": 3, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 1, "name": "Switch", "number": "194", "set": "SVI" },
+                { "count": 1, "name": "Escape Rope", "number": "125", "set": "BST" },
+                { "count": 1, "name": "Feather Ball", "number": "141", "set": "ASR" },
+                { "count": 1, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 1, "name": "Hisuian Heavy Ball", "number": "146", "set": "ASR" },
+                { "count": 1, "name": "Pal Pad", "number": "182", "set": "SVI" },
+                { "count": 2, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 1, "name": "Box of Disaster", "number": "154", "set": "LOR" },
+                { "count": 1, "name": "Choice Belt", "number": "176", "set": "PAL" },
+                { "count": 2, "name": "Path to the Peak", "number": "148", "set": "CRE" },
+                { "count": 1, "name": "Lost City", "number": "161", "set": "LOR" }, 
+            ],
+            "energy": [
+                { "count": 4, "name": "Fusion Strike Energy - Special", "number": "244", "set": "FST" },
+                { "count": 3, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" }
             ]
         }
     },
@@ -393,7 +441,7 @@ let masters = [
                 { "count": 1, "name": "Eiscue", "number": "44", "set": "BRS" },
             ],
             "trainer": [
-                { "count": 3, "name": "Elesa's Sparkle", "number": "233", "set": "FST" },
+                { "count": 2, "name": "Elesa's Sparkle", "number": "233", "set": "FST" },
                 { "count": 2, "name": "Judge", "number": "176", "set": "SVI" },
                 { "count": 2, "name": "Boss's Orders", "number": "172", "set": "PAL" },
                 { "count": 1, "name": "Iono", "number": "185", "set": "PAL" },
@@ -411,7 +459,7 @@ let masters = [
                 { "count": 1, "name": "Box of Disaster", "number": "154", "set": "LOR" },
                 { "count": 1, "name": "Choice Belt", "number": "176", "set": "PAL" },
                 { "count": 2, "name": "Path to the Peak", "number": "148", "set": "CRE" },
-                { "count": 1, "name": "Lost City", "number": "161", "set": "LOR" }, 
+                { "count": 2, "name": "Lost City", "number": "161", "set": "LOR" }, 
             ],
             "energy": [
                 { "count": 4, "name": "Fusion Strike Energy - Special", "number": "244", "set": "FST" },
