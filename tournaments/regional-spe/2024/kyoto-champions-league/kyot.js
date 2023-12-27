e92a99e974fb1566941df07839597ccd2065ec17
AlexWilsonNC <alexwilson0017@yahoo.com>
2023-12-27 13:48:00 -0500

    kyoto

---
+++ tournaments/regional-spe/2024/kyoto-champions-league/kyot.js
@@ -0,0 +1,399 @@
+let masters = [
+  {
+    "firstName": "Haruki Miwa",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/kyogre.png',
+    sprite2: '../../../../assets/sprites/comfey.png',
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 1, "name": "Kyogre", "number": "3", "set": "CEL" },
+        { "count": 1, "name": "Iron Hands ex", "number": "70", "set": "PAR" },
+        { "count": 1, "name": "Roaring Moon ex", "number": "124", "set": "PAR" },
+        { "count": 1, "name": "Raikou V", "number": "48", "set": "BRS" },
+        { "count": 1, "name": "Sableye", "number": "70", "set": "LOR" },
+        { "count": 1, "name": "Cramorant", "number": "50", "set": "LOR" },
+        { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" },
+        { "count": 1, "name": "Manaphy", "number": "41", "set": "BRS" },
+        { "count": 1, "name": "Ditto", "number": "132", "set": "MEW" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 1, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+        { "count": 1, "name": "Roxanne", "number": "150", "set": "ASR" },
+        { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+        { "count": 4, "name": "Mirage Gate", "number": "163", "set": "LOR" },
+        { "count": 4, "name": "Nest Ball", "number": "181", "set": "SVI" },
+        { "count": 4, "name": "Switch Cart", "number": "154", "set": "ASR" },
+        { "count": 3, "name": "Escape Rope", "number": "125", "set": "BST" },
+        { "count": 2, "name": "Super Rod", "number": "188", "set": "PAL" },
+        { "count": 2, "name": "Energy Recycler", "number": "124", "set": "BST" },
+        { "count": 1, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+        { "count": 1, "name": "Hisuian Heavy Ball", "number": "146", "set": "ASR" },
+        { "count": 1, "name": "Pal Pad", "number": "182", "set": "SVI" },
+        { "count": 1, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+        { "count": 2, "name": "PokeStop", "number": "68", "set": "PGO" },
+        { "count": 1, "name": "Artazon", "number": "171", "set": "PAL" },
+
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 3, "name": "Basic Darkness Energy", "number": "7", "set": "SVE" },
+        { "count": 2, "name": "Basic Lightning Energy", "number": "4", "set": "SVE" },
+        { "count": 2, "name": "Basic Psychic Energy", "number": "5", "set": "SVE" },
+        { "count": 1, "name": "Jet Energy - Special", "number": "190", "set": "PAL" },
+      ]
+    }
+  },
+  {
+    "firstName": "Shuma Kikuchi",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/roaring-moon.png',
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "Roaring Moon ex", "number": "124", "set": "PAR" },
+        { "count": 1, "name": "Morpeko", "number": "121", "set": "PAR" },
+        { "count": 1, "name": "Darkrai VSTAR", "number": "99", "set": "ASR" },
+        { "count": 1, "name": "Darkrai V", "number": "98", "set": "ASR" },
+        { "count": 1, "name": "Lumineon V", "number": "250", "set": "PR-SW" },
+        { "count": 1, "name": "Squawkabilly ex", "number": "169", "set": "PAL" },
+        { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" },
+      ], "trainer": [
+        { "count": 4, "name": "Professor Sada's Vitality", "number": "170", "set": "PAR" },
+        { "count": 1, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+        { "count": 1, "name": "Iono", "number": "185", "set": "PAL" },
+        { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+        { "count": 4, "name": "Dark Patch", "number": "139", "set": "ASR" },
+        { "count": 4, "name": "Cross Switcher", "number": "230", "set": "FST" },
+        { "count": 4, "name": "Trekking Shoes", "number": "145", "set": "CRZ" },
+        { "count": 4, "name": "Crossceiver", "number": "231", "set": "FST" },
+        { "count": 3, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+        { "count": 3, "name": "Earthen Vessel", "number": "163", "set": "PAR" },
+        { "count": 2, "name": "Nest Ball", "number": "181", "set": "SVI" },
+        { "count": 1, "name": "Switch Cart", "number": "154", "set": "ASR" },
+        { "count": 1, "name": "Switch", "number": "194", "set": "SVI" },
+        { "count": 1, "name": "Super Rod", "number": "188", "set": "PAL" },
+        { "count": 1, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+        { "count": 4, "name": "PokeStop", "number": "68", "set": "PGO" },
+      ], "energy": [
+        { "count": 9, "name": "Darkness Energy - Basic", "number": "231", "set": "CRE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Kazuki Takahashi",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/gholdengo.png',
+    sprite2: '../../../../assets/sprites/palkia-origin.png',
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Gholdengo ex", "number": "139", "set": "PAR" },
+        { "count": 4, "name": "Gimmighoul", "number": "88", "set": "PAR" },
+        { "count": 1, "name": "Origin Forme Palkia VSTAR", "number": "40", "set": "ASR" },
+        { "count": 1, "name": "Origin Forme Palkia V", "number": "39", "set": "ASR" },
+        { "count": 1, "name": "Milotic", "number": "38", "set": "EVS" },
+        { "count": 1, "name": "Feebas", "number": "35", "set": "PAR" },
+        { "count": 1, "name": "Manaphy", "number": "41", "set": "BRS" },
+        { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" },
+      ], "trainer": [
+        { "count": 4, "name": "Worker", "number": "167", "set": "SIT" },
+        { "count": 2, "name": "Irida", "number": "147", "set": "ASR" },
+        { "count": 1, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+        { "count": 1, "name": "Roxanne", "number": "150", "set": "ASR" },
+        { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+        { "count": 4, "name": "Cross Switcher", "number": "230", "set": "FST" },
+        { "count": 4, "name": "Superior Energy Retrieval", "number": "189", "set": "PAL" },
+        { "count": 3, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+        { "count": 3, "name": "Nest Ball", "number": "181", "set": "SVI" },
+        { "count": 2, "name": "Earthen Vessel", "number": "163", "set": "PAR" },
+        { "count": 1, "name": "Super Rod", "number": "188", "set": "PAL" },
+        { "count": 1, "name": "Energy Retrieval", "number": "171", "set": "SVI" },
+        { "count": 1, "name": "Letter of Encouragement", "number": "189", "set": "OBF" },
+        { "count": 4, "name": "PokeStop", "number": "68", "set": "PGO" },
+      ], "energy": [
+        { "count": 6, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+        { "count": 5, "name": "Metal Energy - Basic", "number": "159", "set": "CRZ" },
+      ]
+    }
+  },
+  {
+    "firstName": "Shota Hirota",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/mew-vmax.png',
+    sprite2: '../../../../assets/sprites/genesect.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Ryohei Miyamoto",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/arceus.png',
+    sprite2: '../../../../assets/sprites/zeraora.png',
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Arceus VSTAR", "number": "123", "set": "BRS" },
+        { "count": 4, "name": "Arceus V", "number": "122", "set": "BRS" },
+        { "count": 1, "name": "Zeraora VSTAR", "number": "265", "set": "PR-SW" },
+        { "count": 1, "name": "Zeraora VMAX", "number": "264", "set": "PR-SW" },
+        { "count": 2, "name": "Zeraora V", "number": "263", "set": "PR-SW" },
+        { "count": 1, "name": "Flying Pikachu VMAX", "number": "7", "set": "CEL" },
+        { "count": 2, "name": "Flying Pikachu V", "number": "6", "set": "CEL" },
+        { "count": 1, "name": "Ditto", "number": "132", "set": "MEW" },
+
+      ], "trainer": [
+        { "count": 4, "name": "Iono", "number": "185", "set": "PAL" },
+        { "count": 4, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+        { "count": 3, "name": "Judge", "number": "176", "set": "SVI" },
+        { "count": 3, "name": "Professor's Research", "number": "190", "set": "SVI" },
+        { "count": 4, "name": "Nest Ball", "number": "181", "set": "SVI" },
+        { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+        { "count": 1, "name": "Escape Rope", "number": "125", "set": "BST" },
+        { "count": 1, "name": "Canceling Cologne", "number": "136", "set": "ASR" },
+        { "count": 1, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+        { "count": 3, "name": "Path to the Peak", "number": "148", "set": "CRE" },
+        { "count": 1, "name": "Lost City", "number": "161", "set": "LOR" },
+      ], "energy": [
+        { "count": 8, "name": "Lightning Energy - Basic", "number": "155", "set": "CRZ" },
+        { "count": 4, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" },
+        { "count": 3, "name": "V Guard Energy - Special", "number": "169", "set": "SIT" }
+      ]
+    }
+  },
+  {
+    "firstName": "Dai Akizuki",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/charizard.png',
+    sprite2: '../../../../assets/sprites/pidgeot.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Masato Matsushita",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/urshifu-rapid-strike-gmax.png',
+    sprite2: '../../../../assets/sprites/iron-valiant.png',
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+       { "count": 3, "name": "Rapid Strike Urshifu VMAX", "number": "88", "set": "BST" },
+        { "count": 2, "name": "Rapid Strike Urshifu V", "number": "87", "set": "BST" },
+       { "count": 4, "name": "Iron Valiant ex", "number": "89", "set": "PAR" },
+       { "count": 1, "name": "Medicham V", "number": "83", "set": "EVS" },
+       { "count": 1, "name": "Radiant Charizard", "number": "20", "set": "CRZ" },
+       { "count": 1, "name": "Squawkabilly ex", "number": "169", "set": "PAL" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+        { "count": 3, "name": "Iono", "number": "185", "set": "PAL" },
+       { "count": 3, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+       { "count": 1, "name": "Worker", "number": "167", "set": "SIT" },
+       { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+       { "count": 4, "name": "Switch", "number": "194", "set": "SVI" },
+       { "count": 4, "name": "Switch Cart", "number": "154", "set": "ASR" },
+       { "count": 4, "name": "Escape Rope", "number": "125", "set": "BST" },
+       { "count": 3, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+       { "count": 1, "name": "Nest Ball", "number": "181", "set": "SVI" },
+       { "count": 1, "name": "Hisuian Heavy Ball", "number": "146", "set": "ASR" },
+       { "count": 1, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+       { "count": 2, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+       { "count": 2, "name": "Future Booster Energy Capsule", "number": "164", "set": "PAR" },
+       { "count": 1, "name": "Technical Machine: Devolution", "number": "177", "set": "PAR" },
+       { "count": 3, "name": "Town Store", "number": "196", "set": "OBF" },
+      ],
+      "energy": [
+        { "count": 4, "name": "Rapid Strike Energy - Special", "number": "140", "set": "BST" },
+       { "count": 3, "name": "Luminous Energy - Special", "number": "191", "set": "PAL" },
+      ]
+    }
+  },
+  {
+    "firstName": "Yuichi Okada",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/giratina-origin.png',
+    sprite2: '../../../../assets/sprites/comfey.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Ryosuke Kasai",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/charizard.png',
+    sprite2: '../../../../assets/sprites/pidgeot.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Seiya Yanagi",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/giratina-origin.png',
+    sprite2: '../../../../assets/sprites/comfey.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Mizuki Takao",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/comfey.png',
+    sprite2: '../../../../assets/sprites/sableye.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Naoaki Nishimura",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/arceus.png',
+    sprite2: '../../../../assets/sprites/giratina-origin.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Hitoshi Takahata",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/gardevoir.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Issei Fukae",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/gardevoir.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Ryuki Okada",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/charizard.png',
+    sprite2: '../../../../assets/sprites/pidgeot.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "Naoto Asakura",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Kyoto Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/charizard.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ], "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+]
