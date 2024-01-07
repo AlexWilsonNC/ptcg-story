79d8ffe0c8d8985269f85d5ddc2d035044af0d35
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-01-07 18:44:29 -0500

    portland

--- tournaments/regional-spe/2024/portland/port.js
+++ tournaments/regional-spe/2024/portland/port.js
@@ -144,11 +144,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Keaton Gill",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Portland Regionals",
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/hyphen.png',
+        sprite2: '../../../../assets/sprites/charizard.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -162,11 +162,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Dan Hugar",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Portland Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/hyphen.png',
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -180,11 +180,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Justin Teng",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Portland Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/hyphen.png',
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/sableye.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -198,11 +198,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Vance Kelley",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Portland Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/hyphen.png',
+        sprite1: '../../../../assets/sprites/mew-vmax.png',
+        sprite2: '../../../../assets/sprites/genesect.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -216,11 +216,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Mario Martinez",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Portland Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/hyphen.png',
+        sprite1: '../../../../assets/sprites/mew-vmax.png',
+        sprite2: '../../../../assets/sprites/genesect.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -234,11 +234,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Nicolas Gist",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Portland Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/hyphen.png',
+        sprite1: '../../../../assets/sprites/mew-vmax.png',
+        sprite2: '../../../../assets/sprites/genesect.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -252,11 +252,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Yosha Riley",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Portland Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/hyphen.png',
+        sprite1: '../../../../assets/sprites/inteleon-vmax.png',
+        sprite2: '../../../../assets/sprites/urshifu-rapid-strike-gmax.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -270,11 +270,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Terrence Miller",
+        flag: '../../../../assets/flags/usa.png',
         "event": "2024 Portland Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/hyphen.png',
+        sprite1: '../../../../assets/sprites/mew-vmax.png',
+        sprite2: '../../../../assets/sprites/genesect.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
