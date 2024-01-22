a1ebe0b976dbfe8cf1850d56ca8b7cfa82de1d20
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-01-22 00:25:48 -0500

    clt

--- tournaments/regional-spe/2024/charlotte/charlotte.js
+++ tournaments/regional-spe/2024/charlotte/charlotte.js
@@ -5,15 +5,35 @@ let masters = [
     "event": "2024 Charlotte Regionals",
     sprite1: '../../../../assets/sprites/giratina-origin.png',
     sprite2: '../../../../assets/sprites/comfey.png',
-    // list: '../../../../assets/list-icon.png',
-    list: '../../../../assets/sprites/blank.png',
+    list: '../../../../assets/list-icon.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-      ], "trainer": [
-        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-      ], "energy": [
-        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+        { "count": 3, "name": "Giratina VSTAR", "number": "131", "set": "LOR" },
+        { "count": 3, "name": "Giratina V", "number": "130", "set": "LOR" },
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 1, "name": "Sableye", "number": "70", "set": "LOR" },
+        { "count": 1, "name": "Cramorant", "number": "50", "set": "LOR" },
+        { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 2, "name": "Roxanne", "number": "150", "set": "ASR" },
+        { "count": 2, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+        { "count": 1, "name": "Avery", "number": "130", "set": "CRE" },
+        { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+        { "count": 4, "name": "Mirage Gate", "number": "163", "set": "LOR" },
+        { "count": 4, "name": "Switch Cart", "number": "154", "set": "ASR" },
+        { "count": 3, "name": "Nest Ball", "number": "181", "set": "SVI" },
+        { "count": 2, "name": "Super Rod", "number": "188", "set": "PAL" },
+        { "count": 2, "name": "Pokegear 3.0", "number": "186", "set": "SVI" },
+        { "count": 1, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+        { "count": 4, "name": "Path to the Peak", "number": "148", "set": "CRE" },
+      ],
+      "energy": [
+        { "count": 4, "name": "Psychic Energy - Basic", "number": "232", "set": "CRE" },
+        { "count": 3, "name": "Grass Energy - Basic", "number": "152", "set": "CRZ" },
+        { "count": 3, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+        { "count": 4, "name": "Jet Energy - Special", "number": "190", "set": "PAL" },
       ]
     }
   },
