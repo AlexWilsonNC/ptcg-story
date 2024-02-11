11f96f9a1fde65089c49e68bb7e3800db732e5f6
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-02-11 15:13:12 -0500

    dortmund

--- tournaments/regional-spe/2024/dortmund/dort.js
+++ tournaments/regional-spe/2024/dortmund/dort.js
@@ -348,21 +348,49 @@ let masters = [
         }
     },
     {
-        "firstName": "Lennert Van Overloop",
+        "firstName": "Lennert van Overloop",
         flag: '../../../../assets/flags/belgium.png',
         "event": "2024 Dortmund Regionals",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/charizard.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        list: '../../../../assets/list-icon.png',
         "decklist": {
             "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ]
+                { "count": 3, "name": "Charizard ex", "number": "125", "set": "OBF" },
+                { "count": 1, "name": "Charmeleon", "number": "27", "set": "OBF" },
+                { "count": 2, "name": "Charmander", "number": "4", "set": "MEW" },
+                { "count": 1, "name": "Charmander", "number": "26", "set": "OBF" },
+                { "count": 1, "name": "Charmander", "number": "47", "set": "PR-SV" },
+                { "count": 2, "name": "Pidgeot ex", "number": "164", "set": "OBF" },
+                { "count": 2, "name": "Pidgey", "number": "16", "set": "MEW" },
+                { "count": 1, "name": "Radiant Charizard", "number": "20", "set": "CRZ" },
+                { "count": 1, "name": "Rotom V", "number": "58", "set": "LOR" },
+                { "count": 1, "name": "Lumineon V", "number": "250", "set": "PR-SW" },
+                { "count": 1, "name": "Mew", "number": "11", "set": "CEL" },
+                { "count": 1, "name": "Manaphy", "number": "41", "set": "BRS" },
+                { "count": 1, "name": "Jirachi", "number": "126", "set": "PAR" },
+              ], "trainer": [
+                { "count": 3, "name": "Arven", "number": "186", "set": "OBF" },
+                { "count": 3, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 3, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 1, "name": "Professor's Research", "number": "190", "set": "SVI" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+                { "count": 4, "name": "Rare Candy", "number": "191", "set": "SVI" },
+                { "count": 2, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 2, "name": "Super Rod", "number": "188", "set": "PAL" },
+                { "count": 2, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+                { "count": 1, "name": "Level Ball", "number": "129", "set": "BST" },
+                { "count": 1, "name": "Switch", "number": "194", "set": "SVI" },
+                { "count": 1, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 1, "name": "Vitality Band", "number": "185", "set": "SSH" },
+                { "count": 1, "name": "Choice Belt", "number": "176", "set": "PAL" },
+                { "count": 1, "name": "Artazon", "number": "171", "set": "PAL" },
+                { "count": 1, "name": "Collapsed Stadium", "number": "137", "set": "BRS" },
+                { "count": 1, "name": "Lost City", "number": "161", "set": "LOR" },
+              ], "energy": [
+                { "count": 6, "name": "Fire Energy - Basic", "number": "2", "set": "SVE" },
+                    ]
         }
     },
     {
