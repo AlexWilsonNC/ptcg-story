b76838a7fcda7cb077b8dc9cfa3b5e059c7ff130
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-02-17 21:15:19 -0500

    champions league

---
+++ players/player-list.js
@@ -0,0 +1,30 @@
+let players = [
+    {
+        playerName: "Alex Wilson",
+        country: "US",
+        accomplishments: [
+            {
+                event: "Knoxville Regionals",
+                eventLink: "../tournaments/regional-spe/2024/knoxville/results",
+                date: "Feb 10, 2024",
+                place: "71",
+                prize: "",
+                deck: {
+                    sprite1: '../assets/sprites/mew-vmax.png',
+                    sprite2: '../assets/sprites/genesect.png',
+                }
+            },
+            {
+                event: "2022 EUIC",
+                eventLink: "../major-events/2022/euic/results",
+                date: "Apr 22, 2022",
+                place: "59",
+                prize: "500",
+                deck: {
+                    sprite1: '../assets/sprites/arceus.png',
+                    sprite2: '../assets/sprites/crobat-vmax.png',
+                }
+            },
+        ]
+    }
+]
---
+++ tournaments/regional-spe/2024/fukuoka-champions-league/results.js
@@ -0,0 +1,290 @@
+let masters = [
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+      ], "trainer": [
+        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+      ], "energy": [
+        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+]
