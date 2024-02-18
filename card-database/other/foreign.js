2974559f6eec3d1cbae7084d4e2c341687fb14b9
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-02-18 10:01:38 -0500

    athens

--- card-database/other/foreign.js
+++ card-database/other/foreign.js
@@ -14,5 +14,45 @@ let fr = [
             "small": "https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/5d6c243d-91da-47a7-b3f6-448861cfc0fb/card-026.png?format=750w",
             "large": "https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/5d6c243d-91da-47a7-b3f6-448861cfc0fb/card-026.png?format=750w"
         }
+    },
+    {
+        "id": "fr-3",
+        "name": "Cinccino",
+        "images": {
+            "small": "https://limitlesstcg.nyc3.digitaloceanspaces.com/tpc/SV5K/SV5K_60_R_JP_LG.png",
+            "large": "https://limitlesstcg.nyc3.digitaloceanspaces.com/tpc/SV5K/SV5K_60_R_JP_LG.png"
+        }
+    },
+    {
+        "id": "fr-4",
+        "name": "Mist Energy",
+        "images": {
+            "small": "https://limitlesstcg.nyc3.digitaloceanspaces.com/tpc/SV5M/SV5M_71_R_JP_LG.png",
+            "large": "https://limitlesstcg.nyc3.digitaloceanspaces.com/tpc/SV5M/SV5M_71_R_JP_LG.png"
+        }
+    },
+    {
+        "id": "fr-5",
+        "name": "Master Ball",
+        "images": {
+            "small": "https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1703403248656-9JFPD3HYNNSCEE80QW31/1530_032_T_MasterBall_ol.png?format=750w",
+            "large": "https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1703403248656-9JFPD3HYNNSCEE80QW31/1530_032_T_MasterBall_ol.png?format=750w"
+        }
+    },
+    {
+        "id": "fr-6",
+        "name": "idk",
+        "images": {
+            "small": "https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1705064032198-QL33O8V90BABWXED9VDE/IMG_6448.png?format=750w",
+            "large": "https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1705064032198-QL33O8V90BABWXED9VDE/IMG_6448.png?format=750w"
+        }
+    },
+    {
+        "id": "fr-7",
+        "name": "PRIME CATCHER",
+        "images": {
+            "small": "https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1703403248680-7AUURXPT5WT83RAQ12LV/SV5_17.png?format=750w",
+            "large": "https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1703403248680-7AUURXPT5WT83RAQ12LV/SV5_17.png?format=750w"
+        }
     }
 ]
--- tournaments/regional-spe/2024/fukuoka-champions-league/results.js
+++ tournaments/regional-spe/2024/fukuoka-champions-league/results.js
@@ -1,5 +1,84 @@
 let masters = [
   {
+    "firstName": "@daisuke_pokeka",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/lugia.png',
+    sprite2: '../../../../assets/sprites/cinccino.png',
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "Lugia VSTAR", "number": "139", "set": "SIT" },
+        { "count": 4, "name": "Lugia V", "number": "138", "set": "SIT" },
+        { "count": 4, "name": "Archeops", "number": "147", "set": "SIT" },
+        { "count": 3, "name": "Cinccino", "number": "3", "set": "FR" },
+        { "count": 3, "name": "Minccino", "number": "124", "set": "BRS" },
+        { "count": 2, "name": "Snorlax", "number": "143", "set": "LOR" },
+        { "count": 1, "name": "Lumineon V", "number": "250", "set": "PR-SW" },
+      ],
+      "trainer": [
+        { "count": 3, "name": "Iono", "number": "185", "set": "PAL" },
+        { "count": 2, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+        { "count": 2, "name": "Professor's Research", "number": "189", "set": "SVI" },
+        { "count": 2, "name": "Serena", "number": "164", "set": "SIT" },
+        { "count": 1, "name": "Jacq", "number": "175", "set": "SVI" },
+        { "count": 4, "name": "Capturing Aroma", "number": "153", "set": "SIT" },
+        { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+        { "count": 2, "name": "Great Ball", "number": "183", "set": "PAL" },
+        { "count": 1, "name": "Master Ball", "number": "5", "set": "FR" },
+        { "count": 2, "name": "Collapsed Stadium", "number": "137", "set": "BRS" }
+      ],
+      "energy": [
+        { "count": 4, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" },
+        { "count": 4, "name": "Jet Energy - Special", "number": "190", "set": "PAL" },
+        { "count": 4, "name": "Gift Energy - Special", "number": "171", "set": "LOR" },
+        { "count": 3, "name": "Mist Energy - Special", "number": "4", "set": "FR" },
+        { "count": 1, "name": "Therapeutic Energy - Special", "number": "193", "set": "PAL" },
+        { "count": 1, "name": "V Guard Energy - Special", "number": "169", "set": "SIT" },
+      ]
+    }
+  },
+  {
+    "firstName": "@miTSUriN_miri",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Fukuoka Champions League",
+    sprite1: '../../../../assets/sprites/arceus.png',
+    sprite2: '../../../../assets/sprites/vulpix-alola.png',
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 4, "name": "Arceus VSTAR", "number": "123", "set": "BRS" },
+        { "count": 4, "name": "Arceus V", "number": "122", "set": "BRS" },
+        { "count": 2, "name": "Alolan Vulpix VSTAR", "number": "34", "set": "SIT" },
+        { "count": 2, "name": "Alolan Vulpix V", "number": "33", "set": "SIT" },
+        { "count": 1, "name": "Radiant Greninja", "number": "46", "set": "ASR" }
+        ], "trainer": [
+          { "count": 4, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+          { "count": 3, "name": "Professor's Research", "number": "147", "set": "BRS" }, 
+          { "count": 2, "name": "Colress's Experiment", "number": "155", "set": "LOR" }, 
+          { "count": 2, "name": "Iono", "number": "185", "set": "PAL" },
+          { "count": 2, "name": "Professor Turo's Scenario", "number": "171", "set": "PAR" },
+          { "count": 1, "name": "Judge", "number": "235", "set": "FST" }, 
+          { "count": 1, "name": "Cheren's Care", "number": "134", "set": "BRS" },
+          { "count": 1, "name": "IDK", "number": "6", "set": "FR" },
+          { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+          { "count": 4, "name": "Nest Ball", "number": "181", "set": "SVI" },
+          { "count": 2, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+          { "count": 1, "name": "Super Rod", "number": "188", "set": "PAL" },
+          { "count": 1, "name": "Switch", "number": "194", "set": "SVI" },
+          { "count": 1, "name": "Switch", "number": "7", "set": "FR" },
+          { "count": 2, "name": "Lost City", "number": "161", "set": "LOR" },
+        ], "energy": [
+          { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+          { "count": 4, "name": "Double Turbo Energy - Special", "number": "151", "set": "BRS" },
+          { "count": 2, "name": "Mist Energy - Special", "number": "4", "set": "FR" },
+          { "count": 2, "name": "Mist Energy - Special", "number": "182", "set": "PAR" },
+          { "count": 1, "name": "V Guard Energy - Special", "number": "169", "set": "SIT" },
+
+        ]
+    }
+  },
+  {
     "firstName": "--",
     flag: '../../../../assets/flags/japan.png',
     "event": "2024 Fukuoka Champions League",
@@ -9,11 +88,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -27,11 +106,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -45,11 +124,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -63,11 +142,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -81,11 +160,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -99,11 +178,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -117,11 +196,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -135,11 +214,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -153,11 +232,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -171,11 +250,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -189,11 +268,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -207,11 +286,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -225,11 +304,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
@@ -243,47 +322,11 @@ let masters = [
     list: '../../../../assets/sprites/blank.png',
     "decklist": {
       "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
-      ]
-    }
-  },
-  {
-    "firstName": "--",
-    flag: '../../../../assets/flags/japan.png',
-    "event": "2024 Fukuoka Champions League",
-    sprite1: '../../../../assets/sprites/blank.png',
-    sprite2: '../../../../assets/sprites/hyphen.png',
-    // list: '../../../../assets/list-icon.png',
-    list: '../../../../assets/sprites/blank.png',
-    "decklist": {
-      "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
-      ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
-      ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
-      ]
-    }
-  },
-  {
-    "firstName": "--",
-    flag: '../../../../assets/flags/japan.png',
-    "event": "2024 Fukuoka Champions League",
-    sprite1: '../../../../assets/sprites/blank.png',
-    sprite2: '../../../../assets/sprites/hyphen.png',
-    // list: '../../../../assets/list-icon.png',
-    list: '../../../../assets/sprites/blank.png',
-    "decklist": {
-      "pokemon": [
-        { "count": 4, "name": "Comfey", "number": "79", "set": "LOR" },
-      ], "trainer": [
-        { "count": 4, "name": "Colress's Experiment", "number": "155", "set": "LOR" },
-      ], "energy": [
-        { "count": 3, "name": "Basic Water Energy", "number": "3", "set": "SVE" },
+        { "count": 4, "name": "eeee", "number": "00", "set": "00" },
       ]
     }
   },
