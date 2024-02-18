0de930091b328001497223235283d286c819b2a5
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-02-18 18:00:36 -0500

    champions league

---
+++ tournaments/regional-spe/2015/overland-park/results.js
@@ -0,0 +1,146 @@
+let masters = [
+    {
+        "firstName": "Harrison Leven",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Overland Park Regionals",
+        sprite1: '../../../../assets/sprites/yveltal.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Caleb Gedemer",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Overland Park Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir-mega.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Jordan Roberts",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2015 Overland Park Regionals",
+        sprite1: '../../../../assets/sprites/seismitoad.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Travis Nunlist",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Overland Park Regionals",
+        sprite1: '../../../../assets/sprites/rayquaza.png',
+        sprite2: '../../../../assets/sprites/eelektrik.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Kevin Norton",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2015 Overland Park Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/seismitoad.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Kyle Haverland",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Overland Park Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/groudon-primal.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Michael Slutsky",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Overland Park Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/klinklang.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Shawn Bernaky",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2015 Overland Park Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir-mega.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    }
+]
