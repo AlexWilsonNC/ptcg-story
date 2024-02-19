90ae691041038e5c6c55f9ad5a3c7a1356008df8
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-02-18 19:35:53 -0500

    15 regs

---
+++ tournaments/regional-spe/2015/seattle/results.js
@@ -0,0 +1,205 @@
+let masters = [
+    {
+        "firstName": "Chase Moloney",
+        flag: '../../../../assets/flags/canada.png',
+        "event": "2015 Seattle Regionals",
+        sprite1: '../../../../assets/sprites/yveltal.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "Yveltal-EX", "number": "79", "set": "XY" },
+                { "count": 2, "name": "Darkrai-EX", "number": "63", "set": "DEX" },
+                { "count": 2, "name": "Seismitoad-EX", "number": "20", "set": "FFI" },
+                { "count": 1, "name": "Yveltal", "number": "78", "set": "XY" },
+                { "count": 1, "name": "Archeops", "number": "67", "set": "NVI" },
+                { "count": 1, "name": "Jirachi-EX", "number": "60", "set": "PLB" },
+                { "count": 1, "name": "Keldeo-EX", "number": "49", "set": "BCR" },
+                { "count": 1, "name": "Spiritomb", "number": "87", "set": "LTR" },
+            ], "trainer": [
+                { "count": 4, "name": "Professor Sycamore", "number": "101", "set": "PHF" },
+                { "count": 3, "name": "N", "number": "100", "set": "PR-BLW" },
+                { "count": 1, "name": "Maxie's Hidden Ball Trick", "number": "133", "set": "PRC" },
+                { "count": 1, "name": "Lysandre", "number": "90", "set": "FLF" },
+                { "count": 1, "name": "Colress", "number": "118", "set": "PLS" },
+                { "count": 4, "name": "VS Seeker", "number": "109", "set": "PHF" },
+                { "count": 4, "name": "Ultra Ball", "number": "93", "set": "ROS" },
+                { "count": 4, "name": "Hypnotoxic Laser", "number": "123", "set": "PLS" },
+                { "count": 4, "name": "Dark Patch", "number": "93", "set": "DEX" },
+                { "count": 4, "name": "Battle Compressor", "number": "92", "set": "PHF" },
+                { "count": 1, "name": "Computer Search", "number": "137", "set": "BCR" },
+                { "count": 3, "name": "Muscle Band", "number": "121", "set": "XY" },
+                { "count": 2, "name": "Virbank City Gym", "number": "126", "set": "PLS" },
+            ], "energy": [
+                { "count": 8, "name": "Darkness", "number": "111", "set": "BLW" },
+                { "count": 4, "name": "Double Colorless Energy", "number": "111", "set": "PHF" },
+            ]
+        }
+    },
+    {
+        "firstName": "Andrew Jackson",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Seattle Regionals",
+        sprite1: '../../../../assets/sprites/landorus-therian.png',
+        sprite2: '../../../../assets/sprites/crobat.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 2, "name": "Landorus-EX", "number": "89", "set": "BCR" },
+                { "count": 2, "name": "Hawlucha", "number": "63", "set": "FFI" },
+                { "count": 2, "name": "Lucario-EX", "number": "54", "set": "FFI" },
+                { "count": 2, "name": "Crobat", "number": "33", "set": "PHF" },
+                { "count": 3, "name": "Golbat", "number": "32", "set": "PHF" },
+                { "count": 4, "name": "Zubat", "number": "53", "set": "PLS" },
+                { "count": 1, "name": "Dedenne", "number": "34", "set": "FFI" },
+                { "count": 1, "name": "Jirachi-EX", "number": "60", "set": "PLB" },
+            ], "trainer": [
+                { "count": 4, "name": "Professor Sycamore", "number": "101", "set": "PHF" },
+                { "count": 2, "name": "N", "number": "100", "set": "PR-BLW" },
+                { "count": 2, "name": "Colress", "number": "118", "set": "PLS" },
+                { "count": 1, "name": "Lysandre", "number": "90", "set": "FLF" },
+                { "count": 1, "name": "AZ", "number": "91", "set": "PHF" },
+                { "count": 1, "name": "Lysandre's Trump Card", "number": "99", "set": "PHF" },
+                { "count": 4, "name": "VS Seeker", "number": "109", "set": "PHF" },
+                { "count": 4, "name": "Acro Bike", "number": "122", "set": "PRC" },
+                { "count": 3, "name": "Ultra Ball", "number": "93", "set": "ROS" },
+                { "count": 2, "name": "Super Scoop Up", "number": "100", "set": "FFI" },
+                { "count": 2, "name": "Level Ball", "number": "89", "set": "NXD" },
+                { "count": 1, "name": "Battle Compressor", "number": "92", "set": "PHF" },
+                { "count": 3, "name": "Muscle Band", "number": "121", "set": "XY" },
+                { "count": 2, "name": "Fighting Stadium", "number": "90", "set": "FFI" },
+                { "count": 1, "name": "Silent Lab", "number": "140", "set": "PRC" },
+            ], "energy": [
+                { "count": 6, "name": "Fighting", "number": "110", "set": "BLW" },
+                { "count": 4, "name": "Strong Energy", "number": "104", "set": "FFI" },
+            ]
+        }
+    },
+    {
+        "firstName": "Oscar Morales",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Seattle Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/manectric-mega.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Mia Violet",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Seattle Regionals",
+        sprite1: '../../../../assets/sprites/seismitoad.png',
+        sprite2: '../../../../assets/sprites/dusknoir.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "Seismitoad-EX", "number": "20", "set": "FFI" },
+                { "count": 3, "name": "Shaymin-EX", "number": "77", "set": "ROS" },
+                { "count": 2, "name": "Dusknoir", "number": "63", "set": "BCR" },
+                { "count": 1, "name": "Dusclops", "number": "39", "set": "FLF" },
+                { "count": 3, "name": "Duskull", "number": "61", "set": "BCR" },
+            ], "trainer": [
+                { "count": 4, "name": "Professor Sycamore", "number": "101", "set": "PHF" },
+                { "count": 1, "name": "Lysandre's Trump Card", "number": "99", "set": "PHF" },
+                { "count": 4, "name": "VS Seeker", "number": "109", "set": "PHF" },
+                { "count": 4, "name": "Roller Skates", "number": "125", "set": "XY" },
+                { "count": 4, "name": "Hypnotoxic Laser", "number": "123", "set": "PLS" },
+                { "count": 4, "name": "Super Scoop Up", "number": "100", "set": "FFI" },
+                { "count": 4, "name": "Pokemon Catcher", "number": "36", "set": "KSS" },
+                { "count": 4, "name": "Trainers' Mail", "number": "92", "set": "ROS" },
+                { "count": 3, "name": "Crushing Hammer", "number": "34", "set": "KSS" },
+                { "count": 1, "name": "Enhanced Hammer", "number": "94", "set": "PHF" },
+                { "count": 1, "name": "Rare Candy", "number": "135", "set": "PRC" },
+                { "count": 1, "name": "Startling Megaphone", "number": "97", "set": "FLF" },
+                { "count": 1, "name": "Dowsing Machine", "number": "128", "set": "PLS" },
+                { "count": 2, "name": "Head Ringer", "number": "97", "set": "PHF" },
+                { "count": 2, "name": "Muscle Band", "number": "121", "set": "XY" },
+                { "count": 1, "name": "Float Stone", "number": "99", "set": "PLF" },
+                { "count": 2, "name": "Virbank City Gym", "number": "126", "set": "PLS" },
+            ], "energy": [
+                { "count": 4, "name": "Double Colorless Energy", "number": "111", "set": "PHF" },
+            ]
+        }
+    },
+    {
+        "firstName": "Tristen Cox",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2015 Seattle Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Millie Ramirez",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2015 Seattle Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Kian Amini",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Seattle Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/seismitoad.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Elija Covitz",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Seattle Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    }
+]
