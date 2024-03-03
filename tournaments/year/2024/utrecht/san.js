2a930f9f05c26b2a526dddbefc67e2687ce3e1b2
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-03-03 12:47:06 -0500

    utrecht

--- tournaments/year/2024/utrecht/san.js
+++ tournaments/year/2024/utrecht/san.js
@@ -9,6 +9,41 @@ let masters = [
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
             "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Utrecht Special Event",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Tord Reklev",
+        flag: '../../../../assets/flags/norway.png',
+        "event": "2024 Utrecht Special Event",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/urshifu-rapid-strike-gmax.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
                 { "count": 2, "name": "Charizard ex", "number": "125", "set": "OBF" },
                 { "count": 1, "name": "Charmander", "number": "4", "set": "MEW" },
                 { "count": 1, "name": "Charmander", "number": "26", "set": "OBF" },
@@ -58,47 +93,11 @@ let masters = [
         }
     },
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
+        "firstName": "Cristian Sarnataro",
+        flag: '../../../../assets/flags/italy.png',
         "event": "2024 Utrecht Special Event",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/hyphen.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
-        "decklist": {
-            "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ]
-        }
-    },
-    {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
-        "event": "2024 Utrecht Special Event",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/hyphen.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
-        "decklist": {
-            "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ]
-        }
-    },
-    {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
-        "event": "2024 Utrecht Special Event",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/hyphen.png',
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
@@ -151,17 +150,43 @@ let masters = [
         "firstName": "Oscar Madsen",
         flag: '../../../../assets/flags/denmark.png',
         "event": "2024 Utrecht Special Event",
-        sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/charizard.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        list: '../../../../assets/list-icon.png',
         "decklist": {
             "pokemon": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ], "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+                { "count": 3, "name": "Charizard ex", "number": "125", "set": "OBF" },
+                { "count": 1, "name": "Charmeleon", "number": "27", "set": "OBF" },
+                { "count": 3, "name": "Charmander", "number": "4", "set": "MEW" },
+                { "count": 1, "name": "Charmander", "number": "26", "set": "OBF" },
+                { "count": 2, "name": "Pidgeot ex", "number": "164", "set": "OBF" },
+                { "count": 2, "name": "Pidgey", "number": "16", "set": "MEW" },
+                { "count": 1, "name": "Radiant Charizard", "number": "20", "set": "CRZ" },
+                { "count": 1, "name": "Rotom V", "number": "58", "set": "LOR" },
+                { "count": 1, "name": "Lumineon V", "number": "250", "set": "PR-SW" },
+                { "count": 1, "name": "Mew", "number": "11", "set": "CEL" },
+                { "count": 1, "name": "Manaphy", "number": "41", "set": "BRS" },
+                { "count": 1, "name": "Jirachi", "number": "126", "set": "PAR" },
+              ], "trainer": [
+                { "count": 4, "name": "Arven", "number": "186", "set": "OBF" },
+                { "count": 3, "name": "Boss's Orders", "number": "172", "set": "PAL" },
+                { "count": 3, "name": "Iono", "number": "185", "set": "PAL" },
+                { "count": 1, "name": "Roxanne", "number": "150", "set": "ASR" },
+                { "count": 4, "name": "Battle VIP Pass", "number": "225", "set": "FST" },
+                { "count": 4, "name": "Ultra Ball", "number": "196", "set": "SVI" },
+                { "count": 4, "name": "Rare Candy", "number": "191", "set": "SVI" },
+                { "count": 2, "name": "Lost Vacuum", "number": "135", "set": "CRZ" },
+                { "count": 2, "name": "Super Rod", "number": "188", "set": "PAL" },
+                { "count": 1, "name": "Counter Catcher", "number": "160", "set": "PAR" },
+                { "count": 1, "name": "Nest Ball", "number": "181", "set": "SVI" },
+                { "count": 1, "name": "Level Ball", "number": "129", "set": "BST" },
+                { "count": 1, "name": "Forest Seal Stone", "number": "156", "set": "SIT" },
+                { "count": 1, "name": "Choice Belt", "number": "176", "set": "PAL" },
+                { "count": 2, "name": "Artazon", "number": "171", "set": "PAL" },
+                { "count": 1, "name": "Collapsed Stadium", "number": "137", "set": "BRS" },
+              ], "energy": [
+                { "count": 6, "name": "Fire Energy - Basic", "number": "2", "set": "SVE" },
+                { "count": 1, "name": "Jet Energy - Special", "number": "190", "set": "PAL" },
             ]
         }
     },
