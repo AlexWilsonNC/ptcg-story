9fd6eada29abf60843fc436e1614ee3371eee333
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-03-10 14:03:25 -0400

    mario day

---
+++ tournaments/year/2024/goiania/results.js
@@ -0,0 +1,322 @@
+let masters = [
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Lucas Jordão",
+        flag: '../../../../assets/flags/brazil.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Jorge Salas",
+        flag: '../../../../assets/flags/chile.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "William Azevedo",
+        flag: '../../../../assets/flags/brazil.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Vinícius Fernandez",
+        flag: '../../../../assets/flags/brazil.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Vinicius Augusto Gomes",
+        flag: '../../../../assets/flags/brazil.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/charizard.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Eduardo Molina",
+        flag: '../../../../assets/flags/chile.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Andrei Imada",
+        flag: '../../../../assets/flags/brazil.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Francisco Osorio",
+        flag: '../../../../assets/flags/chile.png',
+        "event": "2024 Goiânia Regionals",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
