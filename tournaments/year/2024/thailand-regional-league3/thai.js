c20b450219da9584f2d6da0c44ab34e825a36660
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-03-31 18:47:07 -0400

    hp

---
+++ tournaments/year/2024/thailand-regional-league3/thai.js
@@ -0,0 +1,290 @@
+// let masters = [
+//   {
+//     "firstName": "Wasin Taechakijviboon",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+//   {
+//     "firstName": "eeee",
+//     flag: '../../../../assets/sprites/blank.png',
+//     "event": "2024 Thailand Regional League #3",
+//     sprite1: "../../../../assets/sprites/blank.png",
+//     sprite2: "../../../../assets/sprites/hyphen.png",
+//     // list: '../../../../assets/list-icon.png',
+//     list: '../../../../assets/sprites/blank.png',
+//     "decklist": {
+//       "pokemon": [
+//         { "count": 3, "name": "Inteleon VMAX", "number": "79", "set": "FST" },
+//       ], "trainer": [
+//         { "count": 4, "name": "Professor's Research", "number": "190", "set": "SVI" },
+//       ], "energy": [
+//         { "count": 7, "name": "Water Energy - Basic", "number": "231", "set": "CRE" },
+//       ]
+//     }
+//   },
+// ]
