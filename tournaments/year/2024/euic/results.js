d984bc8bbb8484770ce9fbd69f0a818849881fd2
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-04-06 13:33:08 -0400

    euic

--- tournaments/year/2024/euic/results.js
+++ tournaments/year/2024/euic/results.js
@@ -829,3 +829,26 @@ let masters = [
     //     }
     // },
 ]
+
+let seniors = [
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 EUIC",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
