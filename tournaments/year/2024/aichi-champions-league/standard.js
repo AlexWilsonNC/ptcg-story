695db10b6f5a6d3ef80657c426feb1d2e94528c6
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-04-14 13:45:19 -0400

    events

---
+++ tournaments/year/2024/aichi-champions-league/standard.js
@@ -0,0 +1,162 @@
+let masters = [
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Aichi Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Aichi Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Aichi Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Aichi Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Aichi Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Aichi Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Aichi Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/japan.png',
+    "event": "2024 Aichi Champions League",
+    sprite1: '../../../../assets/sprites/blank.png',
+    sprite2: '../../../../assets/sprites/hyphen.png',
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "trainer": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ],
+      "energy": [
+        { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+      ]
+    }
+  },
+]
