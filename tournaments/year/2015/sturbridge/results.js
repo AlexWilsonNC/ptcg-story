af285180814d9ab7aefd10e0023156d6f2681abd
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-04-18 00:37:38 -0400

    2015

---
+++ tournaments/year/2015/sturbridge/results.js
@@ -0,0 +1,182 @@
+let masters = [
+{
+        "firstName": "Dylan Bryan",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Sturbridge Regionals",
+        sprite1: '../../../../assets/sprites/manectric-mega.png',
+        sprite2: '../../../../assets/sprites/aegislash.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Azul Garcia Griego",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Sturbridge Regionals",
+        sprite1: '../../../../assets/sprites/florges.png',
+        sprite2: '../../../../assets/sprites/aromatisse.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                {"count": 3, "name": "Florges-EX", "set": "PHF", "number": "67"},
+                {"count": 3, "name": "Xerneas", "set": "XY", "number": "96"},
+                {"count": 3, "name": "Aromatisse", "set": "XY", "number": "93"},
+                {"count": 3, "name": "Spritzee", "set": "FLF", "number": "67"},
+                {"count": 1, "name": "Xerneas-EX", "set": "XY", "number": "97"},
+                {"count": 1, "name": "Mewtwo-EX", "set": "NXD", "number": "54"},
+                {"count": 1, "name": "Spiritomb", "set": "LTR", "number": "87"},
+                        ], "trainer": [
+                            {"count": 4, "name": "Professor Juniper", "set": "BLW", "number": "101"},
+                            {"count": 3, "name": "N", "set": "NVI", "number": "92"},
+                            {"count": 3, "name": "AZ", "set": "PHF", "number": "91"},
+                            {"count": 2, "name": "Lysandre", "set": "AOR", "number": "78"},
+                            {"count": 1, "name": "Lysandre's Trump Card", "set": "PHF", "number": "99"},
+                            {"count": 1, "name": "Colress", "set": "PLS", "number": "118"},
+                            {"count": 4, "name": "VS Seeker", "set": "PHF", "number": "109"},
+                            {"count": 4, "name": "Ultra Ball", "set": "PLB", "number": "90"},
+                            {"count": 3, "name": "Max Potion", "set": "EPO", "number": "94"},
+                            {"count": 1, "name": "Level Ball", "set": "NXD", "number": "89"},
+                            {"count": 1, "name": "Computer Search", "set": "BCR", "number": "137"},
+                            {"count": 3, "name": "Hard Charm", "set": "XY", "number": "119"},
+                            {"count": 4, "name": "Fairy Garden", "set": "XY", "number": "117"},
+                        ], "energy": [
+                            {"count": 7, "name": "Fairy Energy", "set": "XY", "number": "140"},
+                            {"count": 4, "name": "Wonder Energy", "set": "PRC", "number": "144"},
+                        ]
+        }
+    },
+{
+        "firstName": "Jimmy O'Brien",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Sturbridge Regionals",
+        sprite1: '../../../../assets/sprites/trevenant.png',
+        sprite2: '../../../../assets/sprites/accelgor.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Chris Leandro",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Sturbridge Regionals",
+        sprite1: '../../../../assets/sprites/florges.png',
+        sprite2: '../../../../assets/sprites/aromatisse.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                {"count": 3, "name": "Florges-EX", "set": "PHF", "number": "67"},
+                {"count": 3, "name": "Xerneas", "set": "XY", "number": "96"},
+                {"count": 3, "name": "Aromatisse", "set": "XY", "number": "93"},
+                {"count": 3, "name": "Spritzee", "set": "FLF", "number": "67"},
+                {"count": 1, "name": "Xerneas-EX", "set": "XY", "number": "97"},
+                {"count": 1, "name": "Mewtwo-EX", "set": "NXD", "number": "54"},
+                {"count": 1, "name": "Spiritomb", "set": "LTR", "number": "87"},
+                        ], "trainer": [
+                            {"count": 4, "name": "Professor Juniper", "set": "BLW", "number": "101"},
+                            {"count": 3, "name": "N", "set": "NVI", "number": "92"},
+                            {"count": 3, "name": "AZ", "set": "PHF", "number": "91"},
+                            {"count": 2, "name": "Lysandre", "set": "AOR", "number": "78"},
+                            {"count": 1, "name": "Lysandre's Trump Card", "set": "PHF", "number": "99"},
+                            {"count": 1, "name": "Colress", "set": "PLS", "number": "118"},
+                            {"count": 4, "name": "VS Seeker", "set": "PHF", "number": "109"},
+                            {"count": 4, "name": "Ultra Ball", "set": "PLB", "number": "90"},
+                            {"count": 3, "name": "Max Potion", "set": "EPO", "number": "94"},
+                            {"count": 1, "name": "Level Ball", "set": "NXD", "number": "89"},
+                            {"count": 1, "name": "Computer Search", "set": "BCR", "number": "137"},
+                            {"count": 3, "name": "Hard Charm", "set": "XY", "number": "119"},
+                            {"count": 4, "name": "Fairy Garden", "set": "XY", "number": "117"},
+                        ], "energy": [
+                            {"count": 7, "name": "Fairy Energy", "set": "XY", "number": "140"},
+                            {"count": 4, "name": "Wonder Energy", "set": "PRC", "number": "144"},
+            ]
+        }
+    },
+{
+        "firstName": "Thomas Masse",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Sturbridge Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/exeggutor.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Michael Diaz",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Sturbridge Regionals",
+        sprite1: '../../../../assets/sprites/trevenant.png',
+        sprite2: '../../../../assets/sprites/accelgor.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Russell LaParre",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Sturbridge Regionals",
+        sprite1: '../../../../assets/sprites/flygon',
+        sprite2: '../../../../assets/sprites/dusknoir.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Drew Guritzky",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2015 Sturbridge Regionals",
+        sprite1: '../../../../assets/sprites/trevenant.png',
+        sprite2: '../../../../assets/sprites/accelgor.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
