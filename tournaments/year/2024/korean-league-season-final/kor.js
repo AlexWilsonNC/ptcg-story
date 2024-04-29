95cf3e34d66e1fad34a39967330f4ef176d48f47
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-04-29 00:02:26 -0400

    event catchup

---
+++ tournaments/year/2024/korean-league-season-final/kor.js
@@ -0,0 +1,312 @@
+let masters = [
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/chien-pao.png",
+    sprite2: "../../../../assets/sprites/baxcalibur.png",
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+        {"count": 2, "name": "Chien-Pao ex", "set": "PAL", "number": "61"},
+        {"count": 2, "name": "Baxcalibur", "set": "PAL", "number": "60"},
+        {"count": 2, "name": "Frigibax", "set": "PAL", "number": "57"},
+        {"count": 1, "name": "Frigibax", "set": "PAL", "number": "58"},
+        {"count": 2, "name": "Bibarel", "set": "BRS", "number": "121"},
+        {"count": 2, "name": "Bidoof", "set": "CRZ", "number": "111"},
+        {"count": 1, "name": "Iron Hands ex", "set": "PAR", "number": "70"},
+        {"count": 1, "name": "Radiant Greninja", "set": "ASR", "number": "46"},
+        {"count": 1, "name": "Iron Bundle", "set": "PAR", "number": "56"},
+            ], "trainer": [
+              {"count": 4, "name": "Irida", "set": "ASR", "number": "147"},
+              {"count": 2, "name": "Ciphermaniac's Codebreaking", "set": "TEF", "number": "145"},
+              {"count": 1, "name": "Iono", "set": "PAF", "number": "80"},
+              {"count": 4, "name": "Buddy-Buddy Poffin", "set": "TEF", "number": "144"},
+              {"count": 4, "name": "Nest Ball", "set": "SVI", "number": "181"},
+              {"count": 4, "name": "Ultra Ball", "set": "SVI", "number": "196"},
+              {"count": 4, "name": "Superior Energy Retrieval", "set": "PAL", "number": "189"},
+              {"count": 3, "name": "Super Rod", "set": "PAL", "number": "188"},
+              {"count": 3, "name": "Rare Candy", "set": "PAF", "number": "89"},
+              {"count": 2, "name": "Earthen Vessel", "set": "PAR", "number": "163"},
+              {"count": 1, "name": "Counter Catcher", "set": "PAR", "number": "160"},
+              {"count": 1, "name": "Hisuian Heavy Ball", "set": "ASR", "number": "146"},
+              {"count": 1, "name": "Canceling Cologne", "set": "ASR", "number": "136"},
+              {"count": 1, "name": "Prime Catcher", "set": "TEF", "number": "157"},
+              {"count": 2, "name": "PokéStop", "set": "PGO", "number": "68"},
+            ], "energy": [
+              {"count": 8, "name": "Basic Water Energy", "set": "SVE", "number": "3"},
+              {"count": 1, "name": "Basic Lightning Energy", "set": "SVE", "number": "4"},
+            ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/comfey.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/chien-pao.png",
+    sprite2: "../../../../assets/sprites/baxcalibur.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/iron-hands.png",
+    sprite2: "../../../../assets/sprites/iron-crown.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/comfey.png",
+    sprite2: "../../../../assets/sprites/iron-hands.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/raging-bolt.png",
+    sprite2: "../../../../assets/sprites/sandy-shocks.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/charizard.png",
+    sprite2: "../../../../assets/sprites/pidgeot.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/arceus.png",
+    sprite2: "../../../../assets/sprites/giratina-origin.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/comfey.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/chien-pao.png",
+    sprite2: "../../../../assets/sprites/baxcalibur.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/espathra.png",
+    sprite2: "../../../../assets/sprites/xatu.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/chien-pao.png",
+    sprite2: "../../../../assets/sprites/baxcalibur.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/lugia.png",
+    sprite2: "../../../../assets/sprites/cinccino.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/lugia.png",
+    sprite2: "../../../../assets/sprites/cinccino.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/arceus.png",
+    sprite2: "../../../../assets/sprites/armarouge.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Name Pending",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League (Season 3)",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/gardevoir.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+]
