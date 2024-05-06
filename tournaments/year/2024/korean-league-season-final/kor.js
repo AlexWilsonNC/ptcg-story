409b6ac9beb286cb8d4ee6d2b277fbbb20958fee
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-05-06 18:41:32 -0400

    may 4 events

--- tournaments/year/2024/korean-league-season-final/kor.js
+++ tournaments/year/2024/korean-league-season-final/kor.js
@@ -21,11 +21,11 @@ let masters = [
         { "count": 1, "name": "Cleffa", "set": "OBF", "number": "80" },
         { "count": 1, "name": "Manaphy", "set": "BRS", "number": "41" },
         { "count": 1, "name": "Jirachi", "set": "PAR", "number": "126" },
-    ],
-    "trainer": [
+      ],
+      "trainer": [
         { "count": 3, "name": "Arven", "set": "OBF", "number": "186" },
         { "count": 2, "name": "Iono", "set": "PAF", "number": "80" },
-        {"count": 2, "name": "Eri", "set": "TEF", "number": "146"},
+        { "count": 2, "name": "Eri", "set": "TEF", "number": "146" },
         { "count": 2, "name": "Boss’s Orders (Ghetsis)", "set": "PAL", "number": "172" },
         { "count": 1, "name": "Professor Turo's Scenario", "set": "PAR", "number": "171" },
         { "count": 1, "name": "Roxanne", "set": "ASR", "number": "150" },
@@ -36,15 +36,178 @@ let masters = [
         { "count": 1, "name": "Nest Ball", "set": "PAF", "number": "84" },
         { "count": 1, "name": "Counter Catcher", "set": "PAR", "number": "160" },
         { "count": 1, "name": "Lost Vacuum", "set": "CRZ", "number": "135" },
-        {"count": 1, "name": "Pal Pad", "set": "SVI", "number": "182"},
+        { "count": 1, "name": "Pal Pad", "set": "SVI", "number": "182" },
         { "count": 1, "name": "Prime Catcher", "set": "TEF", "number": "157" },
         { "count": 1, "name": "Forest Seal Stone", "set": "SIT", "number": "156" },
         { "count": 1, "name": "Defiance Band", "set": "SVI", "number": "169" },
         { "count": 1, "name": "Collapsed Stadium", "set": "BRS", "number": "137" },
-    ],
-    "energy": [
+      ],
+      "energy": [
         { "count": 6, "name": "Basic Fire Energy", "set": "SVE", "number": "2" },
-]
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League Final Season",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/hyphen.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League Final Season",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/hyphen.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League Final Season",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/hyphen.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League Final Season",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/hyphen.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League Final Season",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/hyphen.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League Final Season",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/hyphen.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "--",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League Final Season",
+    sprite1: "../../../../assets/sprites/blank.png",
+    sprite2: "../../../../assets/sprites/hyphen.png",
+    // list: '../../../../assets/list-icon.png',
+    list: '../../../../assets/sprites/blank.png',
+    "decklist": {
+      "pokemon": [
+        { "count": 3, "name": "eeee", "number": "79", "set": "FST" },
+      ], "trainer": [
+        { "count": 4, "name": "eeee", "number": "190", "set": "SVI" },
+      ], "energy": [
+        { "count": 7, "name": "Water Energy", "number": "3", "set": "SVE" },
+      ]
+    }
+  },
+  {
+    "firstName": "Salvador",
+    flag: '../../../../assets/flags/korea.png',
+    "event": "2024 Korean League Final Season",
+    sprite1: "../../../../assets/sprites/lugia.png",
+    sprite2: "../../../../assets/sprites/archeops.png",
+    list: '../../../../assets/list-icon.png',
+    "decklist": {
+      "pokemon": [
+        {"count": 3, "name": "Lugia VSTAR", "set": "SIT", "number": "139"},
+        {"count": 3, "name": "Lugia V", "set": "SIT", "number": "138"},
+        {"count": 4, "name": "Archeops", "set": "SIT", "number": "147"},
+        {"count": 3, "name": "Cinccino", "set": "TEF", "number": "137"},
+        {"count": 4, "name": "Minccino", "set": "BRS", "number": "124"},
+        {"count": 1, "name": "Snorlax", "set": "LOR", "number": "143"},
+        {"count": 1, "name": "Lumineon V", "set": "BRS", "number": "40"},
+            ], "trainer": [
+              {"count": 3, "name": "Professor's Research", "set": "PAF", "number": "87"},
+              {"count": 3, "name": "Boss’s Orders (Ghetsis)", "set": "PAL", "number": "172"},
+              {"count": 3, "name": "Iono", "set": "PAF", "number": "80"},
+              {"count": 1, "name": "Serena", "set": "SIT", "number": "164"},
+              {"count": 1, "name": "Jacq", "set": "SVI", "number": "175"},
+              {"count": 4, "name": "Ultra Ball", "set": "PAF", "number": "91"},
+              {"count": 4, "name": "Capturing Aroma", "set": "SIT", "number": "153"},
+              {"count": 3, "name": "Great Ball", "set": "PAL", "number": "183"},
+              {"count": 1, "name": "Master Ball", "set": "TEF", "number": "153"},
+              {"count": 1, "name": "Collapsed Stadium", "set": "BRS", "number": "137"},
+            ], "energy": [
+        {"count": 4, "name": "Gift Energy", "set": "LOR", "number": "171"},
+        {"count": 4, "name": "Jet Energy", "set": "PAL", "number": "190"},
+        {"count": 3, "name": "Mist Energy", "set": "TEF", "number": "161"},
+        {"count": 3, "name": "Double Turbo Energy", "set": "BRS", "number": "151"},
+        {"count": 2, "name": "V Guard Energy", "set": "SIT", "number": "169"},
+        {"count": 1, "name": "Therapeutic Energy", "set": "PAL", "number": "193"},
+      ]
     }
   },
   {
