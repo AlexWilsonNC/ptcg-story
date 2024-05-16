c9ffbec5dcbf40ecbb3321d5cd3b6cea659003b5
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-05-16 16:20:43 -0400

    may 12 events

--- tournaments/year/2024/thailand-championships/results.js
+++ tournaments/year/2024/thailand-championships/results.js
@@ -1,10 +1,334 @@
 let masters = [
-    {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
-        "event": "2024 eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+{
+        "firstName": "Pacharadanai Boonsup",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Napapol Chanvitan",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "Giratina VSTAR", "set": "LOR", "number": "131" },
+                { "count": 3, "name": "Giratina V", "set": "LOR", "number": "130" },
+                { "count": 4, "name": "Comfey", "set": "LOR", "number": "79" },
+                { "count": 2, "name": "Cramorant", "set": "LOR", "number": "50" },
+                { "count": 1, "name": "Sableye", "set": "LOR", "number": "70" },
+                { "count": 1, "name": "Radiant Greninja", "set": "ASR", "number": "46" },
+                {"count": 1, "name": "Bloodmoon Ursaluna ex", "set": "FR", "number": "28"},
+            ],
+            "trainer": [
+                { "count": 4, "name": "Colress's Experiment", "set": "LOR", "number": "155" },
+                { "count": 2, "name": "Boss’s Orders (Ghetsis)", "set": "PAL", "number": "172" },
+                { "count": 1, "name": "Roxanne", "set": "ASR", "number": "150" },
+                {"count": 1, "name": "Iono", "set": "PAF", "number": "80"},
+                { "count": 4, "name": "Mirage Gate", "set": "LOR", "number": "163" },
+                { "count": 4, "name": "Nest Ball", "set": "PAF", "number": "84" },
+                {"count": 4, "name": "Switch Cart", "set": "ASR", "number": "154"},
+                { "count": 2, "name": "Buddy-Buddy Poffin", "set": "TEF", "number": "144" },
+                { "count": 2, "name": "Super Rod", "set": "PAL", "number": "188" },
+                { "count": 2, "name": "Counter Catcher", "set": "PAR", "number": "160" },
+                { "count": 1, "name": "Lost Vacuum", "set": "CRZ", "number": "135" },
+                {"count": 1, "name": "Canceling Cologne", "set": "ASR", "number": "136"},
+                {"count": 1, "name": "Unfair Stamp", "set": "FR", "number": "27"},
+                { "count": 1, "name": "Rescue Board", "set": "TEF", "number": "159" },
+                {"count": 1, "name": "Forest Seal Stone", "set": "SIT", "number": "156"},
+                { "count": 1, "name": "Temple of Sinnoh", "set": "ASR", "number": "155" },
+            ],
+            "energy": [
+                { "count": 4, "name": "Basic Psychic Energy", "set": "SVE", "number": "5" },
+                { "count": 3, "name": "Basic Grass Energy", "set": "SVE", "number": "1" },
+                { "count": 3, "name": "Basic Water Energy", "set": "SVE", "number": "3" },
+                { "count": 3, "name": "Jet Energy", "set": "PAL", "number": "190" },
+            ]
+        }
+    },
+{
+        "firstName": "Thanthawat Chujitngamsuwong",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/sableye.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Putt Sakdhnagool",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Supachok Kongsuwan",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Tunyavat Wannaprasert",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/hyphen.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Naphat Dangsripon",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Phrutthinun Lerksomboon",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/koraidon.png',
+        sprite2: '../../../../assets/sprites/roaring-moon.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Jetnipat Likitsombat",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/sableye.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Athitat Sooksri",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/lugia.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Pipatphoom Rattanakaianan",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Solos Kammalet",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/lugia.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Thongdech Sakprasard",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Poonyapat Leelalumlert",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/sableye.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Amarin Saksit",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Najohn Kitsowwapak",
+        flag: '../../../../assets/flags/thailand.png',
+        "event": "2024 Thailand Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
