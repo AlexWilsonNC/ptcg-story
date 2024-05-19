fe7d72a9b910b9de51a131da72fcd93752d6312b
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-05-19 15:40:44 -0400

    may 19 events

--- tournaments/year/2024/singapore-championships/results.js
+++ tournaments/year/2024/singapore-championships/results.js
@@ -1,10 +1,371 @@
 let masters = [
     {
-        "firstName": "--",
+        "firstName": "Ao Yichen",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Bjorn Soh Cai Wei",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/lugia.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "Archeops", "set": "SIT", "number": "147" },
+                { "count": 4, "name": "Lugia V", "set": "SIT", "number": "138" },
+                { "count": 3, "name": "Cinccino", "set": "TEF", "number": "137" },
+                { "count": 4, "name": "Minccino", "set": "BRS", "number": "124" },
+                { "count": 3, "name": "Lugia VSTAR", "set": "SIT", "number": "139" },
+                { "count": 1, "name": "Snorlax", "set": "LOR", "number": "143" },
+                { "count": 1, "name": "Flutter Mane", "set": "TEF", "number": "78" },
+                { "count": 1, "name": "Lumineon V", "set": "BRS", "number": "40" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "Professor's Research", "set": "PAF", "number": "87" },
+                { "count": 3, "name": "Iono", "set": "PAF", "number": "80" },
+                { "count": 3, "name": "Boss’s Orders (Ghetsis)", "set": "PAL", "number": "172" },
+                { "count": 1, "name": "Jacq", "set": "SVI", "number": "175" },
+                { "count": 4, "name": "Ultra Ball", "set": "PAF", "number": "91" },
+                { "count": 3, "name": "Capturing Aroma", "set": "SIT", "number": "153" },
+                { "count": 2, "name": "Great Ball", "set": "PAL", "number": "183" },
+                { "count": 1, "name": "Hero's Cape", "set": "TEF", "number": "152" },
+                { "count": 1, "name": "Mesagoza", "set": "SVI", "number": "178" },
+            ],
+            "energy": [
+                { "count": 4, "name": "Double Turbo Energy", "set": "BRS", "number": "151" },
+                { "count": 4, "name": "Gift Energy", "set": "LOR", "number": "171" },
+                { "count": 4, "name": "Jet Energy", "set": "PAL", "number": "190" },
+                { "count": 2, "name": "Mist Energy", "set": "TEF", "number": "161" },
+                { "count": 2, "name": "V Guard Energy", "set": "SIT", "number": "169" },
+                { "count": 1, "name": "Therapeutic Energy", "set": "PAL", "number": "193" },
+            ]
+        }
+    },
+    {
+        "firstName": "Benjamin Isa Roberts",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/roaring-moon.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Gerald Ong Wei Kai",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/roaring-moon.png',
+        sprite2: '../../../../assets/sprites/dudunsparce.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Leong Jun Hao",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Dave Wong Wei Jie",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Foo Ming Wei",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/roaring-moon.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Kaizer Sim",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/roaring-moon.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "Comfey", "set": "LOR", "number": 79 },
+                { "count": 1, "name": "Roaring Moon ex", "set": "PAR", "number": 124 },
+                { "count": 1, "name": "Iron Hands ex", "set": "PAR", "number": 70 },
+                { "count": 1, "name": "Raikou V", "set": "BRS", "number": 48 },
+                {"count": 1, "name": "Hoopa ex", "set": "PAR", "number": "98"},
+                {"count": 1, "name": "Mew ex", "set": "MEW", "number": "151"},
+                { "count": 1, "name": "Sableye", "set": "LOR", "number": 70 },
+                { "count": 1, "name": "Cramorant", "set": "LOR", "number": 50 },
+                { "count": 1, "name": "Radiant Greninja", "set": "ASR", "number": 46 },
+                {"count": 1, "name": "Iron Bundle", "set": "PAR", "number": "56"},
+                { "count": 1, "name": "Manaphy", "set": "BRS", "number": 41 }
+            ], "trainer": [
+                { "count": 4, "name": "Colress's Experiment", "set": "LOR", "number": 155 },
+                { "count":1, "name": "Boss’s Orders (Ghetsis)", "set": "PAL", "number": 172 },
+                { "count": 1, "name": "Roxanne", "set": "ASR", "number": 150 },
+                { "count": 4, "name": "Buddy-Buddy Poffin", "set": "TEF", "number": 144 },
+                { "count": 4, "name": "Nest Ball", "set": "SVI", "number": 181 },
+                { "count": 4, "name": "Mirage Gate", "set": "LOR", "number": 163 },
+                { "count": 4, "name": "Super Rod", "set": "PAL", "number": 188 },
+                { "count": 4, "name": "Switch Cart", "set": "ASR", "number": 154 },
+                { "count": 3, "name": "Switch", "set": "SVI", "number": 194 },
+                { "count": 1, "name": "Lost Vacuum", "set": "CRZ", "number": 135 },
+                { "count": 1, "name": "Hisuian Heavy Ball", "set": "ASR", "number": 146 },
+                { "count": 1, "name": "Prime Catcher", "set": "TEF", "number": 157 },
+                { "count": 1, "name": "Forest Seal Stone", "set": "SIT", "number": 156 },
+                { "count": 1, "name": "Rescue Board", "set": "TEF", "number": 159 },
+                { "count": 2, "name": "Town Store", "set": "OBF", "number": 196 },
+            ], "energy": [
+                { "count": 4, "name": "Basic Darkness Energy", "set": "SVE", "number": 7 },
+                { "count": 3, "name": "Basic Water Energy", "set": "SVE", "number": 3 },
+                { "count": 2, "name": "Basic Lightning Energy", "set": "SVE", "number": 4 },
+                { "count": 1, "name": "Basic Psychic Energy", "set": "SVE", "number": 5 }]
+        }
+    },
+    {
+        "firstName": "Chan Bing Jun",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/arceus.png',
+        sprite2: '../../../../assets/sprites/armarouge.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Dennis Piay",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Ng Zheng Wei Gerrard",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Wayne Tan Wei Peng",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/koraidon.png',
+        sprite2: '../../../../assets/sprites/roaring-moon.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                {"count": 4, "name": "Roaring Moon", "set": "TEF", "number": "109"},
+                {"count": 4, "name": "Flutter Mane", "set": "TEF", "number": "78"},
+                {"count": 3, "name": "Koraidon", "set": "TEF", "number": "119"},
+                {"count": 1, "name": "Cobalion", "set": "SIT", "number": "126"},
+                {"count": 1, "name": "Radiant Greninja", "set": "ASR", "number": "46"},
+                        ],
+            "trainer": [
+                {"count": 4, "name": "Professor Sada's Vitality", "set": "PAR", "number": "170"},
+                {"count": 4, "name": "Explorer's Guidance", "set": "TEF", "number": "147"},
+                {"count": 1, "name": "Boss’s Orders (Ghetsis)", "set": "PAL", "number": "172"},
+                {"count": 1, "name": "Penny", "set": "SVI", "number": "183"},
+                {"count": 4, "name": "Pokégear 3.0", "set": "SVI", "number": "186"},
+                {"count": 4, "name": "Earthen Vessel", "set": "PAR", "number": "163"},
+                {"count": 3, "name": "Ultra Ball", "set": "SVI", "number": "196"},
+                {"count": 3, "name": "Nest Ball", "set": "SVI", "number": "181"},
+                {"count": 3, "name": "Counter Catcher", "set": "PAR", "number": "160"},
+                {"count": 2, "name": "Super Rod", "set": "PAL", "number": "188"},
+                {"count": 1, "name": "Pal Pad", "set": "SVI", "number": "182"},
+                {"count": 1, "name": "Superior Energy Retrieval", "set": "PAL", "number": "189"},
+                {"count": 1, "name": "Hisuian Heavy Ball", "set": "ASR", "number": "146"},
+                {"count": 1, "name": "Awakening Drum", "set": "TEF", "number": "141"},
+                {"count": 4, "name": "Ancient Booster Energy Capsule", "set": "TEF", "number": "140"},
+                {"count": 3, "name": "PokéStop", "set": "PGO", "number": "68"},
+            ],
+            "energy": [
+                {"count": 6, "name": "Basic Darkness Energy", "set": "SVE", "number": "7"},
+                {"count": 1, "name": "Basic Fighting Energy", "set": "SVE", "number": "6"},
+            ]
+        }
+    },
+    {
+        "firstName": "Bryan Quah",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Klive Aw Jun Jie",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Jordan Lau",
+        flag: '../../../../assets/flags/singapore.png',
+        "event": "2024 Singapore Championships",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Igloo",
         flag: '../../../../assets/flags/singapore.png',
         "event": "2024 Singapore Championships",
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/hyphen.png',
+        sprite2: '../../../../assets/sprites/great-tusk.png',
         // list: '../../../../assets/list-icon.png',
         list: '../../../../assets/sprites/blank.png',
         "decklist": {
