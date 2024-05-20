81fbac5f6d86787edb6a4bbc00e6dd8cee63db6d
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-05-20 12:03:31 -0400

    may 20 events

---
+++ tournaments/year/2024/santiago/results.js
@@ -0,0 +1,108 @@
+let masters = [
+{
+        "firstName": "Fernando Lopez",
+        flag: '../../../../assets/flags/chile.png',
+        "event": "2024 Santiago Regionals",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/roaring-moon.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Dalton Acchetta",
+        flag: '../../../../assets/flags/brazil.png',
+        "event": "2024 Santiago Regionals",
+        sprite1: '../../../../assets/sprites/arceus.png',
+        sprite2: '../../../../assets/sprites/giratina-origin.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":4,"name":"Arceus V","set":"BRS","number":122},{"count":4,"name":"Arceus VSTAR","set":"BRS","number":123},{"count":3,"name":"Bidoof","set":"CRZ","number":111},{"count":3,"name":"Bibarel","set":"BRS","number":121},{"count":1,"name":"Giratina V","set":"LOR","number":130},{"count":1,"name":"Giratina VSTAR","set":"LOR","number":131},{"count":1,"name":"Radiant Charizard","set":"CRZ","number":20},{"count":1,"name":"Iron Leaves ex","set":"TEF","number":25},{"count":1,"name":"Skwovet","set":"SVI","number":151}],"trainer":[{"count":4,"name":"Iono","set":"PAF","number":80},{"count":3,"name":"Boss's Orders","set":"PAL","number":172},{"count":2,"name":"Judge","set":"SVI","number":176},{"count":1,"name":"Arven","set":"OBF","number":186},{"count":4,"name":"Nest Ball","set":"PAF","number":84},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":2,"name":"Super Rod","set":"PAL","number":188},{"count":1,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":1,"name":"Switch","set":"SVI","number":194},{"count":1,"name":"Counter Catcher","set":"PAR","number":160},{"count":1,"name":"Lost Vacuum","set":"CRZ","number":135},{"count":1,"name":"Maximum Belt","set":"TEF","number":154},{"count":1,"name":"Choice Belt","set":"PAL","number":176},{"count":1,"name":"Magma Basin","set":"BRS","number":144}],"energy":[{"count":4,"name":"Double Turbo Energy","set":"BRS","number":151},{"count":4,"name":"Grass","set":"Energy","number":1},{"count":3,"name":"Fire","set":"Energy","number":2},{"count":2,"name":"Psychic","set":"Energy","number":5},{"count":1,"name":"Jet Energy","set":"PAL","number":190}]}
+
+    },
+{
+        "firstName": "Giovanni Peragallo",
+        flag: '../../../../assets/flags/chile.png',
+        "event": "2024 Santiago Regionals",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":2,"name":"Ralts","set":"ASR","number":60},{"count":2,"name":"Ralts","set":"SIT","number":67},{"count":4,"name":"Kirlia","set":"SIT","number":68},{"count":2,"name":"Gardevoir ex","set":"PAF","number":29},{"count":1,"name":"Drifloon","set":"SVI","number":89},{"count":1,"name":"Scream Tail","set":"PAR","number":86},{"count":1,"name":"Flutter Mane","set":"TEF","number":78},{"count":1,"name":"Mimikyu","set":"PAF","number":37},{"count":1,"name":"Radiant Greninja","set":"ASR","number":46},{"count":1,"name":"Manaphy","set":"BRS","number":41},{"count":1,"name":"Jirachi","set":"PAR","number":126}],"trainer":[{"count":4,"name":"Iono","set":"PAF","number":80},{"count":4,"name":"Arven","set":"OBF","number":186},{"count":1,"name":"Boss's Orders","set":"PAL","number":172},{"count":1,"name":"Eri","set":"TEF","number":146},{"count":1,"name":"Professor Turo's Scenario","set":"PAR","number":171},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":3,"name":"Ultra Ball","set":"PAF","number":91},{"count":3,"name":"Counter Catcher","set":"PAR","number":160},{"count":3,"name":"Super Rod","set":"PAL","number":188},{"count":2,"name":"Nest Ball","set":"PAF","number":84},{"count":1,"name":"Earthen Vessel","set":"PAR","number":163},{"count":1,"name":"Hisuian Heavy Ball","set":"ASR","number":146},{"count":1,"name":"Pal Pad","set":"SVI","number":182},{"count":2,"name":"Technical Machine: Evolution","set":"PAR","number":178},{"count":1,"name":"Hero's Cape","set":"TEF","number":152},{"count":1,"name":"Luxurious Cape","set":"PAR","number":166},{"count":1,"name":"Bravery Charm","set":"PAL","number":173}],"energy":[{"count":9,"name":"Psychic","set":"Energy","number":5}]}
+    },
+{
+        "firstName": "Angel Aranibar Huamaní",
+        flag: '../../../../assets/flags/peru.png',
+        "event": "2024 Santiago Regionals",
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+{
+        "firstName": "Yerco Valencia",
+        flag: '../../../../assets/flags/chile.png',
+        "event": "2024 Santiago Regionals",
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":3,"name":"Frigibax","set":"PAL","number":57},{"count":2,"name":"Baxcalibur","set":"PAL","number":60},{"count":2,"name":"Bidoof","set":"CRZ","number":111},{"count":2,"name":"Bibarel","set":"BRS","number":121},{"count":2,"name":"Chien-Pao ex","set":"PAL","number":61},{"count":1,"name":"Iron Hands ex","set":"PAR","number":70},{"count":1,"name":"Radiant Greninja","set":"ASR","number":46},{"count":1,"name":"Iron Bundle","set":"PAR","number":56}],"trainer":[{"count":4,"name":"Irida","set":"ASR","number":147},{"count":1,"name":"Ciphermaniac's Codebreaking","set":"TEF","number":145},{"count":1,"name":"Cyllene","set":"ASR","number":138},{"count":4,"name":"Superior Energy Retrieval","set":"PAL","number":189},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":4,"name":"Nest Ball","set":"PAF","number":84},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":4,"name":"Rare Candy","set":"PAF","number":89},{"count":3,"name":"Super Rod","set":"PAL","number":188},{"count":1,"name":"Earthen Vessel","set":"PAR","number":163},{"count":1,"name":"Hisuian Heavy Ball","set":"ASR","number":146},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Canceling Cologne","set":"ASR","number":136},{"count":1,"name":"Lost Vacuum","set":"CRZ","number":135},{"count":2,"name":"PokéStop","set":"PGO","number":68}],"energy":[{"count":8,"name":"Water","set":"Energy","number":3},{"count":2,"name":"Lightning","set":"Energy","number":4}]}
+    },
+{
+        "firstName": "Vinícius Fernandez",
+        flag: '../../../../assets/flags/brazil.png',
+        "event": "2024 Santiago Regionals",
+        sprite1: '../../../../assets/sprites/banette.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":4,"name":"Shuppet","set":"SVI","number":87},{"count":3,"name":"Banette ex","set":"SVI","number":88},{"count":1,"name":"Banette","set":"LOR","number":73},{"count":3,"name":"Ralts","set":"SIT","number":67},{"count":3,"name":"Kirlia","set":"SIT","number":68},{"count":2,"name":"Gardevoir ex","set":"PAF","number":29},{"count":2,"name":"Mew ex","set":"MEW","number":151},{"count":1,"name":"Radiant Alakazam","set":"SIT","number":59},{"count":1,"name":"Spiritomb","set":"PAL","number":89}],"trainer":[{"count":4,"name":"Iono","set":"PAF","number":80},{"count":2,"name":"Boss's Orders","set":"PAL","number":172},{"count":2,"name":"Professor Turo's Scenario","set":"PAR","number":171},{"count":1,"name":"Professor's Research","set":"PAF","number":88},{"count":1,"name":"Ciphermaniac's Codebreaking","set":"TEF","number":145},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":4,"name":"Nest Ball","set":"PAF","number":84},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":4,"name":"Trekking Shoes","set":"CRZ","number":145},{"count":2,"name":"Counter Catcher","set":"PAR","number":160},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Super Rod","set":"PAL","number":188},{"count":2,"name":"Calamitous Wasteland","set":"PAL","number":175}],"energy":[{"count":8,"name":"Psychic","set":"Energy","number":5}]}
+    },
+{
+        "firstName": "João Requena",
+        flag: '../../../../assets/flags/brazil.png',
+        "event": "2024 Santiago Regionals",
+        sprite1: '../../../../assets/sprites/banette.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":4,"name":"Shuppet","set":"SVI","number":87},{"count":3,"name":"Banette ex","set":"SVI","number":88},{"count":1,"name":"Banette","set":"LOR","number":73},{"count":3,"name":"Ralts","set":"SIT","number":67},{"count":3,"name":"Kirlia","set":"SIT","number":68},{"count":2,"name":"Gardevoir ex","set":"PAF","number":29},{"count":2,"name":"Mew ex","set":"MEW","number":151},{"count":1,"name":"Radiant Alakazam","set":"SIT","number":59},{"count":1,"name":"Spiritomb","set":"PAL","number":89}],"trainer":[{"count":4,"name":"Iono","set":"PAF","number":80},{"count":2,"name":"Professor's Research","set":"PAF","number":88},{"count":2,"name":"Boss's Orders","set":"PAL","number":172},{"count":2,"name":"Professor Turo's Scenario","set":"PAR","number":171},{"count":1,"name":"Ciphermaniac's Codebreaking","set":"TEF","number":145},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":4,"name":"Nest Ball","set":"PAF","number":84},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":3,"name":"Trekking Shoes","set":"CRZ","number":145},{"count":2,"name":"Counter Catcher","set":"PAR","number":160},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Super Rod","set":"PAL","number":188},{"count":2,"name":"Calamitous Wasteland","set":"PAL","number":175}],"energy":[{"count":8,"name":"Psychic","set":"Energy","number":5}]}
+    },
+{
+        "firstName": "Jonathan Fabrizio Bellucci Lanciano",
+        flag: '../../../../assets/flags/argentina.png',
+        "event": "2024 Santiago Regionals",
+        sprite1: '../../../../assets/sprites/arceus.png',
+        sprite2: '../../../../assets/sprites/giratina-origin.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
