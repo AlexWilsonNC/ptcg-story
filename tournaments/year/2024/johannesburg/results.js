61ff06615091711166e5fbdc40ef064af2b5b9b4
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-05-29 14:22:13 -0400

    la

--- tournaments/year/2024/johannesburg/results.js
+++ tournaments/year/2024/johannesburg/results.js
@@ -1,22 +1,74 @@
 let masters = [
     {
-        "firstName": "--",
-        flag: '../../../../assets/flags/unknown.png',
-        "event": "2024 eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+        "firstName": "Jonah Alter",
+        flag: '../../../../assets/flags/south-africa.png',
+        "event": "2024 Johannesburg Special Event",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":3,"name":"Charmander","set":"OBF","number":26},{"count":1,"name":"Charmander","set":"PAF","number":7},{"count":2,"name":"Charmeleon","set":"PAF","number":8},{"count":3,"name":"Charizard ex","set":"PAF","number":54},{"count":2,"name":"Pidgey","set":"MEW","number":16},{"count":2,"name":"Pidgeot ex","set":"OBF","number":164},{"count":1,"name":"Bidoof","set":"CRZ","number":111},{"count":1,"name":"Bibarel","set":"BRS","number":121},{"count":1,"name":"Rotom V","set":"CRZ","number":45},{"count":1,"name":"Regieleki","set":"ASR","number":51},{"count":1,"name":"Lumineon V","set":"BRS","number":40},{"count":1,"name":"Radiant Charizard","set":"CRZ","number":20},{"count":1,"name":"Manaphy","set":"BRS","number":41}],"trainer":[{"count":3,"name":"Iono","set":"PAF","number":80},{"count":3,"name":"Arven","set":"OBF","number":186},{"count":2,"name":"Boss's Orders","set":"PAL","number":172},{"count":1,"name":"Eri","set":"TEF","number":146},{"count":1,"name":"Professor Turo's Scenario","set":"PAR","number":171},{"count":1,"name":"Roxanne","set":"ASR","number":150},{"count":4,"name":"Rare Candy","set":"PAF","number":89},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":2,"name":"Super Rod","set":"PAL","number":188},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Counter Catcher","set":"PAR","number":160},{"count":1,"name":"Nest Ball","set":"PAF","number":84},{"count":1,"name":"Lost Vacuum","set":"CRZ","number":135},{"count":1,"name":"Pal Pad","set":"SVI","number":182},{"count":1,"name":"Forest Seal Stone","set":"SIT","number":156},{"count":1,"name":"Defiance Band","set":"SVI","number":169},{"count":1,"name":"Technical Machine: Devolution","set":"PAR","number":177},{"count":1,"name":"Collapsed Stadium","set":"BRS","number":137}],"energy":[{"count":6,"name":"Fire","set":"Energy","number":2}]}
+    },
+    {
+        "firstName": "Prenesh Naidoo",
+        flag: '../../../../assets/flags/south-africa.png',
+        "event": "2024 Johannesburg Special Event",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":3,"name":"Charmander","set":"OBF","number":26},{"count":1,"name":"Charmander","set":"PAF","number":7},{"count":2,"name":"Charmeleon","set":"PAF","number":8},{"count":3,"name":"Charizard ex","set":"PAF","number":54},{"count":2,"name":"Pidgey","set":"MEW","number":16},{"count":2,"name":"Pidgeot ex","set":"OBF","number":164},{"count":1,"name":"Bidoof","set":"CRZ","number":111},{"count":1,"name":"Bibarel","set":"BRS","number":121},{"count":1,"name":"Rotom V","set":"CRZ","number":45},{"count":1,"name":"Regieleki","set":"ASR","number":51},{"count":1,"name":"Lumineon V","set":"BRS","number":40},{"count":1,"name":"Radiant Charizard","set":"CRZ","number":20},{"count":1,"name":"Manaphy","set":"BRS","number":41}],"trainer":[{"count":3,"name":"Iono","set":"PAF","number":80},{"count":3,"name":"Arven","set":"OBF","number":186},{"count":2,"name":"Boss's Orders","set":"PAL","number":172},{"count":1,"name":"Eri","set":"TEF","number":146},{"count":1,"name":"Professor Turo's Scenario","set":"PAR","number":171},{"count":1,"name":"Roxanne","set":"ASR","number":150},{"count":4,"name":"Rare Candy","set":"PAF","number":89},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":2,"name":"Super Rod","set":"PAL","number":188},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Counter Catcher","set":"PAR","number":160},{"count":1,"name":"Nest Ball","set":"PAF","number":84},{"count":1,"name":"Lost Vacuum","set":"CRZ","number":135},{"count":1,"name":"Pal Pad","set":"SVI","number":182},{"count":1,"name":"Forest Seal Stone","set":"SIT","number":156},{"count":1,"name":"Defiance Band","set":"SVI","number":169},{"count":1,"name":"Technical Machine: Devolution","set":"PAR","number":177},{"count":1,"name":"Collapsed Stadium","set":"BRS","number":137}],"energy":[{"count":6,"name":"Fire","set":"Energy","number":2}]}
+    },
+    {
+        "firstName": "Matthew Vrede",
+        flag: '../../../../assets/flags/south-africa.png',
+        "event": "2024 Johannesburg Special Event",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":3,"name":"Charmander","set":"OBF","number":26},{"count":1,"name":"Charmander","set":"PAF","number":7},{"count":2,"name":"Charmeleon","set":"PAF","number":8},{"count":3,"name":"Charizard ex","set":"PAF","number":54},{"count":2,"name":"Pidgey","set":"MEW","number":16},{"count":2,"name":"Pidgeot ex","set":"OBF","number":164},{"count":1,"name":"Bidoof","set":"CRZ","number":111},{"count":1,"name":"Bibarel","set":"BRS","number":121},{"count":1,"name":"Radiant Charizard","set":"CRZ","number":20},{"count":1,"name":"Cleffa","set":"OBF","number":80},{"count":1,"name":"Manaphy","set":"BRS","number":41},{"count":1,"name":"Lumineon V","set":"BRS","number":40},{"count":1,"name":"Rotom V","set":"CRZ","number":45}],"trainer":[{"count":3,"name":"Arven","set":"OBF","number":186},{"count":3,"name":"Iono","set":"PAF","number":80},{"count":2,"name":"Boss's Orders","set":"PAL","number":172},{"count":2,"name":"Professor Turo's Scenario","set":"PAR","number":171},{"count":1,"name":"Team Yell's Cheer","set":"BRS","number":149},{"count":1,"name":"Roxanne","set":"ASR","number":150},{"count":4,"name":"Rare Candy","set":"PAF","number":89},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":2,"name":"Super Rod","set":"PAL","number":188},{"count":1,"name":"Nest Ball","set":"PAF","number":84},{"count":1,"name":"Lost Vacuum","set":"CRZ","number":135},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Counter Catcher","set":"PAR","number":160},{"count":1,"name":"Defiance Band","set":"SVI","number":169},{"count":1,"name":"Forest Seal Stone","set":"SIT","number":156},{"count":1,"name":"Choice Belt","set":"PAL","number":176},{"count":1,"name":"Collapsed Stadium","set":"BRS","number":137}],"energy":[{"count":6,"name":"Fire","set":"Energy","number":2}]}
+    },
+    {
+        "firstName": "Byron Lilford",
+        flag: '../../../../assets/flags/south-africa.png',
+        "event": "2024 Johannesburg Special Event",
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":3,"name":"Frigibax","set":"PAL","number":57},{"count":2,"name":"Baxcalibur","set":"PAL","number":60},{"count":2,"name":"Pidgey","set":"MEW","number":16},{"count":2,"name":"Pidgeot ex","set":"OBF","number":164},{"count":2,"name":"Chien-Pao ex","set":"PAL","number":61},{"count":1,"name":"Iron Hands ex","set":"PAR","number":70},{"count":1,"name":"Radiant Greninja","set":"ASR","number":46},{"count":1,"name":"Iron Bundle","set":"PAR","number":56},{"count":1,"name":"Mew ex","set":"MEW","number":151},{"count":1,"name":"Minior","set":"PAR","number":99}],"trainer":[{"count":4,"name":"Irida","set":"ASR","number":147},{"count":1,"name":"Ciphermaniac's Codebreaking","set":"TEF","number":145},{"count":1,"name":"Cyllene","set":"ASR","number":138},{"count":4,"name":"Superior Energy Retrieval","set":"PAL","number":189},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":4,"name":"Nest Ball","set":"PAF","number":84},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":4,"name":"Rare Candy","set":"PAF","number":89},{"count":3,"name":"Super Rod","set":"PAL","number":188},{"count":1,"name":"Earthen Vessel","set":"PAR","number":163},{"count":1,"name":"Hisuian Heavy Ball","set":"ASR","number":146},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Canceling Cologne","set":"ASR","number":136},{"count":2,"name":"PokéStop","set":"PGO","number":68}],"energy":[{"count":8,"name":"Water","set":"Energy","number":3},{"count":1,"name":"Lightning","set":"Energy","number":4}]}
+    },
+    {
+        "firstName": "Abe Morales",
+        flag: '../../../../assets/flags/south-africa.png',
+        "event": "2024 Johannesburg Special Event",
+        sprite1: '../../../../assets/sprites/arceus.png',
+        sprite2: '../../../../assets/sprites/giratina-origin.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":4,"name":"Arceus V","set":"BRS","number":122},{"count":3,"name":"Arceus VSTAR","set":"BRS","number":123},{"count":3,"name":"Bidoof","set":"CRZ","number":111},{"count":3,"name":"Bibarel","set":"BRS","number":121},{"count":2,"name":"Giratina V","set":"LOR","number":130},{"count":2,"name":"Giratina VSTAR","set":"LOR","number":131},{"count":2,"name":"Iron Leaves ex","set":"TEF","number":25},{"count":1,"name":"Skwovet","set":"SVI","number":151},{"count":1,"name":"Relicanth","set":"TEF","number":84}],"trainer":[{"count":3,"name":"Judge","set":"SVI","number":176},{"count":3,"name":"Iono","set":"PAF","number":80},{"count":2,"name":"Boss's Orders","set":"PAL","number":172},{"count":1,"name":"Eri","set":"TEF","number":146},{"count":1,"name":"Cheren's Care","set":"BRS","number":134},{"count":4,"name":"Nest Ball","set":"PAF","number":84},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":1,"name":"Switch","set":"SVI","number":194},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Lost Vacuum","set":"CRZ","number":135},{"count":1,"name":"Pal Pad","set":"SVI","number":182},{"count":3,"name":"Lost City","set":"LOR","number":161}],"energy":[{"count":6,"name":"Grass","set":"Energy","number":1},{"count":4,"name":"Double Turbo Energy","set":"BRS","number":151},{"count":3,"name":"Psychic","set":"Energy","number":5},{"count":1,"name":"Jet Energy","set":"PAL","number":190}]}
+    },
+    {
+        "firstName": "Mahomed Mahomed",
+        flag: '../../../../assets/flags/south-africa.png',
+        "event": "2024 Johannesburg Special Event",
+        sprite1: '../../../../assets/sprites/giratina-origin.png',
+        sprite2: '../../../../assets/sprites/comfey.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":4,"name":"Comfey","set":"LOR","number":79},{"count":3,"name":"Giratina V","set":"LOR","number":130},{"count":3,"name":"Giratina VSTAR","set":"LOR","number":131},{"count":1,"name":"Sableye","set":"LOR","number":70},{"count":1,"name":"Cramorant","set":"LOR","number":50},{"count":1,"name":"Radiant Greninja","set":"ASR","number":46},{"count":1,"name":"Iron Leaves ex","set":"TEF","number":25},{"count":1,"name":"Manaphy","set":"BRS","number":41},{"count":1,"name":"Spiritomb","set":"PAL","number":89}],"trainer":[{"count":4,"name":"Colress's Experiment","set":"LOR","number":155},{"count":2,"name":"Roxanne","set":"ASR","number":150},{"count":1,"name":"Boss's Orders","set":"PAL","number":172},{"count":4,"name":"Mirage Gate","set":"LOR","number":163},{"count":4,"name":"Nest Ball","set":"PAF","number":84},{"count":2,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":2,"name":"Super Rod","set":"PAL","number":188},{"count":2,"name":"Pokégear 3.0","set":"SVI","number":186},{"count":2,"name":"Switch Cart","set":"ASR","number":154},{"count":2,"name":"Switch","set":"SVI","number":194},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Counter Catcher","set":"PAR","number":160},{"count":1,"name":"Lost Vacuum","set":"CRZ","number":135},{"count":1,"name":"Rescue Board","set":"TEF","number":159},{"count":1,"name":"Forest Seal Stone","set":"SIT","number":156},{"count":1,"name":"Temple of Sinnoh","set":"ASR","number":155}],"energy":[{"count":4,"name":"Grass","set":"Energy","number":1},{"count":3,"name":"Psychic","set":"Energy","number":5},{"count":3,"name":"Water","set":"Energy","number":3},{"count":3,"name":"Jet Energy","set":"PAL","number":190}]}
+    },
+    {
+        "firstName": "Aaqil Sedien",
+        flag: '../../../../assets/flags/south-africa.png',
+        "event": "2024 Johannesburg Special Event",
+        sprite1: '../../../../assets/sprites/charizard.png',
+        sprite2: '../../../../assets/sprites/pidgeot.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":3,"name":"Charmander","set":"OBF","number":26},{"count":1,"name":"Charmander","set":"PAF","number":7},{"count":1,"name":"Charmeleon","set":"PAF","number":8},{"count":1,"name":"Charmeleon","set":"OBF","number":27},{"count":3,"name":"Charizard ex","set":"PAF","number":54},{"count":2,"name":"Pidgey","set":"MEW","number":16},{"count":2,"name":"Pidgeot ex","set":"OBF","number":164},{"count":1,"name":"Bidoof","set":"CRZ","number":111},{"count":1,"name":"Bibarel","set":"BRS","number":121},{"count":1,"name":"Radiant Charizard","set":"CRZ","number":20},{"count":1,"name":"Rotom V","set":"CRZ","number":45},{"count":1,"name":"Lumineon V","set":"BRS","number":40},{"count":1,"name":"Cleffa","set":"OBF","number":80},{"count":1,"name":"Manaphy","set":"BRS","number":41}],"trainer":[{"count":3,"name":"Arven","set":"OBF","number":186},{"count":3,"name":"Iono","set":"PAF","number":80},{"count":2,"name":"Boss's Orders","set":"PAL","number":172},{"count":2,"name":"Professor Turo's Scenario","set":"PAR","number":171},{"count":1,"name":"Roxanne","set":"ASR","number":150},{"count":1,"name":"Team Yell's Cheer","set":"BRS","number":149},{"count":4,"name":"Rare Candy","set":"PAF","number":89},{"count":4,"name":"Ultra Ball","set":"PAF","number":91},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":2,"name":"Super Rod","set":"PAL","number":188},{"count":1,"name":"Nest Ball","set":"PAF","number":84},{"count":1,"name":"Prime Catcher","set":"TEF","number":157},{"count":1,"name":"Counter Catcher","set":"PAR","number":160},{"count":1,"name":"Lost Vacuum","set":"CRZ","number":135},{"count":1,"name":"Forest Seal Stone","set":"SIT","number":156},{"count":1,"name":"Defiance Band","set":"SVI","number":169},{"count":1,"name":"Choice Belt","set":"PAL","number":176},{"count":1,"name":"Collapsed Stadium","set":"BRS","number":137}],"energy":[{"count":6,"name":"Fire","set":"Energy","number":2}]}
+    },
+    {
+        "firstName": "Calvin Botha",
+        flag: '../../../../assets/flags/south-africa.png',
+        "event": "2024 Johannesburg Special Event",
         sprite1: '../../../../assets/sprites/blank.png',
-        sprite2: '../../../../assets/sprites/hyphen.png',
-        // list: '../../../../assets/list-icon.png',
-        list: '../../../../assets/sprites/blank.png',
-        "decklist": {
-            "pokemon": [
-                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
-            ],
-            "trainer": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ],
-            "energy": [
-                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
-            ]
-        }
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {"pokemon":[{"count":2,"name":"Ralts","set":"ASR","number":60},{"count":2,"name":"Ralts","set":"SIT","number":67},{"count":4,"name":"Kirlia","set":"SIT","number":68},{"count":2,"name":"Gardevoir ex","set":"PAF","number":29},{"count":2,"name":"Drifloon","set":"SVI","number":89},{"count":2,"name":"Flutter Mane","set":"TEF","number":78},{"count":1,"name":"Scream Tail","set":"PAR","number":86},{"count":1,"name":"Radiant Greninja","set":"ASR","number":46},{"count":1,"name":"Manaphy","set":"BRS","number":41},{"count":1,"name":"Jirachi","set":"PAR","number":126}],"trainer":[{"count":4,"name":"Arven","set":"OBF","number":186},{"count":4,"name":"Iono","set":"PAF","number":80},{"count":1,"name":"Boss's Orders","set":"PAL","number":172},{"count":1,"name":"Professor Turo's Scenario","set":"PAR","number":171},{"count":1,"name":"Eri","set":"TEF","number":146},{"count":4,"name":"Buddy-Buddy Poffin","set":"TEF","number":144},{"count":3,"name":"Ultra Ball","set":"PAF","number":91},{"count":2,"name":"Nest Ball","set":"PAF","number":84},{"count":2,"name":"Super Rod","set":"PAL","number":188},{"count":2,"name":"Counter Catcher","set":"PAR","number":160},{"count":2,"name":"Earthen Vessel","set":"PAR","number":163},{"count":1,"name":"Hisuian Heavy Ball","set":"ASR","number":146},{"count":1,"name":"Pal Pad","set":"SVI","number":182},{"count":2,"name":"Technical Machine: Evolution","set":"PAR","number":178},{"count":1,"name":"Hero's Cape","set":"TEF","number":152},{"count":1,"name":"Luxurious Cape","set":"PAR","number":166},{"count":1,"name":"Bravery Charm","set":"PAL","number":173}],"energy":[{"count":9,"name":"Psychic","set":"Energy","number":5}]}
     },
 ]
