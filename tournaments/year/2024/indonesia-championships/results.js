c34550487be52144d62769dab54da0fd8ba51a87
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-06-04 13:07:16 -0400

    importedenergy gone

---
+++ tournaments/year/2024/indonesia-championships/results.js
@@ -0,0 +1,348 @@
+let masters = [
+    {
+        "firstName": "Darwin Rasubala",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/roaring-moon.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                {"count": 4, "name": "Comfey", "set": "LOR", "number": "79"},
+                {"count": 1, "name": "Sableye", "set": "LOR", "number": "70"},
+                {"count": 1, "name": "Cramorant", "set": "LOR", "number": "50"},
+                {"count": 1, "name": "Raikou V", "set": "BRS", "number": "48"},
+                {"count": 1, "name": "Iron Hands ex", "set": "PAR", "number": "70"},
+                {"count": 1, "name": "Roaring Moon ex", "set": "PAR", "number": "124"},
+                {"count": 1, "name": "Hoopa ex", "set": "PAR", "number": "98"},
+                {"count": 1, "name": "Rotom V", "set": "CRZ", "number": "45"},
+                {"count": 1, "name": "Radiant Greninja", "set": "ASR", "number": "46"},
+                {"count": 1, "name": "Manaphy", "set": "BRS", "number": "41"},
+                        ],
+            "trainer": [
+                {"count": 4, "name": "Colress's Experiment", "set": "LOR", "number": "155"},
+                {"count": 1, "name": "Boss’s Orders (Ghetsis)", "set": "PAL", "number": "172"},
+                {"count": 1, "name": "Roxanne", "set": "ASR", "number": "150"},
+                {"count": 4, "name": "Mirage Gate", "set": "LOR", "number": "163"},
+                {"count": 4, "name": "Nest Ball", "set": "PAF", "number": "84"},
+                {"count": 4, "name": "Switch Cart", "set": "ASR", "number": "154"},
+                {"count": 4, "name": "Super Rod", "set": "PAL", "number": "188"},
+                {"count": 3, "name": "Switch", "set": "SVI", "number": "194"},
+                {"count": 3, "name": "Buddy-Buddy Poffin", "set": "TEF", "number": "144"},
+                {"count": 2, "name": "Lost Vacuum", "set": "CRZ", "number": "135"},
+                {"count": 1, "name": "Hisuian Heavy Ball", "set": "ASR", "number": "146"},
+                {"count": 1, "name": "Pokégear 3.0", "set": "SVI", "number": "186"},
+                {"count": 1, "name": "Prime Catcher", "set": "TEF", "number": "157"},
+                {"count": 1, "name": "Rescue Board", "set": "TEF", "number": "159"},
+                {"count": 1, "name": "Forest Seal Stone", "set": "SIT", "number": "156"},
+                {"count": 2, "name": "Town Store", "set": "OBF", "number": "196"},
+            ],
+            "energy": [
+                {"count": 4, "name": "Basic Darkness Energy", "set": "SVE", "number": "7"},
+                {"count": 3, "name": "Basic Water Energy", "set": "SVE", "number": "3"},
+                {"count": 2, "name": "Basic Lightning Energy", "set": "SVE", "number": "4"},
+                {"count": 1, "name": "Basic Psychic Energy", "set": "SVE", "number": "5"},
+            ]
+        }
+    },
+    {
+        "firstName": "Said Azmi",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/chien-pao.png',
+        sprite2: '../../../../assets/sprites/baxcalibur.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Angga Fachrurrozi",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Anggie Tjakra",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Rafli Attar Ricco",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Christian",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Audityo Pramudya Mulyo",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Cai",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Alexander Kusumapradja",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Muhamad Adystia Nugraha",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Terry Hilario",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Cedriec",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Ilyyas Sukmadjarna",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Jonathan",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Effendy Sanjaya",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Paskalis Paskal",
+        flag: '../../../../assets/flags/indonesia.png',
+        "event": "2024 Indonesia Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
