93bc47728c03575fec7c7b5195f78e1bbf78f355
Alexander Wilson <alexwilson0017@yahoo.com>
2024-06-08 04:13:04 +0000

    2010

--- tournaments/year/2024/retro-celebration-2010/results.js
+++ tournaments/year/2024/retro-celebration-2010/results.js
@@ -19,4 +19,144 @@ let masters = [
             ]
         }
     },
+    {
+        "firstName": "Erik Nance",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2024 NAIC Retro Celebration (2010)",
+        sprite1: '../../../../assets/sprites/steelix.png',
+        sprite2: '../../../../assets/sprites/blissey.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Grant Shaw",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2024 NAIC Retro Celebration (2010)",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/jumpluff.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Velen Ricci",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2024 NAIC Retro Celebration (2010)",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/jumpluff.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Henry Chao",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2024 NAIC Retro Celebration (2010)",
+        sprite1: '../../../../assets/sprites/dialga.png',
+        sprite2: '../../../../assets/sprites/garchomp.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Cyrus Davis",
+        flag: '../../../../assets/flags/canada.png',
+        "event": "2024 NAIC Retro Celebration (2010)",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gengar.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Mark Nilson",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2024 NAIC Retro Celebration (2010)",
+        sprite1: '../../../../assets/sprites/luxray.png',
+        sprite2: '../../../../assets/sprites/garchomp.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Evan Cole",
+        flag: '../../../../assets/flags/usa.png',
+        "event": "2024 NAIC Retro Celebration (2010)",
+        sprite1: '../../../../assets/sprites/dialga.png',
+        sprite2: '../../../../assets/sprites/garchomp.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
 ]
