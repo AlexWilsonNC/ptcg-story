0083604fc9dbb29e406f8420781886825c50282c
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-06-16 00:58:39 -0400

    13 nats

---
+++ tournaments/year/2013/australia-nationals/results.js
@@ -0,0 +1,163 @@
+let masters = [
+    {
+        firstName: 'Kerwin Lee',
+        flag: '../../../../assets/flags/australia.png',
+        sprite1: '../../../../assets/sprites/rayquaza.png',
+        sprite2: '../../../../assets/sprites/eelektrik.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        event: '2013 Australia National Championships',
+    decklist: {
+      "pokemon": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "trainer": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "energy": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ]
+    },
+    },
+    {
+        firstName: 'Shane Quinn',
+        flag: '../../../../assets/flags/australia.png',
+        sprite1: '../../../../assets/sprites/keldeo-resolute.png',
+        sprite2: '../../../../assets/sprites/blastoise.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        event: '2013 Australia National Championships',
+    decklist: {
+      "pokemon": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "trainer": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "energy": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ]
+    },
+    },
+    {
+        firstName: 'Myles O’Neill',
+        flag: '../../../../assets/flags/australia.png',
+        sprite1: '../../../../assets/sprites/kyurem.png',
+        sprite2: '../../../../assets/sprites/deoxys.png',
+        list: '../../../../assets/list-icon.png',
+        event: '2013 Australia National Championships',
+    decklist: {
+      "pokemon": [
+        { "count": 3, "name": "Kyurem", "set": "PLF", "number": "31" },
+        { "count": 4, "name": "Deoxys-EX", "set": "PLF", "number": "53" },
+        { "count": 2, "name": "Thundurus-EX", "set": "PLF", "number": "38" },
+        { "count": 1, "name": "Absol", "set": "PLF", "number": "67" },
+      ], "trainer": [
+        { "count": 4, "name": "Professor Juniper", "set": "DEX", "number": "98" },
+        { "count": 4, "name": "N", "set": "DEX", "number": "96" },
+        { "count": 3, "name": "Colress", "set": "PLS", "number": "118" },
+        { "count": 2, "name": "Skyla", "set": "BCR", "number": "134" },
+        { "count": 1, "name": "Computer Search", "set": "BCR", "number": "137" },
+        { "count": 4, "name": "Pokémon Catcher", "set": "EPO", "number": "95" },
+        { "count": 4, "name": "Hypnotoxic Laser", "set": "PLS", "number": "123" },
+        { "count": 4, "name": "Switch", "set": "BLW", "number": "104" },
+        { "count": 3, "name": "Colress Machine", "set": "PLS", "number": "119" },
+        { "count": 3, "name": "Ultra Ball", "set": "DEX", "number": "102" },
+        { "count": 1, "name": "Team Plasma Ball", "set": "PLF", "number": "105" },
+        { "count": 2, "name": "Float Stone", "set": "PLF", "number": "99" },
+        { "count": 2, "name": "Virbank City Gym", "set": "PLS", "number": "126" },
+      ], "energy": [
+        { "count": 1, "name": "Lightning Energy", "set": "BLW", "number": "108" },
+        { "count": 4, "name": "Plasma Energy", "set": "PLS", "number": "127" },
+        { "count": 4, "name": "Prism Energy", "set": "NXD", "number": "93" },
+        { "count": 4, "name": "Blend Energy WaterLightningFightingMetal", "set": "DRX", "number": "118" },
+      ]
+    },
+    },
+    {
+        firstName: 'Thomas Winkelman',
+        flag: '../../../../assets/flags/australia.png',
+        sprite1: '../../../../assets/sprites/darkrai.png',
+        sprite2: '../../../../assets/sprites/sableye.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        event: '2013 Australia National Championships',
+    decklist: {
+      "pokemon": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "trainer": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "energy": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ]
+    },
+    },
+    {
+        firstName: 'Michael Kan',
+        flag: '../../../../assets/flags/australia.png',
+        sprite1: '../../../../assets/sprites/keldeo-resolute.png',
+        sprite2: '../../../../assets/sprites/blastoise.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        event: '2013 Australia National Championships',
+    decklist: {
+      "pokemon": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "trainer": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "energy": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ]
+    },
+    },
+    {
+        firstName: 'Jennifer Wilson',
+        flag: '../../../../assets/flags/australia.png',
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/klinklang.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        event: '2013 Australia National Championships',
+    decklist: {
+      "pokemon": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "trainer": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "energy": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ]
+    },
+    },
+    {
+        firstName: 'Andrew Tan',
+        flag: '../../../../assets/flags/australia.png',
+        sprite1: '../../../../assets/sprites/kyurem.png',
+        sprite2: '../../../../assets/sprites/deoxys.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        event: '2013 Australia National Championships',
+    decklist: {
+      "pokemon": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "trainer": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "energy": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ]
+    },
+    },
+    {
+        firstName: 'Joey Forster',
+        flag: '../../../../assets/flags/australia.png',
+        sprite1: '../../../../assets/sprites/kyurem.png',
+        sprite2: '../../../../assets/sprites/deoxys.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        event: '2013 Australia National Championships',
+    decklist: {
+      "pokemon": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "trainer": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ], "energy": [
+        {"count": 0, "name": "example", "set": "aa", "number": "00"},
+      ]
+    },
+    },
+]
---
+++ tournaments/year/2013/australia-nationals/seniors.js
@@ -0,0 +1,82 @@
+let masters = [
+    {
+        "firstName": "Kaiwen Cabbabe",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2014 Australia National Championships - Seniors",
+        sprite1: '../../../../assets/sprites/thundurus-therian.png',
+        sprite2: '../../../../assets/sprites/deoxys.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Jordan Palmer",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2014 Australia National Championships - Seniors",
+        sprite1: '../../../../assets/sprites/thundurus-therian.png',
+        sprite2: '../../../../assets/sprites/deoxys.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2014 Australia National Championships - Seniors",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/australia.png',
+        "event": "2014 Australia National Championships - Seniors",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+  ]
