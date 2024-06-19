42d65cb134605a294aa27ee365dc3d98bbc00316
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-06-19 16:27:45 -0400

    13 nats

---
+++ tournaments/year/2013/malaysia-nationals/results.js
@@ -0,0 +1,162 @@
+let masters = [
+  {
+      "firstName": "Shane Chee",
+      flag: '../../../../assets/flags/malaysia.png',
+      "event": "2013 Malaysia National Championships",
+      sprite1: '../../../../assets/sprites/blank.png',
+      sprite2: '../../../../assets/sprites/darkrai.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Kent Ng",
+      flag: '../../../../assets/flags/malaysia.png',
+      "event": "2013 Malaysia National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Yee Wei Chun",
+      flag: '../../../../assets/flags/malaysia.png',
+      "event": "2013 Malaysia National Championships",
+      sprite1: '../../../../assets/sprites/blank.png',
+      sprite2: '../../../../assets/sprites/darkrai.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Nero Koh",
+      flag: '../../../../assets/flags/malaysia.png',
+      "event": "2013 Malaysia National Championships",
+      sprite1: '../../../../assets/sprites/lugia.png',
+      sprite2: '../../../../assets/sprites/darkrai.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Richard Lee",
+      flag: '../../../../assets/flags/malaysia.png',
+      "event": "2013 Malaysia National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Aaron Minjoot",
+      flag: '../../../../assets/flags/malaysia.png',
+      "event": "2013 Malaysia National Championships",
+      sprite1: '../../../../assets/sprites/blank.png',
+      sprite2: '../../../../assets/sprites/klinklang.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Izmir Armani",
+      flag: '../../../../assets/flags/malaysia.png',
+      "event": "2013 Malaysia National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Lawrence Chong",
+      flag: '../../../../assets/flags/malaysia.png',
+      "event": "2013 Malaysia National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+]
