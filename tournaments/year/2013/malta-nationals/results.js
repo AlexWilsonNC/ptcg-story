dfeb270f742c245b35f9790add7749ac34d4ebbe
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-06-19 16:43:49 -0400

    13 nats

--- tournaments/year/2013/malta-nationals/results.js
+++ tournaments/year/2013/malta-nationals/results.js
@@ -1,10 +1,70 @@
 let masters = [
   {
-      "firstName": "--",
-      flag: '../../../../assets/flags/unknown.png',
-      "event": "2013 eeee National Championships",
+      "firstName": "William Healey",
+      flag: '../../../../assets/flags/malta.png',
+      "event": "2013 Malta National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Alessandro Valastro",
+      flag: '../../../../assets/flags/malta.png',
+      "event": "2013 Malta National Championships",
       sprite1: '../../../../assets/sprites/blank.png',
-      sprite2: '../../../../assets/sprites/hyphen.png',
+      sprite2: '../../../../assets/sprites/darkrai.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Matthew MacKay",
+      flag: '../../../../assets/flags/malta.png',
+      "event": "2013 Malta National Championships",
+      sprite1: '../../../../assets/sprites/blank.png',
+      sprite2: '../../../../assets/sprites/darkrai.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Kevin Duca",
+      flag: '../../../../assets/flags/malta.png',
+      "event": "2013 Malta National Championships",
+      sprite1: '../../../../assets/sprites/blank.png',
+      sprite2: '../../../../assets/sprites/darkrai.png',
       // list: '../../../../assets/list-icon.png',
       list: '../../../../assets/sprites/blank.png',
       "decklist": {
---
+++ tournaments/year/2013/mexico-nationals/results.js
@@ -0,0 +1,162 @@
+let masters = [
+  {
+      "firstName": "Bernardo Nuñez",
+      flag: '../../../../assets/flags/mexico.png',
+      "event": "2013 Mexico National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Hanzel Caballero",
+      flag: '../../../../assets/flags/mexico.png',
+      "event": "2013 Mexico National Championships",
+      sprite1: '../../../../assets/sprites/keldeo-resolute.png',
+      sprite2: '../../../../assets/sprites/blastoise.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Roberto Shirasago",
+      flag: '../../../../assets/flags/mexico.png',
+      "event": "2013 Mexico National Championships",
+      sprite1: '../../../../assets/sprites/keldeo-resolute.png',
+      sprite2: '../../../../assets/sprites/blastoise.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Christian Cruz",
+      flag: '../../../../assets/flags/mexico.png',
+      "event": "2013 Mexico National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Adolfo Tuñon",
+      flag: '../../../../assets/flags/mexico.png',
+      "event": "2013 Mexico National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Alfredo Vargas",
+      flag: '../../../../assets/flags/mexico.png',
+      "event": "2013 Mexico National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Arturo Miranda",
+      flag: '../../../../assets/flags/mexico.png',
+      "event": "2013 Mexico National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+  {
+      "firstName": "Manuel Valdes",
+      flag: '../../../../assets/flags/mexico.png',
+      "event": "2013 Mexico National Championships",
+      sprite1: '../../../../assets/sprites/kyurem.png',
+      sprite2: '../../../../assets/sprites/deoxys.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+]
