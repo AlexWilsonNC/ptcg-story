5c0151084a9f4197ce60d5857bae3f8c54031138
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-06-20 01:27:12 -0400

    taiwan

---
+++ tournaments/year/2024/taiwan-championships/results.js
@@ -0,0 +1,172 @@
+let masters = [
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Taiwan Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/snorlax.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Taiwan Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/sableye.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Taiwan Championships",
+        sprite1: '../../../../assets/sprites/comfey.png',
+        sprite2: '../../../../assets/sprites/sableye.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "--",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Taiwan Championships",
+        sprite1: '../../../../assets/sprites/lugia.png',
+        sprite2: '../../../../assets/sprites/archeops.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "Jiao",
+        flag: '../../../../assets/flags/taiwan.png',
+        "event": "2024 Taiwan Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/gardevoir.png',
+        list: '../../../../assets/list-icon.png',
+        "decklist": {
+            "pokemon": [
+                {"count": 2, "name": "Gardevoir ex", "set": "PAF", "number": "29"},
+                {"count": 4, "name": "Kirlia", "set": "SIT", "number": "68"},
+                {"count": 4, "name": "Ralts", "set": "ASR", "number": "60"},
+                {"count": 2, "name": "Munkidori", "set": "TWM", "number": "95"},
+                {"count": 1, "name": "Scream Tail", "set": "PAR", "number": "86"},
+                {"count": 1, "name": "Klefki", "set": "SVI", "number": "96"},
+                {"count": 1, "name": "Flutter Mane", "set": "TEF", "number": "78"},
+                {"count": 1, "name": "Mew ex", "set": "MEW", "number": "151"},
+                {"count": 1, "name": "Squawkabilly ex", "set": "PAF", "number": "75"},
+                {"count": 1, "name": "Manaphy", "set": "BRS", "number": "41"},
+            ], "trainer": [
+                {"count": 4, "name": "Iono", "set": "PAF", "number": "80"},
+                {"count": 2, "name": "Arven", "set": "OBF", "number": "186"},
+                {"count": 2, "name": "Professor Turo's Scenario", "set": "PAR", "number": "171"},
+                {"count": 1, "name": "Professor’s Research (Professor Sada)", "set": "SVI", "number": "189"},
+                {"count": 1, "name": "Boss’s Orders (Ghetsis)", "set": "PAL", "number": "172"},
+                {"count": 4, "name": "Buddy-Buddy Poffin", "set": "TEF", "number": "144"},
+                {"count": 3, "name": "Nest Ball", "set": "SVI", "number": "181"},
+                {"count": 2, "name": "Counter Catcher", "set": "PAR", "number": "160"},
+                {"count": 2, "name": "Super Rod", "set": "PAL", "number": "188"},
+                {"count": 2, "name": "Earthen Vessel", "set": "PAR", "number": "163"},
+                {"count": 2, "name": "Ultra Ball", "set": "SVI", "number": "196"},
+                {"count": 2, "name": "Rare Candy", "set": "SVI", "number": "191"},
+                {"count": 1, "name": "Enhanced Hammer", "set": "TWM", "number": "148"},
+                {"count": 1, "name": "Pal Pad", "set": "SVI", "number": "182"},
+                {"count": 1, "name": "Unfair Stamp", "set": "TWM", "number": "165"},
+                {"count": 1, "name": "Hisuian Heavy Ball", "set": "ASR", "number": "146"},
+                {"count": 1, "name": "Technical Machine: Evolution", "set": "PAR", "number": "178"},
+                {"count": 1, "name": "Artazon", "set": "PAF", "number": "76"},
+            ], "energy": [
+                {"count": 7, "name": "Basic Psychic Energy", "set": "SVE", "number": "5"},
+                {"count": 2, "name": "Basic Darkness Energy", "set": "SVE", "number": "7"},
+            ]
+        }
+    },
+    {
+        "firstName": "eeee",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Taiwan Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "eeee",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Taiwan Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+    {
+        "firstName": "eeee",
+        flag: '../../../../assets/flags/unknown.png',
+        "event": "2024 Taiwan Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/hyphen.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ], "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
