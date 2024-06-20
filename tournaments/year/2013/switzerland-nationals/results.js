36acb1fb51194c4eba175bd693a0bae9fca0213c
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-06-20 01:04:33 -0400

    13 nats

---
+++ tournaments/year/2013/switzerland-nationals/results.js
@@ -0,0 +1,162 @@
+let masters = [
+{
+      "firstName": "Xavier Delfose",
+      flag: '../../../../assets/flags/switzerland.png',
+      "event": "2013 Switzerland National Championships",
+      sprite1: '../../../../assets/sprites/landorus-therian.png',
+      sprite2: '../../../../assets/sprites/tornadus.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+{
+      "firstName": "Rafael Katsuya",
+      flag: '../../../../assets/flags/switzerland.png',
+      "event": "2013 Switzerland National Championships",
+      sprite1: '../../../../assets/sprites/landorus-therian.png',
+      sprite2: '../../../../assets/sprites/garbodor.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+{
+      "firstName": "Remo Lehman",
+      flag: '../../../../assets/flags/switzerland.png',
+      "event": "2013 Switzerland National Championships",
+      sprite1: '../../../../assets/sprites/landorus-therian.png',
+      sprite2: '../../../../assets/sprites/garbodor.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+{
+      "firstName": "Remo Zhan",
+      flag: '../../../../assets/flags/switzerland.png',
+      "event": "2013 Switzerland National Championships",
+      sprite1: '../../../../assets/sprites/lugia.png',
+      sprite2: '../../../../assets/sprites/snorlax.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+{
+      "firstName": "Luca Clavadetscher",
+      flag: '../../../../assets/flags/switzerland.png',
+      "event": "2013 Switzerland National Championships",
+      sprite1: '../../../../assets/sprites/keldeo-resolute.png',
+      sprite2: '../../../../assets/sprites/blastoise.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+{
+      "firstName": "Louis Bich",
+      flag: '../../../../assets/flags/switzerland.png',
+      "event": "2013 Switzerland National Championships",
+      sprite1: '../../../../assets/sprites/blank.png',
+      sprite2: '../../../../assets/sprites/klinklang.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+{
+      "firstName": "Miguel Garcia",
+      flag: '../../../../assets/flags/switzerland.png',
+      "event": "2013 Switzerland National Championships",
+      sprite1: '../../../../assets/sprites/rayquaza.png',
+      sprite2: '../../../../assets/sprites/eelektrik.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+{
+      "firstName": "Kevin Staub",
+      flag: '../../../../assets/flags/switzerland.png',
+      "event": "2013 Switzerland National Championships",
+      sprite1: '../../../../assets/sprites/landorus-therian.png',
+      sprite2: '../../../../assets/sprites/tornadus.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+]
---
+++ tournaments/year/2013/switzerland-nationals/seniors.js
@@ -0,0 +1,82 @@
+let masters = [
+    {
+        "firstName": "Nino Kunz",
+        flag: '../../../../assets/flags/switzerland.png',
+        "event": "2013 Switzerland National Championships",
+        sprite1: '../../../../assets/sprites/landorus-therian.png',
+        sprite2: '../../../../assets/sprites/garbodor.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+  {
+        "firstName": "Titouan",
+        flag: '../../../../assets/flags/switzerland.png',
+        "event": "2013 Switzerland National Championships",
+        sprite1: '../../../../assets/sprites/landorus-therian.png',
+        sprite2: '../../../../assets/sprites/tornadus.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+  {
+        "firstName": "Andrea M",
+        flag: '../../../../assets/flags/switzerland.png',
+        "event": "2013 Switzerland National Championships",
+        sprite1: '../../../../assets/sprites/keldeo-resolute.png',
+        sprite2: '../../../../assets/sprites/blastoise.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+  {
+        "firstName": "Bastien",
+        flag: '../../../../assets/flags/switzerland.png',
+        "event": "2013 Switzerland National Championships",
+        sprite1: '../../../../assets/sprites/blank.png',
+        sprite2: '../../../../assets/sprites/klinklang.png',
+        // list: '../../../../assets/list-icon.png',
+        list: '../../../../assets/sprites/blank.png',
+        "decklist": {
+            "pokemon": [
+                { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "trainer": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ],
+            "energy": [
+                { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+            ]
+        }
+    },
+]
