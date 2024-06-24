337e8568c3e05e9484bc6fc686f44a7210641464
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-06-24 00:38:11 -0400

    2011 nats

---
+++ tournaments/year/2024/worlds/results.js
@@ -0,0 +1,22 @@
+let masters = [
+  {
+      "firstName": "eeee",
+      flag: '../../../../assets/flags',
+      "event": "2012 eeee National Championships",
+      sprite1: '../../../../assets/sprites/blank.png',
+      sprite2: '../../../../assets/sprites/hyphen.png',
+      // list: '../../../../assets/list-icon.png',
+      list: '../../../../assets/sprites/blank.png',
+      "decklist": {
+          "pokemon": [
+              { "count": 3, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "trainer": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ],
+          "energy": [
+              { "count": 4, "name": "ee", "number": "ee", "set": "ee" },
+          ]
+      }
+  },
+]
